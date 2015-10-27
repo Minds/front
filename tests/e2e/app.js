1177023c045fd70a9de45d9afa29b59e919f64ef
Ignacio Sarnaglia <ignacio@argeniss.com>
2015-10-27 12:47:44 -0300

    Fixes protractor to run tests in order

--- tests/e2e/app.js
+++ tests/e2e/app.js
@@ -1,19 +1,21 @@
 // import { verifyNoBrowserErrors } from 'angular2/src/test_lib/e2e_util';
 
 describe('testing the tests', () => {
-	// afterEach(verifyNoBrowserErrors);
 
-	browser.get('/');
+	beforeEach(function() {
+		browser.get('/');
+	});
+
+	afterEach(function() {
+	});
+
 
   it('should have a title', function(){
-    expect(browser.getTitle()).toEqual("Login | Minds");
+    expect(browser.getTitle()).toEqual("Home | Minds");
   });
 
   it('should have a title', function(){
-    expect(browser.getTitle()).toEqual("Login | Minds");
+    expect(browser.getTitle()).toEqual("Home | Minds");
   });
-
-//  element(by.css('[value="add"]')).click();
-	//browser.pause();
 
 });
