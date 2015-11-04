7fd8c1d354b82700d8b213dd0d136552ed6f3e6c
Mark Harding <mark@kramnorth.com>
2015-11-04 11:48:30 +0000

    (test): adds a search test

--- protractor.js
+++ protractor.js
@@ -1,37 +1,57 @@
+/**
+ * To run locally
+ *    export MINDS_E2E_LOCAL=TRUE
+ *   to select a browser run
+ *       export MINDS_E2E_BROWSERS=Chrome (firefox, safari)
+ *
+ * To run on Sauce Labs
+ *    export MINDS_E2E_BASE=https://new.minds.com/
+ *    export MINDS_E2E_LOCAL=FALSE
+ */
+
 require('babel/register');
 
-var BASE_URL = process.env.minds_base_url ? process.env.minds_base_url : 'http://dev.minds.io/';
+var BASE_URL = process.env.MINDS_E2E_BASE ? process.env.MINDS_E2E_BASE : 'http://dev.minds.io/';
 var BROWSERS = require('./sauce.conf');
 var aliases = BROWSERS.aliases;
 
-var _browsers = process.env.browsers ? process.env.browsers : ['DESKTOP'];
+var _browsers = process.env.MINDS_E2E_BROWSERS ? process.env.MINDS_E2E_BROWSERS : ['CHROME'];
 
-exports.config = {
+var config = {
 	baseUrl: BASE_URL,
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
 
 	onPrepare: function() {
 		browser.ignoreSynchronization = true;
     browser.driver.get(BASE_URL);
 		if(BASE_URL == 'https://new.minds.com/')
     	browser.manage().addCookie('beta', 'angular2', '/', 'new.minds.com');
-
 	},
 	specs: [
 		'./tests/e2e/helpers.js',
 		'./tests/e2e/*.js'
 	],
 	exclude: [],
-  multiCapabilities: aliases[_browsers].map(function(alias){
-    console.log('testing: ' + BROWSERS.customLaunchers[alias].browserName);
-    var b = BROWSERS.customLaunchers[alias];
-    b.name = BROWSERS.customLaunchers[alias].browserName + ' (' + b.version + ') test';
-    return b;
-  }),
+	capabilities: {
+		browserName: _browsers
+	},
 	framework: 'jasmine2',
 	jasmineNodeOpts: {
 		showColors: true,
 		defaultTimeoutInterval: 60000
 	}
 };
+
+if(process.env.MINDS_E2E_LOCAL == 'TRUE'){
+	config.directConnect =  true;
+} else {
+	config.sauceUser =  process.env.SAUCE_USERNAME;
+	config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+	config.multiCapabilities = aliases[_browsers].map(function(alias){
+	    console.log('testing: ' + BROWSERS.customLaunchers[alias].browserName);
+	    var b = BROWSERS.customLaunchers[alias];
+	    b.name = BROWSERS.customLaunchers[alias].browserName + ' (' + b.version + ') test';
+	    return b;
+	  });
+}
+
+exports.config = config;
