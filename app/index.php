e607ad01c2b80f5a90ef6dab40f701c4495c5f9d
Mark Harding <mark@kramnorth.com>
2015-11-19 18:00:05 +0000

    (fix): do not support android 4.3 browser

--- app/index.php
+++ app/index.php
@@ -49,8 +49,8 @@ ini_set( 'display_errors','1');
 
     <script>
       var ua = window.navigator.userAgent;
-      if(ua.indexOf("MSIE") > 0 ||
-        (ua.indexOf("Android 4.3") > 0 && ua.indexOf('Chrome') > 0) //android 4.3, but not chrome browser
+      if(ua.indexOf("MSIE") > -1 ||
+        (ua.indexOf("Android 4.3") > -1 && (!ua.indexOf('Chrome') > -1)) //android 4.3, but not chrome browser
         ){
         window.location.href = "/not-supported";
       }
