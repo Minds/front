56afc2cc38706b7b810a8ca0f261f146de0463f5
Mark Harding <mark@kramnorth.com>
2015-12-10 17:05:35 +0000

    (feat): uses large ad blocks

--- app/src/components/ads/google-ads.ts
+++ app/src/components/ads/google-ads.ts
@@ -2,16 +2,25 @@ import { Component, CORE_DIRECTIVES, EventEmitter, ElementRef } from 'angular2/a
 
 @Component({
   selector: 'google-ad',
+  //template: `
+  //  <ins
+  //    *ng-if="visible"
+  //    class="adsbygoogle"
+  //     style="display:block; width:100%;"
+  //     data-ad-client="ca-pub-9303771378013875"
+  //     data-ad-slot="7588308825"
+  //     data-ad-format="auto"
+  //     data-ad-test="on"
+  //     ></ins>
+  //`,
   template: `
-    <ins
-      *ng-if="visible"
-      class="adsbygoogle"
-       style="display:block; width:100%;"
+    <ins class="adsbygoogle"
+       style="display:block;width:336px;height:280px;margin: auto; padding: 8px; background: #EEE;"
        data-ad-client="ca-pub-9303771378013875"
-       data-ad-slot="7588308825"
-       data-ad-format="auto"
-       data-ad-test="on"
-       ></ins>
+       data-ad-slot="5788264423"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
   `,
   directives: [ CORE_DIRECTIVES ],
   host: {
