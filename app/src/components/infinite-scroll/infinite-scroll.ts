e41a18cf17e3d29cf23d074c59c698fa8e4ea72f
Mark Harding <mark@kramnorth.com>
2016-01-05 11:56:09 +0000

    (feat): implements a clickable load-more button

---
+++ app/src/components/infinite-scroll/infinite-scroll.ts
@@ -0,0 +1,66 @@
+import { Component, View, EventEmitter, ElementRef, Inject } from 'angular2/core';
+import { CORE_DIRECTIVES } from 'angular2/common';
+
+import { MDL_DIRECTIVES } from '../../directives/material';
+import { Material as MaterialService } from "../../services/ui";
+import { ScrollFactory } from '../../services/ux/scroll';
+
+
+@Component({
+  selector: 'infinite-scroll',
+  inputs: ['distance', 'on', 'inProgress', 'moreData', 'hideManual'],
+  outputs: ['loadHandler: load'],
+  template: `
+    <div class="mdl-spinner mdl-js-spinner is-active" [mdl] [hidden]="!inProgress"></div>
+    <div class="m-infinite-scroll-manual mdl-color--blue-grey-200 mdl-color-text--blue-grey-500"
+      [hidden]="inProgress || !moreData"
+      (click)="manualLoad()"
+      *ngIf="!hideManual">
+      Click to load more
+    </div>
+    <div class="m-infinite-scroll-manual mdl-color--blue-grey-200 mdl-color-text--blue-grey-500"
+      [hidden]="moreData"
+      *ngIf="!hideManual">
+      Nothing more to load
+    </div>
+  `,
+  directives: [ CORE_DIRECTIVES, MDL_DIRECTIVES ]
+})
+
+
+export class InfiniteScroll{
+
+  scroll = ScrollFactory.build();
+
+  element : any;
+  loadHandler: EventEmitter<boolean> = new EventEmitter(true);
+  distance : any;
+  inProgress : boolean = false;
+  moreData : boolean = true;
+  hideManual : boolean = false;
+  _content : any;
+  _listener;
+
+  constructor(_element: ElementRef) {
+    this.element = _element.nativeElement;
+    this.init();
+  }
+
+  init(){
+    this._listener = this.scroll.listen((view) => {
+      if(this.element.offsetTop - this.element.clientHeight - view.height <= view.top && this.moreData){
+        this.loadHandler.next(true);
+      }
+    });
+  }
+
+  manualLoad(){
+    this.loadHandler.next(true);
+  }
+
+  ngOnDestroy(){
+    if(this._listener)
+      this.scroll.unListen(this._listener);
+  }
+
+}
