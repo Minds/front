71aa8ea67f23893cfe07a797312dd3ee6621b7c1
Mark Harding <mark@kramnorth.com>
2016-02-19 11:11:46 +0000

    (fix): 2fa

--- app/src/controllers/settings/two-factor/two-factor.ts
+++ app/src/controllers/settings/two-factor/two-factor.ts
@@ -16,14 +16,67 @@ import { Material } from '../../../directives/material';
 export class SettingsTwoFactor{
 
   minds : Minds;
-  user : any;
-  settings : string;
+  telno : number;
+  secret;
+  waitingForCheck : boolean = false;
+
+  inProgress : boolean = false;
+  error : string = "";
 
   constructor(public client: Client){
     this.minds = window.Minds;
+    this.load();
   }
 
-  send(smsNumber : any){
-
+  load(){
+    this.inProgress = true;
+    this.client.get('api/v1/twofactor')
+      .then((response : any) => {
+        if(response.telno)
+          this.telno = response.telno;
+        this.inProgress = false;
+      });
   }
+
+  setup(smsNumber : any){
+    this.telno = smsNumber;
+    this.waitingForCheck = true;
+    this.error = "";
+    this.client.post('api/v1/twofactor/setup', { tel: smsNumber })
+      .then((response : any) => {
+        this.secret = response.secret;
+      })
+      .catch(() => {
+        this.waitingForCheck = false;
+        this.telno = null;
+        this.error = "The number you entered was incorrect";
+      })
+  }
+
+  check(code : number){
+    this.client.post('api/v1/twofactor/check/' + this.secret, {
+        code: code,
+        telno: this.telno
+      })
+      .then((response : any) => {
+        this.waitingForCheck = false;
+      })
+      .catch((response : any) => {
+        this.waitingForCheck = false;
+        this.telno = null;
+        this.error = "The code was incorrect. Try again.";
+      });
+  }
+
+  retry(){
+    this.telno = null;
+    this.waitingForCheck = false;
+  }
+
+  cancel(){
+    this.client.delete('api/v1/twofactor');
+    this.telno = null;
+    this.error = "";
+  }
+
 }
