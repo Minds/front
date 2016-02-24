6d54a6a958c82dfd412b2b80d3309b41adabd1ea
Mark Harding <mark@kramnorth.com>
2016-02-24 19:26:20 +0000

    (fix): new tools for ng-beta.7

--- tools/tasks/build.js.ts
+++ tools/tasks/build.js.ts
@@ -6,16 +6,20 @@ export = function buildJS(gulp, plugins) {
   return function () {
     let tsProject = tsProjectFn(plugins);
     let src = [
+      'typings/browser.d.ts',
+      'tools/manual_typings/**/*.d.ts',
       join(APP_SRC, '**/*.ts'),
       '!' + join(APP_SRC, '**/*_spec.ts')
     ];
 
     let result = gulp.src(src)
       .pipe(plugins.plumber())
+      .pipe(plugins.sourcemaps.init())
       .pipe(plugins.inlineNg2Template({ base: APP_SRC }))
       .pipe(plugins.typescript(tsProject));
 
     return result.js
+      .pipe(plugins.sourcemaps.write())
       .pipe(plugins.template(templateLocals()))
       .pipe(gulp.dest(TMP_DIR));
   };
