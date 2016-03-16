b58a5fef5f7e9110abfa33edddb036af0a058643
Mark Harding <mark@kramnorth.com>
2016-03-16 17:20:53 +0000

    (fix): login flow from homepage and register pages

--- app/src/controllers/home/register/register.ts
+++ app/src/controllers/home/register/register.ts
@@ -2,6 +2,9 @@ import { Component } from 'angular2/core';
 import { CORE_DIRECTIVES, FORM_DIRECTIVES, ControlGroup, FormBuilder, Validators } from 'angular2/common';
 import { Router, RouteParams } from 'angular2/router';
 
+import { FORM_COMPONENTS } from '../../../components/forms/forms';
+
+import { SignupModalService } from '../../../components/modal/signup/service';
 import { Material } from '../../../directives/material';
 import { Client } from '../../../services/api';
 import { SessionFactory } from '../../../services/session';
@@ -10,11 +13,12 @@ import { SessionFactory } from '../../../services/session';
 @Component({
   selector: 'minds-register',
   templateUrl: 'src/controllers/home/register/register.html',
-  directives: [ FORM_DIRECTIVES, Material ]
+  directives: [ FORM_DIRECTIVES, Material, FORM_COMPONENTS ]
 })
 
 export class Register {
 
+  minds = window.Minds;
 	session = SessionFactory.build();
   errorMessage : string = "";
   twofactorToken : string = "";
@@ -24,61 +28,15 @@ export class Register {
 
   form : ControlGroup;
 
-	constructor(public client : Client, public router: Router, public params: RouteParams, fb: FormBuilder){
-    this.form = fb.group({
-      username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      password2: ['', Validators.required]
-    });
+	constructor(public client : Client, public router: Router, public params: RouteParams, private modal : SignupModalService){
 
     if(params.params['referrer'])
       this.referrer = params.params['referrer'];
 	}
 
-	register(e){
-    e.preventDefault();
-    this.errorMessage = "";
-
-    if(this.form.value.password != this.form.value.password2){
-        this.errorMessage = "Passwords must match.";
-        return;
-    }
-
-    this.inProgress = true;
-		var self = this; //this <=> that for promises
-		this.client.post('api/v1/register', this.form.value)
-			.then((data : any) => {
-			  this.form.value = null;
-
-        this.inProgress = false;
-				self.session.login(data.user);
-
-        if(this.referrer)
-          self.router.navigateByUrl(this.referrer);
-        else
-				  self.router.navigate(['/Channel', {
-            username: data.user.username,
-            editToggle: true
-          }]);
-			})
-			.catch((e) => {
-        console.log(e);
-        this.inProgress = false;
-        if(e.status == 'failed'){
-          //incorrect login details
-          self.errorMessage = "Incorrect username/password. Please try again.";
-          self.session.logout();
-        }
-
-        if(e.status == 'error'){
-          //two factor?
-          self.errorMessage = e.message;
-          self.session.logout();
-        }
-
-        return;
-			});
-	}
+  registered(){
+    this.modal.setDisplay('onboarding').open();
+    this.router.navigate(['/Discovery', {filter:'suggested', 'type': 'channels'}]);
+  }
 
 }
