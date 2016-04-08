382a450029a87e2057f51526c0753919bec062a5
Mark Harding <mark@kramnorth.com>
2016-04-08 09:57:53 +0100

    (fix): tags fix

--- app/src/components/forms/tags-input/tags.ts
+++ app/src/components/forms/tags-input/tags.ts
@@ -9,18 +9,16 @@ import { SessionFactory } from '../../../services/session';
 
 @Component({
   selector: 'minds-form-tags-input',
-  host: {
-    '(click)': 'focus()'
-  },
+
   inputs: [ '_tags: tags' ],
-  outputs: [ 'change' ],
+  outputs: [ 'change: tagsChange' ],
   directives: [ FORM_DIRECTIVES, Material ],
   template: `
     <div class="m-form-tags-input-tags-tag mdl-shadow--2dp mdl-color--blue-grey-600 mdl-color-text--blue-grey-50" *ngFor="#tag of tags; #i = index" (click)="removeTag(i)">
       <span>{{tag}}</span>
       <i class="material-icons mdl-color-text--white">close</i>
     </div>
-    <input type="text" [placeholder]="placeholder" #input (keyup)="keyUp(input, $event)" [size]="input.value.length ? input.value.length : 1">
+    <input type="text" [(ngModel)]="input" (keyup)="keyUp($event)" [size]="input.length ? input.length : 1">
   `
 })
 
@@ -30,6 +28,7 @@ export class TagsInput{
   error : string = "";
   inProgress : boolean = false;
 
+  input : string = "";
   placeholder : string = "+";
   tags : Array<string> = [];
   change : EventEmitter<any> = new EventEmitter();
@@ -43,19 +42,21 @@ export class TagsInput{
       this.tags = tags;
   }
 
-  keyUp(input, e){
+  keyUp(e){
+
     switch(e.keyCode){
       case 32: //space
       case 9: //tab
       case 13: //enter
-        if(input.value){
-          this.tags.push(input.value.replace(' ', ''));
-          input.value = "";
+        if(this.input){
+
+          this.tags.push(this.input);
+          this.input = "";
         }
         break;
       case 8: //backspace
         //remove the last tag if we don't have an input
-        if(!input.value){
+        if(!this.input){
           this.tags.pop();
         }
         break;
@@ -66,6 +67,7 @@ export class TagsInput{
 
   removeTag(index : number){
     this.tags.splice(index,1);
+    this.change.next(this.tags);
   }
 
   focus(){
