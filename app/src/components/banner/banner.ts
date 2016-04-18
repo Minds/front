d6a75c9833fc97d797970141609d2764dea60b48
Mark Harding <mark@kramnorth.com>
2016-04-18 17:07:49 +0100

    (fix): allow banners to have timestamps

--- app/src/components/banner/banner.ts
+++ app/src/components/banner/banner.ts
@@ -67,7 +67,7 @@ export class MindsBanner{
     if(!value)
       return;
     this.object = value;
-    this.src = "/fs/v1/banners/" + this.object.guid + '/' + this.top;
+    this.src = "/fs/v1/banners/" + this.object.guid + '/' + this.top + '/' + this.object.banner;
   }
 
   set _src(value : any){
