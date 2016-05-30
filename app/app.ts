51a9f00168ffd4ae325fd557074e7a960abc069a
Emi Balbuena <edgebal@gmail.com>
2016-05-30 01:06:57 -0300

    (feat): notification filters

--- app/app.ts
+++ app/app.ts
@@ -74,6 +74,7 @@ import {Groups, GroupsProfile, GroupsCreator} from './src/plugins/Groups/groups'
   { path: '/archive/edit/:guid', component: ArchiveEdit, as: 'Archive-Edit'},
 
   { path: '/notifications', component: Notifications, as: 'Notifications'},
+  { path: '/notifications/:filter', component: Notifications, as: 'Notifications-Filter'},
 
   { path: '/groups/:filter', component: Groups, as: 'Groups'},
   { path: '/groups/create', component: GroupsCreator, as: 'Groups-Create'},
