802495d0091b21117c27975ab8ad05f4e3939d33
Mark Harding <mark@marksmusiconline.co.uk>
2016-07-07 10:02:22 +0000

    (fix): angular2 import paths for invite controller

--- app/src/components/modal/invite/invite.ts
+++ app/src/components/modal/invite/invite.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter } from 'angular2/core';
-import { CORE_DIRECTIVES } from 'angular2/common';
-import { ROUTER_DIRECTIVES, Router } from 'angular2/router';
+import { Component, EventEmitter } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router-deprecated';
 
 import { Modal } from '../modal';
 import { SessionFactory } from '../../../services/session';
