35b1058323346cce6e4e39787eba435f617a4bfd
Mark Harding <mark@kramnorth.com>
2016-12-02 11:51:25 +0000

    (fix): socket.io path changed

--- tools/config.ts
+++ tools/config.ts
@@ -74,7 +74,7 @@ export const DEV_NPM_DEPENDENCIES: InjectableDependency[] = normalizeDependencie
   // -ng-rc- { src: '@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js', inject: 'libs', dest: JS_DEST },
   // -ng-rc- { src: '@angular/router/bundles/router.umd.min.js', inject: 'libs', dest: JS_DEST },
   // -ng-rc- { src: '@angular/router-deprecated/bundles/router-deprecated.umd.min.js', inject: 'libs', dest: JS_DEST },
-  { src: 'socket.io-client/socket.io.js', inject: 'libs', dest: JS_DEST },
+  { src: 'socket.io-client/dist/socket.io.js', inject: 'libs', dest: JS_DEST },
 
   // async
   { src: 'tinymce/tinymce.min.js', inject: 'async', dest: JS_DEST },
@@ -89,7 +89,7 @@ export const PROD_NPM_DEPENDENCIES: InjectableDependency[] = normalizeDependenci
   { src: 'zone.js/dist/zone.min.js', inject: 'shims' },
   { src: 'systemjs/dist/system.js', inject: 'shims' },
   // { src: 'angular2/bundles/angular2-polyfills.min.js', inject: 'libs' },
-  { src: 'socket.io-client/socket.io.js', inject: 'libs' },
+  { src: 'socket.io-client/dist/socket.io.js', inject: 'libs' },
   // { src: 'angular2/es6/dev/src/testing/shims_for_IE.js', inject: 'shims' },
   { src: 'intl/dist/Intl.min.js', inject: 'shims' },
   { src: 'intl/locale-data/jsonp/en.js', inject: 'shims' },
