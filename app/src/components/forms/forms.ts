8cfa93180c43a725322177d2612fed532955380f
Mark Harding <mark@kramnorth.com>
2016-12-14 20:50:45 +0000

    (feat): use state dropdown

--- app/src/components/forms/forms.ts
+++ app/src/components/forms/forms.ts
@@ -5,6 +5,7 @@ import { RegisterForm } from './register/register';
 import { FbRegisterForm } from './fb-register/fb-register';
 import { DateInput } from './date-input/date-input';
 import { CountryInput } from './country-input/country-input';
+import { StateInput } from './state-input/state-input';
 
 export const FORM_COMPONENTS: any[] = [ CityFinderForm, LoginForm, OnboardingForm, RegisterForm, FbRegisterForm,
-  DateInput, CountryInput ];
+  DateInput, CountryInput, StateInput ];
