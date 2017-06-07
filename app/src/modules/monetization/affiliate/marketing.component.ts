c3dedb86f5be5c242d5c5ab3d0e85afcf9f71b60
Mark Harding <mark@kramnorth.com>
2017-06-07 14:50:06 +0100

    (feat): click to become an affiliate

--- app/src/modules/monetization/affiliate/marketing.component.ts
+++ app/src/modules/monetization/affiliate/marketing.component.ts
@@ -8,4 +8,30 @@ import { Client } from '../../../common/api/client.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class AffiliateMarketingComponent {}
+export class AffiliateMarketingComponent {
+
+  user = window.Minds.user;
+
+  constructor(private client : Client, private cd : ChangeDetectorRef){
+  }
+
+  isAffiliate(){
+    for(let program of this.user.programs){
+      if(program == 'affiliate')
+        return true;
+    }
+    return false;
+  }
+
+  join(){
+    this.user.programs.push('affiliate');
+    this.client.put('api/v1/monetization/affiliates');
+    this.detectChanges();
+  }
+
+  detectChanges(){
+    this.cd.markForCheck();
+    this.cd.detectChanges();
+  }
+
+}
