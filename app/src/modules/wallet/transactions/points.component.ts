2ea0420d8c0110f3bb38cdbfe1e6dbf40afd1c0a
Mark Harding <mark@marksmusiconline.co.uk>
2017-06-20 13:17:35 +0000

    (fix): run change detection on wallet load

--- app/src/modules/wallet/transactions/points.component.ts
+++ app/src/modules/wallet/transactions/points.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 import { Client } from '../../../services/api';
 import { SessionFactory } from '../../../services/session';
@@ -17,34 +17,35 @@ export class WalletPointsTransactionsComponent  {
   offset: string = "";
   inProgress : boolean = false;
   moreData : boolean = true;
-
-	constructor(public client: Client){
+  
+  constructor(public client: Client, private cd: ChangeDetectorRef){
     this.load();
-	}
+  }
 
   load(refresh : boolean = false){
-    var self = this;
     this.inProgress = true;
     this.client.get('api/v1/wallet/transactions', { limit: 12, offset: this.offset})
       .then((response : any) => {
 
         if(!response.transactions){
-          self.moreData = false;
-          self.inProgress = false;
+          this.moreData = false;
+          this.inProgress = false;
           return false;
         }
 
         if(refresh){
-          self.transactions = response.transactions
+          this.transactions = response.transactions
         } else {
-          if(self.offset)
+          if(this.offset)
             response.transactions.shift();
           for(let transaction of response.transactions)
-            self.transactions.push(transaction);
+            this.transactions.push(transaction);
         }
 
-        self.offset = response['load-next'];
-        self.inProgress = false;
+        this.offset = response['load-next'];
+        this.inProgress = false;
+        this.cd.markForCheck();
+        this.cd.detectChanges();
       })
       .catch((e)=>{
 
