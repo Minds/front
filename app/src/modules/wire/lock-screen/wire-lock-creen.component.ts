6d7e650935967a577b7a1161e4050dd00fbbd2a0
Mark Harding <mark@kramnorth.com>
2017-08-08 16:23:05 +0100

    (feat): wire lock screen now has unlock buttom

--- app/src/modules/wire/lock-screen/wire-lock-creen.component.ts
+++ app/src/modules/wire/lock-screen/wire-lock-creen.component.ts
@@ -3,6 +3,7 @@ import {
   Output
 } from '@angular/core';
 import { Client } from '../../../services/api/client';
+import { Session } from '../../../services/session';
 
 @Component({
   moduleId: module.id,
@@ -12,21 +13,39 @@ import { Client } from '../../../services/api/client';
 })
 
 export class WireLockScreenComponent implements AfterViewInit {
+
   @Input() entity: any;
   @Output('entityChange') update: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(private client: Client, private cd: ChangeDetectorRef) {
+  inProgress: boolean = false;
+
+  constructor(private client: Client, public session: Session, private cd: ChangeDetectorRef) {
   }
 
   ngAfterViewInit() {
+  }
+
+  unlock() {
+    this.inProgress = true;
+    this.detectChanges();
+
     this.client.get('api/v1/wire/threshold/' + this.entity.guid)
       .then((response: any) => {
+        if (response.paywall) {
+          alert('You must send a wire first');
+        }
         if (response.hasOwnProperty('activity')) {
           this.update.next(response.activity);
           this.detectChanges();
         }
+        this.inProgress = false;
+        this.detectChanges();
       })
-      .catch(e => console.error('got error: ', e));
+      .catch(e => {
+        this.inProgress = false;
+        this.detectChanges();
+        console.error('got error: ', e);
+      });
   }
 
   private detectChanges() {
