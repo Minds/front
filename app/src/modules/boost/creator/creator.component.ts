90718a99fbfabe09b1086535ee233ad68cd95d25
Mark Harding <mark@kramnorth.com>
2017-08-21 15:31:00 +0100

    (fix): make default boost 1000

--- app/src/modules/boost/creator/creator.component.ts
+++ app/src/modules/boost/creator/creator.component.ts
@@ -40,7 +40,7 @@ export class BoostCreatorComponent implements AfterViewInit {
   object: any = {};
 
   boost: BoostStruc = {
-    amount: 2500,
+    amount: 1000,
     currency: 'points',
     type: null,
 
