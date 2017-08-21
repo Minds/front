4619586e5dc332fbe293ff27f442655afab158b9
Emi Balbuena <edgebal@gmail.com>
2017-08-21 15:29:29 -0300

    (feat): Show points on wallet Wire overview
    Conflicts:
    
    	app/src/modules/wire/console/console.component.ts

--- app/src/modules/wire/console/console.component.ts
+++ app/src/modules/wire/console/console.component.ts
@@ -1,7 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
-import { Client } from '../../../services/api';
-import { Session } from '../../../services/session';
-
+import { ChangeDetectionStrategy, Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'm-wire-console',
@@ -11,48 +8,9 @@ import { Session } from '../../../services/session';
 
 export class WireConsoleComponent {
 
-  ready: boolean = true;
-  stats: { sum, count, avg } = {
-    sum: 0,
-    count: 0,
-    avg: 0
-  };
-
   showOptions: boolean = false;
 
-  startDate: string;
-
-  constructor(private client: Client, private session: Session, private cd: ChangeDetectorRef) {
-    const d = new Date();
-    d.setMonth(d.getMonth() - 1);
-    this.startDate = d.toISOString();
-  }
-
-  ngOnInit() {
-    this.getStats();
-  }
-
-  getStats() {
-    this.client.get('api/v1/wire/sums/receiver/' + this.session.getLoggedInUser().guid + '/money',
-      {
-        advanced: true,
-        start: Date.parse(this.startDate) / 1000
-      })
-      .then(({ sum, count, avg }) => {
-        this.stats = {
-          sum: sum,
-          count: count,
-          avg: avg
-        };
-        this.detectChanges();
-      });
-  }
-
-  onStartDateChange(newDate) {
-    this.startDate = newDate;
-    this.detectChanges();
-    this.getStats();
-  }
+  constructor(private cd: ChangeDetectorRef) { }
 
   detectChanges() {
     this.cd.markForCheck();
--- app/src/modules/wire/wire.module.ts
+++ app/src/modules/wire/wire.module.ts
@@ -20,6 +20,7 @@ import { WireConsoleSettingsComponent } from './console/settings/settings.compon
 import { WireLockScreenComponent } from './lock-screen/wire-lock-screen.component';
 import { WireService } from './wire.service';
 import { WireMarketingComponent } from './marketing.component';
+import { WireConsoleOverviewComponent } from "./console/overview/overview.component";
 
 const wireRoutes : Routes = [
   { path: 'wire', component: WireMarketingComponent }
@@ -47,7 +48,8 @@ const wireRoutes : Routes = [
     WireConsoleLedgerComponent,
     WireConsoleSupporterComponent,
     WireConsoleSettingsComponent,
-    WireMarketingComponent
+    WireMarketingComponent,
+    WireConsoleOverviewComponent
   ],
   providers: [
     WireService
