0f3d5d9c0eee59c23c05cb04ba52f1413d385199
Marcelo <malrogf@gmail.com>
2017-09-07 09:47:58 -0300

    (fix): TinyMCE strips https:// part when embedding YouTube (or other videos) (#262)

--- app/src/common/components/editors/tinymce.ts
+++ app/src/common/components/editors/tinymce.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter } from '@angular/core';
 
 declare var tinymce;
 
+type Pattern = {regex: any, type:string, w:number, h:number, url: string, allowFullscreen: boolean};
+
 @Component({
   selector: 'minds-tinymce',
   inputs: [ '_content: content', 'reset' ],
@@ -19,9 +21,53 @@ export class MindsTinymce {
   update = new EventEmitter();
   timeout;
 
+  urlPatterns: Array<Pattern> = [
+    {
+      regex: /youtu\.be\/([\w\-.]+)/,
+      type: 'iframe', w: 560, h: 314,
+      url: 'https://www.youtube.com/embed/$1',
+      allowFullscreen: true
+    },
+    {
+      regex: /youtube\.com(.+)v=([^&]+)/,
+      type: 'iframe', w: 560, h: 314,
+      url: 'https://www.youtube.com/embed/$2',
+      allowFullscreen: true
+    },
+    {
+      regex: /youtube.com\/embed\/([a-z0-9\-_]+(?:\?.+)?)/i,
+      type: 'iframe', w: 560, h: 314,
+      url: 'https://www.youtube.com/embed/$1',
+      allowFullscreen: true
+    },
+    {
+      regex: /vimeo\.com\/([0-9]+)/,
+      type: 'iframe', w: 425, h: 350,
+      url: 'https://player.vimeo.com/video/$1?title=0&byline=0&portrait=0&color=8dc7dc',
+      allowFullscreen: true
+    },
+    {
+      regex: /vimeo\.com\/(.*)\/([0-9]+)/,
+      type: "iframe", w: 425, h: 350,
+      url: "https://player.vimeo.com/video/$2?title=0&amp;byline=0",
+      allowFullscreen: true
+    },
+    {
+      regex: /maps\.google\.([a-z]{2,3})\/maps\/(.+)msid=(.+)/,
+      type: 'iframe', w: 425, h: 350,
+      url: 'https://maps.google.com/maps/ms?msid=$2&output=embed"',
+      allowFullscreen: false
+    },
+    {
+      regex: /dailymotion\.com\/video\/([^_]+)/,
+      type: 'iframe', w: 480, h: 270,
+      url: 'https://www.dailymotion.com/embed/video/$1',
+      allowFullscreen: true
+    }
+  ];
+
 
   ngOnInit(){
-    var self = this;
     tinymce.init({
       selector:'minds-tinymce > textarea',
       autoresize_max_height: '400',
@@ -32,6 +78,37 @@ export class MindsTinymce {
       statusbar: false,
       relative_urls: false,
       remove_script_host: false,
+      media_url_resolver: (data, resolve) => {
+        for (let i: number = 0; i < this.urlPatterns.length; ++i) {
+          const pattern: Pattern = this.urlPatterns[ i ];
+          const match = pattern.regex.exec(data.url);
+          let url;
+
+          if (match) {
+            url = pattern.url;
+
+            for (i = 0; match[ i ]; i++) {
+              url = url.replace('$' + i, function () {
+                return match[ i ];
+              });
+            }
+
+            data.source1 = url;
+            data.type = pattern.type;
+            data.allowFullscreen = pattern.allowFullscreen;
+            data.width = data.width || pattern.w;
+            data.height = data.height || pattern.h;
+          }
+        }
+        let html: string = '';
+        if (data.type === 'iframe') {
+          html +=
+            `<iframe src="${data.source1}" width="${data.width}" height="${data.height}"
+            fullscreen="${data.allowFullscreen}"></iframe>`;
+
+        }
+        resolve({'html': html});
+      },
       plugins: [
 	         "advlist autolink link image lists preview hr anchor pagebreak",
 	         "media nonbreaking code",
@@ -45,13 +122,13 @@ export class MindsTinymce {
           this.ready = true;
           this.content = ed.getContent();
           this.update.next(ed.getContent());
-        })
+        });
 
         ed.on('keyup', (e) => {
           this.ready = true;
           this.content = ed.getContent();
           this.update.next(ed.getContent());
-        })
+        });
 
       }
     });
