cb6fb0e2b9d6bdc140e5d465df1c417589219b91
Marcelo <malrogf@gmail.com>
2018-01-11 14:34:24 -0300

    (feat): add header to FAQ (#492)

--- app/src/modules/faq/faq.page.ts
+++ app/src/modules/faq/faq.page.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Client } from '../../common/api/client.service';
 
-import { FaqService } from './faq.service'; 
+import { FaqService } from './faq.service';
+import { MindsTitle } from '../../services/ux/title';
 
 @Component({
   selector: 'm-faq--page',
@@ -13,7 +14,10 @@ export class FaqPage {
   constructor(
     private client: Client,
     public service: FaqService,
-  ) { }
+    public title: MindsTitle,
+  ) {
+    this.title.setTitle('FAQ');
+  }
 
   ngOnInit() {
   }
