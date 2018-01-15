1882a6a36127f6b99e1bf6d335a20956b6ddad31
narc88 <narc88@gmail.com>
2018-01-15 11:52:23 -0200

    (feat) admin menu and submenu (#501)
    (bug) broken translation

--- app/src/controllers/admin/admin.ts
+++ app/src/controllers/admin/admin.ts
@@ -8,18 +8,7 @@ import { Session, SessionFactory } from '../../services/session';
 
 @Component({
   selector: 'minds-admin',
-  template: `
-    <minds-admin-analytics *ngIf="filter == 'analytics'"></minds-admin-analytics>
-    <minds-admin-boosts *ngIf="filter == 'boosts'"></minds-admin-boosts>
-    <minds-admin-pages *ngIf="filter == 'pages'"></minds-admin-pages>
-    <minds-admin-reports *ngIf="filter == 'reports' || filter == 'appeals'"></minds-admin-reports>
-    <minds-admin-monetization *ngIf="filter == 'monetization'"></minds-admin-monetization>
-    <minds-admin-programs *ngIf="filter == 'programs'"></minds-admin-programs>
-    <minds-admin-payouts *ngIf="filter == 'payouts'"></minds-admin-payouts>
-    <minds-admin-featured *ngIf="filter == 'featured'"></minds-admin-featured>
-    <minds-admin-tagcloud *ngIf="filter == 'tagcloud'"></minds-admin-tagcloud>
-    <m-admin--verify *ngIf="filter == 'verify'"></m-admin--verify>
-  `
+  templateUrl: 'admin.html'
 })
 
 export class Admin {
