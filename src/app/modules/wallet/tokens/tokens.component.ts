aff84e7c8a124f51abda64649771107e61ae5e9e
Marcelo <malrogf@gmail.com>
2018-03-19 16:21:54 -0300

    (chore): only show wallet onboarding on transactions tabs (#666)

--- src/app/modules/wallet/tokens/tokens.component.ts
+++ src/app/modules/wallet/tokens/tokens.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'm-wallet--tokens',
   templateUrl: 'tokens.component.html'
 })
-export class WalletTokensComponent { }
+export class WalletTokensComponent {
+  showOnboarding: boolean = false;
+
+  constructor(route: ActivatedRoute) {
+    route.url.subscribe(() => {
+      this.showOnboarding = route.snapshot.firstChild && route.snapshot.firstChild.routeConfig.path === 'transactions';
+    });
+  }
+}
