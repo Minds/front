046cc442c8dcaa456be5a00a9ae6722d7c694f04
Mark Harding <mark@kramnorth.com>
2018-05-15 10:20:15 -0400

    Revert "(fix): tags get triggered when inputting an email (#833)"
    This reverts commit 360b461064cbe4b16ea8ca2f7915d19ce746ea97.

--- src/app/common/pipes/tags.ts
+++ src/app/common/pipes/tags.ts
@@ -15,16 +15,12 @@ export class TagsPipe {
     var url = /(\b(https?|ftp|file):\/\/[^\s\]\)]+)/gim;
     value = value.replace(url, '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>');
 
-    //<a>tag
-    var mail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gim;
-    value = value.replace(mail, '<a href="mailto:$&" target="_blank" rel="noopener noreferrer">$&</a>');
-
     //#hashtag
     var hash = /(^|\s||)#(\w+)/gim;
     value = value.replace(hash, '$1<a href="/search;q=%23$2;ref=hashtag" target="_blank">#$2</a>');
 
     //@tag
-    var at = /(^|\s)\@(\w*[a-zA-Z_]+\w*)/gim;
+    var at = /(^|\s||)\@(\w*[a-zA-Z_]+\w*)/gim;
     value = value.replace(at, '$1<a class="tag" href="/$2" target="_blank">@$2</a>');
 
     return value;
