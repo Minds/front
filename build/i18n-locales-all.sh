7556691b86730623e1306a414cfaf43bf657d009
Mark Harding <mark@kramnorth.com>
2018-07-09 21:02:17 +0100

    (fix): use relative path

--- build/i18n-locales-all.sh
+++ build/i18n-locales-all.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 do_locale_build () {
     ng build --prod --vc --output-path="$2/$1/" --deploy-url="$3/$1/" --build-optimizer=false \
-        --i18nFile="$DIR/../src/locale/Minds.$1.xliff" --i18nFormat=xlf --locale="$1"
+        --i18nFile="./src/locale/Minds.$1.xliff" --i18nFormat=xlf --locale="$1"
 }
 
 do_locale_build vi $1 $2
