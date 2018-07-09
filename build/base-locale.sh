b320bc18c8248ce516ac88b43d925e75d4c2f30d
Mark Harding <mark@kramnorth.com>
2018-07-09 20:24:47 +0100

    (fix): use sh not bash

--- build/base-locale.sh
+++ build/base-locale.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 ng build --prod --vc --output-path="$1/en/" --deploy-url="$2/en/" --build-optimizer=false
--- build/i18n-locales-all.sh
+++ build/i18n-locales-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 do_locale_build () {
