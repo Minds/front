0ddd79f5e9e4355e501dc45b70241662006d5045
Mark Harding <mark@marksmusiconline.co.uk>
2018-08-06 09:27:29 +0000

    (feat): add video and blogs

--- src/app/modules/blockchain/marketing/marketing.module.ts
+++ src/app/modules/blockchain/marketing/marketing.module.ts
@@ -4,12 +4,15 @@ import { CommonModule as NgCommonModule } from '@angular/common';
 
 import { CommonModule } from '../../../common/common.module';
 import { FaqModule } from '../../faq/faq.module';
+import { VideoModule } from '../../media/components/video/video.module';
 import { MarketingModule } from '../../marketing/marketing.module';
 import { OnboardingModule } from '../../onboarding/onboarding.module';
+import { BlogModule } from '../../blogs/blog.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BlockchainMarketingComponent } from './marketing.component';
 import { BlockchainPurchaseComponent } from './purchase.component';
 import { BlockchainMarketingOnboardComponent } from './onboard.component';
+import { BlockchainMarketingBlogsComponent } from './blogs.component';
 import { ModalsModule } from '../../modals/modals.module';
 
 const routes: Routes = [
@@ -33,11 +36,14 @@ const routes: Routes = [
     FaqModule,
     MarketingModule,
     ModalsModule,
+    BlogModule,
+    VideoModule,
   ],
   declarations: [
     BlockchainMarketingComponent,
     BlockchainMarketingOnboardComponent,
     BlockchainPurchaseComponent,
+    BlockchainMarketingBlogsComponent,
   ],
   exports: [
     BlockchainMarketingComponent,
