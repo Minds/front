dc8af23bfcebe94a8cbc5c2cc7d267ef18831501
Mark Harding <mark@marksmusiconline.co.uk>
2018-08-23 10:41:12 +0000

    (feat): delete your channel

--- src/app/modules/settings/disable/disable.component.ts
+++ src/app/modules/settings/disable/disable.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Client } from '../../../services/api';
-
+import { OverlayModalService } from '../../../services/ux/overlay-modal';
+import { ConfirmPasswordModalComponent } from '../../modals/confirm-password/modal.component';
 
 @Component({
   moduleId: module.id,
@@ -18,7 +19,7 @@ export class SettingsDisableChannelComponent {
   settings: string;
   object: any;
 
-  constructor(public client: Client, public router: Router) {
+  constructor(public client: Client, public router: Router, private overlayModal: OverlayModalService) {
     this.minds = window.Minds;
   }
 
@@ -33,15 +34,22 @@ export class SettingsDisableChannelComponent {
   }
 
   delete() {
-    if (confirm('Your account and all data related to it will be deleted permanently. Are you sure you want to proceed?')) {
-      this.client.delete('api/v1/channel/delete')
-        .then((response: any) => {
-          this.router.navigate(['/logout']);
-        })
-        .catch((e: any) => {
-          alert('Sorry, we could not delete your account');
-        });
+    if (!confirm('Your account and all data related to it will be deleted permanently. Are you sure you want to proceed?')) {
+      return;
     }
+    const creator = this.overlayModal.create(ConfirmPasswordModalComponent, {}, {
+      class: 'm-overlay-modal--small',
+      onComplete: ({ password }) => {
+        this.client.post('api/v2/settings/delete', { password })
+          .then((response: any) => {
+            this.router.navigate(['/logout']);
+          })
+          .catch((e: any) => {
+            alert('Sorry, we could not delete your account');
+          });
+      }
+    });
+    creator.present();
   }
 
 }
