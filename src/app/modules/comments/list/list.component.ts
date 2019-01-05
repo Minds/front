da93381957e59888ff65f1e70ea5403d1b48b9d0
Joseph Hammer <j@fivebits.us>
2019-01-04 23:40:32 -0800

    feat: change offset to token. Offset now start point.

--- src/app/modules/comments/list/list.component.ts
+++ src/app/modules/comments/list/list.component.ts
@@ -124,7 +124,7 @@ export class CommentsListComponent {
 
     this.client.get('api/v1/comments/' + this.guid + '/' + this.parentGuid, { 
       limit: refresh ? 5 : this.limit, 
-      offset: this.offset, 
+      token: this.offset,
       reversed: false 
     })
       .then((response: any) => {
