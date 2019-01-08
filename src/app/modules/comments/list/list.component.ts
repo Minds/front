8e8cd9e15300fc884868ffca3cef2ff101e5d287
Mark Harding <mark@marksmusiconline.co.uk>
2019-01-08 18:08:26 +0000

    (chore): minor style changes to comments

--- src/app/modules/comments/list/list.component.ts
+++ src/app/modules/comments/list/list.component.ts
@@ -124,7 +124,7 @@ export class CommentsListComponent {
 
     this.client.get('api/v1/comments/' + this.guid + '/' + this.parentGuid, { 
       limit: refresh ? 5 : this.limit, 
-      token: this.offset,
+      offset: this.offset,
       reversed: false 
     })
       .then((response: any) => {
