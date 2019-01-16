55a5f5e9201ea6fc55bdd7d63769b4aae1756362
Joseph Hammer <j@fivebits.us>
2019-01-16 01:09:19 -0800

    bug: str.Replace modified to use regex capture groups

--- src/app/common/pipes/tags.ts
+++ src/app/common/pipes/tags.ts
@@ -7,9 +7,7 @@ import { Pipe, Inject, PipeTransform } from '@angular/core';
 /**
  * Tags pipe
  */
-export class TagsPipe implements PipeTransform  {
-
-  results = [];
+export class TagsPipe implements PipeTransform {
 
   /**
    * Tags
@@ -17,79 +15,37 @@ export class TagsPipe implements PipeTransform  {
   tags = {
     url: {
       rule: /(\b(https?|ftp|file):\/\/[^\s\]]+)/gim,
-      replace: (m) => {
-        return `<a href="${m.match[1]}" target="_blank" rel="noopener noreferrer">${m.match[1]}</a>`;
-      }
+      template: '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
     },
     mail: {
-      rule: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gim,
-      replace: (m) => {
-        return `<a href="mailto:${m.match[0]}" target="_blank" rel="noopener noreferrer">${m.match[0]}</a>`;
-      }
+      rule: /([a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)/gim,
+      template: '<a href="mailto:$1" target="_blank" rel="noopener noreferrer">$1</a>'
     },
     hash: {
       rule: /(^|\s||)#(\w+)/gim,
-      replace: (m) => {
-        return  `${m.match[1]}<a href="/newsfeed/tag/${m.match[2]};ref=hashtag">#${m.match[2]}</a>`;
-      }
+      template: '$1<a href="/newsfeed/tag/$2;ref=hashtag">#$2</a>'
     },
     at: {
       rule: /(^|\s|\W)@(\w*[a-zA-Z_-]+\w*)/gim,
-      replace: (m) => {
-        return `${m.match[1]}<a class="tag" href="/${m.match[2]}" target="_blank">@${m.match[2]}</a>`;
-      }
+      template: '$1<a class="tag" href="/$2" target="_blank">@$2</a>'
     }
   };
 
   /**
-   * Push a match to results array
-   * @param match
-   */
-  push(match: any) {
-    // ignore match inside others
-    if (this.results.findIndex(m => match.start >= m.start && match.end <= m.end) !== -1) {
-      return;
-    }
-    this.results.push(match);
-  }
-
-  /**
-   * Parse tags
-   * @param tag
-   * @param value
-   */
-  parse(tag: string, value: string) {
-    let match;
-    while ((match = this.tags[tag].rule.exec(value)) !== null) {
-      this.push({
-        type: tag,
-        start: match.index,
-        end: match.index + match[0].length,
-        match: match
-      });
-    }
-  }
-
-  /**
    * Replace tags
    * @param str
+   * @param tagName
    */
-  replace(str) {
-    this.results.forEach(m => {
-      str = str.replace(m.match[0], this.tags[m.type].replace(m, str));
-    });
-
-    return str;
+  replaceTokens(str, tagName) {
+    let tag = this.tags[tagName];
+    return str.replace(tag.rule, tag.template);
   }
 
   transform(value: string): string {
-    this.results = [];
-    this.parse('url', value);
-    this.parse('mail', value);
-    this.parse('hash', value);
-    this.parse('at', value);
-
-    return this.replace(value);
+    value = this.replaceTokens(value, 'url');
+    value = this.replaceTokens(value, 'mail',);
+    value = this.replaceTokens(value, 'hash');
+    value = this.replaceTokens(value, 'at');
+    return value;
   }
-
 }
