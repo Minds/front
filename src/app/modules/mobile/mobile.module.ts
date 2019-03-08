3b2e1582141fbb2c32c7eb11fd6b1bf1026cb307
Emiliano Balbuena <edgebal@gmail.com>
2019-03-08 11:48:36 +0000

    (feat): Mobile page should read APK info from JSON (#411)

--- src/app/modules/mobile/mobile.module.ts
+++ src/app/modules/mobile/mobile.module.ts
@@ -4,6 +4,9 @@ import { MobileMarketingComponent } from './marketing/marketing.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule as NgCommonModule } from '@angular/common';
 import { CommonModule } from '../../common/common.module';
+import { MobileService } from "./mobile.service";
+import { HttpClient } from "@angular/common/http";
+import { FeaturesService } from "../../services/features.service";
 
 const routes: Routes = [
   {
@@ -26,6 +29,13 @@ const routes: Routes = [
   ],
   entryComponents: [
     MobileMarketingComponent,
+  ],
+  providers: [
+    {
+      provide: MobileService,
+      useFactory: MobileService._,
+      deps: [ HttpClient, FeaturesService ]
+    }
   ]
 })
 
