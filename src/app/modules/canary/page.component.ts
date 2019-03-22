8ac9e8c5e15ef531805c816757eca208f381fcdc
Marcelo Rivera <malrogf@gmail.com>
2019-03-22 10:23:01 -0300

    (fix): reload site when toggling canary

--- src/app/modules/canary/page.component.ts
+++ src/app/modules/canary/page.component.ts
@@ -40,12 +40,15 @@ export class CanaryPageComponent {
       return this.router.navigate(['/login']);
       
     this.user.canary = true;
-    this.client.put('api/v2/canary');
+    await this.client.put('api/v2/canary');
+    window.location.reload();
   }
 
-  turnOff() {
+  async turnOff() {
     this.user.canary = false;
-    this.client.delete('api/v2/canary');
+    await this.client.delete('api/v2/canary');
+
+    window.location.reload();
   }
 
 }
