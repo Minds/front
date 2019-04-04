b8f5c9c79efc398954ddcaad573f68f990c81af3
Emiliano Balbuena <edgebal@gmail.com>
2019-04-04 07:29:44 +0000

    (fix): Notifications toaster not showing on new layout (#945)

--- src/app/common/layout/v2-topbar/v2-topbar.component.ts
+++ src/app/common/layout/v2-topbar/v2-topbar.component.ts
@@ -1,5 +1,14 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewChild
+} from "@angular/core";
 import { Session } from "../../../services/session";
+import { DynamicHostDirective } from "../../directives/dynamic-host.directive";
+import { NotificationsToasterComponent } from "../../../modules/notifications/toaster.component";
 
 @Component({
   selector: 'm-v2-topbar',
@@ -9,18 +18,35 @@ import { Session } from "../../../services/session";
 export class V2TopbarComponent implements OnInit {
   minds = window.Minds;
 
+  @ViewChild(DynamicHostDirective) notificationsToasterHost: DynamicHostDirective;
+
+  componentRef;
+  componentInstance: NotificationsToasterComponent;
+
   constructor(
     protected session: Session,
-    protected cd: ChangeDetectorRef
+    protected cd: ChangeDetectorRef,
+    protected componentFactoryResolver: ComponentFactoryResolver
   ) {
   }
 
   ngOnInit() {
+    this.loadComponent();
     this.session.isLoggedIn(() => this.detectChanges());
   }
 
   getCurrentUser() {
     return this.session.getLoggedInUser();
+  }
+
+  loadComponent() {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(NotificationsToasterComponent),
+      viewContainerRef = this.notificationsToasterHost.viewContainerRef;
+
+    viewContainerRef.clear();
+
+    this.componentRef = viewContainerRef.createComponent(componentFactory);
+    this.componentInstance = this.componentRef.instance;
   }
 
   detectChanges() {
