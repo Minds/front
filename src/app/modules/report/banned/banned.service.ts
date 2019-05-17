41f27df211d09c3730edff7e42bdc7b277566cf8
Mark Harding <mark@marksmusiconline.co.uk>
2019-05-17 16:02:47 +0000

    (fix): do not allow banned page to be show to banned users

--- src/app/modules/report/banned/banned.service.ts
+++ src/app/modules/report/banned/banned.service.ts
@@ -8,9 +8,15 @@ export class BannedService {
   constructor(private router: Router, private session: Session) {
     this.router.events.subscribe((navigationState) => {
       if (navigationState instanceof NavigationStart) {
+        // Forward to moderation banned page if banned
         if (this.session.getLoggedInUser().banned === 'yes'
          && navigationState.url != '/moderation/banned') {
           this.router.navigate(['/moderation/banned']);
+        }
+        // Do not allow access to banned page if not banned
+        if (this.session.getLoggedInUser().banned !== 'yes'
+          && navigationState.url === '/moderation/banned') {
+          this.router.navigate(['/newsfeed']);
         }
       }
     });
