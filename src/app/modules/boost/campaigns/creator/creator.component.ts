d7efde50451aa42a46e8de4c0abbb22bd99fdb4b
Emiliano Balbuena <edgebal@gmail.com>
2019-06-13 19:25:33 -0300

    (feat): Edit screen logic

--- src/app/modules/boost/campaigns/creator/creator.component.ts
+++ src/app/modules/boost/campaigns/creator/creator.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
 import { Campaign } from "../campaigns.type";
 import { CampaignsService } from '../campaigns.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   providers: [CampaignsService],
@@ -8,19 +10,66 @@ import { CampaignsService } from '../campaigns.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'creator.component.html',
 })
-export class BoostCampaignsCreatorComponent implements OnInit {
+export class BoostCampaignsCreatorComponent implements OnInit, OnDestroy {
+
+  isEditing: boolean = false;
+
+  urn: string;
 
   campaign: Campaign;
 
   inProgress: boolean = false;
 
+  protected route$: Subscription;
+
   constructor(
     protected service: CampaignsService,
+    protected route: ActivatedRoute,
+    protected cd: ChangeDetectorRef,
   ) {
   }
 
   ngOnInit() {
     this.reset();
+
+    this.route$ = this.route.params.subscribe(params => {
+      if (params.urn && this.urn !== params.urn) {
+        this.urn = params.urn;
+        this.load();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.route$.unsubscribe();
+  }
+
+  async load() {
+    if (!this.urn) {
+      this.isEditing = false;
+      this.reset();
+      this.detectChanges();
+      return;
+    }
+
+    this.isEditing = true;
+    this.campaign = void 0;
+    this.inProgress = true;
+    this.detectChanges();
+
+    try {
+      const campaign = await this.service.get(this.urn);
+
+      this.campaign = campaign;
+    } catch (e) {
+      console.error('BoostCampaignsCreatorComponent', e);
+      // TODO: Add error UX
+      this.campaign = void 0;
+    }
+
+    this.inProgress = false;
+
+    this.detectChanges();
   }
 
   reset() {
@@ -44,7 +93,8 @@ export class BoostCampaignsCreatorComponent implements OnInit {
   }
 
   canSubmit() {
-    return this.campaign.name &&
+    return this.campaign &&
+      this.campaign.name &&
       this.campaign.type &&
       this.campaign.budget &&
       this.campaign.budget > 0 &&
@@ -58,5 +108,10 @@ export class BoostCampaignsCreatorComponent implements OnInit {
 
   get campaignTypes() {
     return this.service.getCampaignTypes();
+  }
+
+  detectChanges() {
+    this.cd.markForCheck();
+    this.cd.detectChanges();
   }
 }
