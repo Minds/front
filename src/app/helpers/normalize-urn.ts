ec6a1ad8a2925e195bcb313baa7ea6857f17baef
Emiliano Balbuena <edgebal@gmail.com>
2019-07-03 19:56:15 -0300

    (test): Fix Boost Creator tests

--- src/app/helpers/normalize-urn.ts
+++ src/app/helpers/normalize-urn.ts
@@ -1,5 +1,8 @@
 export default function normalizeUrn(urnOrGuid: string) {
-  if (!isNaN(<any> urnOrGuid)) {
+  if (!urnOrGuid) {
+    console.warn(`Invalid empty URN`);
+    return '';
+  } else if (!isNaN(<any> urnOrGuid)) {
     urnOrGuid = `urn:entity:${urnOrGuid}`;
   } else if (urnOrGuid.indexOf('urn:') !== 0) {
     console.warn(`Invalid URN: ${urnOrGuid}`);
