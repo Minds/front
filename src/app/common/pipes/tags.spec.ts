792bf3debde6b6e29559afeb79c283abdcf38482
Marcelo Rivera <malrogf@gmail.com>
2019-07-26 08:01:35 -0300

    (fix): removed fit

--- src/app/common/pipes/tags.spec.ts
+++ src/app/common/pipes/tags.spec.ts
@@ -94,7 +94,7 @@ describe('TagPipe', () => {
     expect(transformedString).toEqual('<a class="tag" href="/test1" target="_blank">@test1</a> <a class="tag" href="/test2" target="_blank">@test2</a>');
   });
 
-  fit('should transform many adjacent tags', () => {
+  it('should transform many adjacent tags', () => {
     const pipe = new TagsPipe(featuresServiceMock);
     const string = '@test1 @test2 @test3 @test4 @test5 @test6 @test7 @test8 @test9 @test10 @test11 @test12 @test13 @test14 @test15';
     console.log(string);
