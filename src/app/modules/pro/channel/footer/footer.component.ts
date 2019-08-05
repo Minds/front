03d2e29414a44a80d0f659a949085e6b5878b1c4
Marcelo Rivera <malrogf@gmail.com>
2019-08-05 09:28:00 -0300

    (feat): footer

--- src/app/modules/pro/channel/footer/footer.component.ts
+++ src/app/modules/pro/channel/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProChannelService } from "../channel.service";
 
 @Component({
   selector: 'm-pro--channel-footer',
@@ -7,74 +8,13 @@ import { Component } from '@angular/core';
 
 export class ProChannelFooterComponent {
 
-  links:any;
-  rowCount: number;
-
-  constructor(){
-    this.links = [
-      {
-        text: 'What we are',
-        src: ''
-      },
-      {
-        text: 'Core Values',
-        src: ''
-      },
-      {
-        text: 'Subscribe',
-        src: ''
-      },
-      {
-        text: 'Support',
-        src: ''
-      },
-      {
-        text: 'Careers',
-        src: ''
-      },
-      {
-        text: 'Events',
-        src: ''
-      },
-      {
-        text: 'Contact',
-        src: ''
-      },
-      {
-        text: 'Contribute',
-        src: ''
-      },
-      {
-        text: 'Investors',
-        src: ''
-      }
-    ];
-
-    this.setLinksToRows();
+  get footerLinks() {
+    return this.channelService.currentChannel.pro_settings.footer_links;
   }
 
-  /**
-   * Distribute links in cols and rows
-   */
-  setLinksToRows() {
-    this.getRowsCount();
-    const links = this.links;
-    this.links = [];
-    let i,j, chunk = this.rowCount;
-    for (i=0,j=links.length; i<j; i+=chunk) {
-      this.links.push(links.slice(i,i+chunk));
-    }
-  }
+  constructor(
+    private channelService: ProChannelService,
+  ) {
 
-  getRowsCount() {
-    const linksCount = this.links.length;
-    let rowMultiplier = 6
-    let rowCount = 1;
-
-    while (((rowMultiplier * rowCount) % linksCount) > 0) {
-      rowCount++;
-    }
-
-    this.rowCount = rowCount;
   }
 }
