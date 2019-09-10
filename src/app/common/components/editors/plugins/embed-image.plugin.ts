066fa68f305c40de1cfb071f83f7b946591ca0b2
Emiliano Balbuena <edgebal@gmail.com>
2019-09-10 15:54:57 +0000

    [Sprint/ModestMonkey] (fix): Avoid Cannot read property 'parentElement' of null (#1779)

--- src/app/common/components/editors/plugins/embed-image.plugin.ts
+++ src/app/common/components/editors/plugins/embed-image.plugin.ts
@@ -197,6 +197,10 @@ export class EmbedImage {
           '.' + imgClass
         );
 
+        if (!image) {
+          return;
+        }
+
         const overlay = image.parentElement.querySelector(
           '.m-blog--image--in-progress-overlay'
         );
