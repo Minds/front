d6fad37e4a5c32f01a41037e37bf24b1a9c0079f
Marcelo Rivera <malrogf@gmail.com>
2019-10-02 12:51:35 -0300

    (fix): update PermissionsService based on new permissions export

--- src/app/common/services/permissions.service.ts
+++ src/app/common/services/permissions.service.ts
@@ -7,8 +7,11 @@ export class PermissionsService {
   canInteract(entity: any, permission: string) {
     let permissions: Permissions = entity.permissions;
 
+    if (!permissions) {
+      return false;
+    }
+
     return (
-      permissions &&
       permissions.permissions.findIndex(item => item === permission) !== -1
     );
   }
