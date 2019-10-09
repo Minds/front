26a8afe0c80cfedffd1e157b7f60d9441c226f23
Olivia Madrid <oliviajmadrid@gmail.com>
2019-10-09 14:20:35 -0600

    (feat): analytics v2 - testing prettyquick error

--- src/app/modules/analytics/v2/components/filter/filter.component.ts
+++ src/app/modules/analytics/v2/components/filter/filter.component.ts
@@ -32,6 +32,7 @@ import isMobileOrTablet from '../../../../../helpers/is-mobile-or-tablet';
 export class AnalyticsFilterComponent implements OnInit, OnDestroy {
   // TODO: extend Filter interface to allow additional fields (like for timespans?)
   @Input() filter: Filter;
+  @Input() dropup: boolean = false;
 
   isMobile: boolean;
   expanded = false;
