3612bb8f18c241aad3df4174a58b49e65fd22748
Olivia Madrid <olivia@minds.com>
2019-10-15 12:22:31 +0000

    Entity centric metrics (analytics v2)

---
+++ src/app/modules/analytics/v2/chart-palette.default.ts
@@ -0,0 +1,52 @@
+const chartPalette: Array<any> = [
+  {
+    id: 'm-white',
+    themeMap: ['#fff', '#161616'],
+  },
+  {
+    id: 'm-transparent',
+    themeMap: ['rgba(0,0,0,0)', 'rgba(0,0,0,0)'],
+  },
+  {
+    id: 'm-grey-50',
+    themeMap: ['rgba(232,232,232,1)', 'rgba(53,53,53,1)'],
+  },
+  {
+    id: 'm-grey-70',
+    themeMap: ['#eee', '#333'],
+  },
+  {
+    id: 'm-grey-130',
+    themeMap: ['#ccc', '#555'],
+  },
+  {
+    id: 'm-grey-160',
+    themeMap: ['#bbb', '#555'],
+  },
+  {
+    id: 'm-grey-300',
+    themeMap: ['#999', '#666'],
+  },
+  {
+    id: 'm-blue',
+    themeMap: ['#4690df', '#44aaff'],
+  },
+  {
+    id: 'm-red-dark',
+    themeMap: ['#c62828', '#e57373'],
+  },
+  {
+    id: 'm-amber-dark',
+    themeMap: ['#ffa000', '#ffecb3'],
+  },
+  {
+    id: 'm-green-dark',
+    themeMap: ['#388e3c', '#8bc34a'],
+  },
+  {
+    id: 'm-blue-grey-500',
+    themeMap: ['#607d8b', '#607d8b'],
+  },
+];
+
+export default chartPalette;
---
+++ src/app/modules/analytics/v2/components/filters/filters.component.ts
@@ -0,0 +1,41 @@
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import {
+  AnalyticsDashboardService,
+  Category,
+  Response,
+  Dashboard,
+  Filter,
+  Option,
+  Metric,
+  Summary,
+  Visualisation,
+  Bucket,
+  Timespan,
+  UserState,
+} from '../../dashboard.service';
+
+@Component({
+  selector: 'm-analytics__filters',
+  templateUrl: './filters.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AnalyticsFiltersComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+
+  filters$ = this.analyticsService.filters$;
+
+  constructor(private analyticsService: AnalyticsDashboardService) {}
+
+  ngOnInit() {
+    // TODO: remove subscription because everything is happening in html
+    // TODO: might even be fine to just get rid of this component and put it in dashboard.ts
+  }
+
+  ngOnDestroy() {}
+}
