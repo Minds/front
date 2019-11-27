51193ccfac7d0b41cecb9af0430696620b7f4d4f
Mark Harding <mark@kramnorth.com>
2019-11-27 09:49:10 +0000

    (fix): failing spec tests

--- src/app/modules/wire/creator/creator.component.ts
+++ src/app/modules/wire/creator/creator.component.ts
@@ -36,7 +36,7 @@ export interface WireStruc {
   payloadType: PayloadType | null;
   guid: any;
   recurring: boolean;
-  recurringInterval: 'once' | 'monthly' | 'yearly' | null;
+  recurringInterval?: 'once' | 'monthly' | 'yearly' | null;
   payload: any;
 }
 
