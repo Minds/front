b045911f1b83d48a34950aae0f312d0ec97a59fe
Olivia Madrid <oliviajmadrid@gmail.com>
2020-01-03 15:03:20 -0700

    WIP so I can switch branches for buddy review

--- src/app/modules/wallet/v2/modal/modal.component.ts
+++ src/app/modules/wallet/v2/modal/modal.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
   Output,
+  Input,
   EventEmitter,
   AfterViewInit,
   OnDestroy,
+  HostListener,
 } from '@angular/core';
 
 @Component({
@@ -11,15 +13,26 @@ import {
   templateUrl: './modal.component.html',
 })
 export class WalletModalComponent implements AfterViewInit, OnDestroy {
+  public _showModal;
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
+  // showModal: boolean = false;
+  // @Input() showModal = false;
+  @Input()
+  public set showModal(val: boolean) {
+    this._showModal = val;
+    console.log('showmodal input', val);
+    if (val) {
+      this.show();
+    }
+  }
+
   // root;
   constructor() {}
 
   ngAfterViewInit() {
-    if (document && document.body) {
-      document.body.classList.add('m-overlay-modal--shown--no-scroll');
-    }
-
+    // if (document && document.body) {
+    //   document.body.classList.add('m-overlay-modal--shown--no-scroll');
+    // }
     // if (!this.root && document && document.body) {
     //   this.root = document.body;
     // }
@@ -30,6 +43,30 @@ export class WalletModalComponent implements AfterViewInit, OnDestroy {
   }
   ngOnDestroy() {
     document.body.classList.remove('m-overlay-modal--shown--no-scroll');
+  }
+
+  show() {
+    console.log('show');
+    if (document && document.body) {
+      document.body.classList.add('m-overlay-modal--shown--no-scroll');
+    }
+  }
+
+  // * MODAL DISMISSAL * --------------------------------------------------------------------------
+  // Dismiss modal when backdrop is clicked and modal is open
+  @HostListener('document:click', ['$event'])
+  clickedBackdrop($event) {
+    console.log('clickedbackdrop');
+    if (this.showModal) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      this.clickedClose();
+    }
+  }
+
+  // Don't dismiss modal if click somewhere other than backdrop
+  clickedModal($event) {
+    $event.stopPropagation();
   }
 
   clickedClose() {
