1a8e63d39ddfb680c469e6023c6ad158e0c0ca4a
Jim Toth <jim@meme.coach>
2020-01-27 15:15:13 -0500

    Remove code-highlight.directive

--- src/app/modules/code-highlight/code-highlight.module.ts
+++ src/app/modules/code-highlight/code-highlight.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { CodeHighlightService } from './code-highlight.service';
 import { CodeHighlightPipe } from './code-highlight.pipe';
-import { CodeHighlightDirective } from './code-highlight.directive';
 
 @NgModule({
-  declarations: [CodeHighlightPipe, CodeHighlightDirective],
-  exports: [CodeHighlightPipe, CodeHighlightDirective],
+  declarations: [CodeHighlightPipe],
+  exports: [CodeHighlightPipe],
   providers: [CodeHighlightService],
 })
 export class CodeHighlightModule {}
