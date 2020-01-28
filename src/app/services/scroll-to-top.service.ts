6f71e69cd78b8884cf524da3c9a560a8f79ff88d
Olivia Madrid <oliviajmadrid@gmail.com>
2020-01-27 23:17:40 -0700

    disable automatic scroll-to-top on wallet pages

--- src/app/services/scroll-to-top.service.ts
+++ src/app/services/scroll-to-top.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from '@angular/core';
-import {
-  Router,
-  ActivatedRoute,
-  Event,
-  NavigationEnd,
-  NavigationStart,
-} from '@angular/router';
+import { Router, ActivatedRoute, RoutesRecognized } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { filter, pairwise } from 'rxjs/operators';
 
 @Injectable()
 export class ScrollToTopService {
@@ -19,20 +14,24 @@ export class ScrollToTopService {
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   listen(): this {
-    this._routerListener = this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationStart) {
-        console.log('urlstart', event.url);
-        console.log('navEventStart', event);
-      }
-      if (event instanceof NavigationEnd) {
-        console.log('urlend', event.url);
-        console.log('navEventEnd', event);
+    this._routerListener = this.router.events
+      .pipe(
+        filter((e: any) => e instanceof RoutesRecognized),
+        pairwise()
+      )
+      .subscribe((events: RoutesRecognized[]) => {
+        // TODOOJM change to wallet
+        const disabledPaths: Array<string> = ['v2wallet'],
+          previousPath = events[0].urlAfterRedirects.split('/')[1],
+          currentPath = events[1].urlAfterRedirects.split('/')[1];
 
-        // if (this.url !== '/wallet')
-        // console.log('this.route.url', this.route.url);
-        window.scrollTo(0, 0);
-      }
-    });
+        const currentPathDisabled = disabledPaths.indexOf(currentPath) > -1;
+        const pathChanged = previousPath !== currentPath;
+
+        if (!currentPathDisabled || (currentPathDisabled && pathChanged)) {
+          window.scrollTo(0, 0);
+        }
+      });
     return this;
   }
 
