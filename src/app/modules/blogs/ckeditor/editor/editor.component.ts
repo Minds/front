8cb589a7ff914e761b80a28f8a2b62f969e8d6c7
Ben Hayward <ben@minds.com>
2020-02-28 21:17:43 +0000

    Added in media uploader

--- src/app/modules/blogs/ckeditor/editor/editor.component.ts
+++ src/app/modules/blogs/ckeditor/editor/editor.component.ts
@@ -12,6 +12,7 @@ import {
   PLATFORM_ID,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { AttachmentService } from '../../../../services/attachment';
 declare var require: any;
 
 @Component({
@@ -27,12 +28,21 @@ export class BlogEditorComponent {
 
   Editor: any;
 
-  constructor(@Inject(PLATFORM_ID) protected platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) protected platformId: Object,
+    private attachment: AttachmentService
+  ) {}
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       const BalloonEditor = require('@bhayward93/ckeditor5-build-minds');
       this.Editor = BalloonEditor;
+      this.Editor.config = {
+        uploadHandler: async file => {
+          const response = this.attachment.upload(await file);
+          return `http://localhost:8080/fs/v1/thumbnail/${await response}/xlarge`;
+        },
+      };
     }
   }
 
