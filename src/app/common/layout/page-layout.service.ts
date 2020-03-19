5d671e7c7b277d889d195ede3e2593eb94f8129c
Mark Harding <mark@kramnorth.com>
2020-03-19 18:04:19 +0000

    (chore): change to layout and comments for new nav release

--- src/app/common/layout/page-layout.service.ts
+++ src/app/common/layout/page-layout.service.ts
@@ -1,8 +1,54 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Injectable()
 export class PageLayoutService {
+  routerSubscription: Subscription;
+
   hasRightPane$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  isFullWidth$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  hasTopbarBorder$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  hasTopbarBackground$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+
+  constructor(private router: Router) {
+    this.routerSubscription = this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(data => {
+        this.reset();
+      });
+  }
+
+  /**
+   * Forces to use full width
+   * @return void
+   */
+  useFullWidth(): void {
+    setTimeout(() => this.isFullWidth$.next(true));
+  }
+
+  useTopbarBorder(): void {
+    setTimeout(() => this.hasTopbarBorder$.next(true));
+  }
+
+  removeTopbarBorder(): void {
+    setTimeout(() => this.hasTopbarBorder$.next(false));
+  }
+
+  useTopbarBackground(): void {
+    setTimeout(() => this.hasTopbarBackground$.next(true));
+  }
+
+  removeTopbarBackground(): void {
+    setTimeout(() => this.hasTopbarBackground$.next(false));
+  }
+
+  reset(): void {
+    setTimeout(() => {
+      this.isFullWidth$.next(false);
+      this.hasTopbarBorder$.next(true);
+      this.hasTopbarBackground$.next(true);
+    });
+  }
 }
