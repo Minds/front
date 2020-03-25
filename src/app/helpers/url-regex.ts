991e8b79e0949d7fde6681a3108ee9854aabc768
Olivia Madrid <oliviajmadrid@gmail.com>
2020-03-25 15:38:39 -0600

    remove unicode flag from regex that was breaking logins on safari

--- src/app/helpers/url-regex.ts
+++ src/app/helpers/url-regex.ts
@@ -1,3 +1,3 @@
 export const createUrlRegex = (): RegExp => {
-  return /https?:\/\/([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w-]*)*\/?\??([^#\s\b]*)?#?([^\s\b]*)?\b/iu;
+  return /https?:\/\/([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w-]*)*\/?\??([^#\s\b]*)?#?([^\s\b]*)?\b/i;
 };
