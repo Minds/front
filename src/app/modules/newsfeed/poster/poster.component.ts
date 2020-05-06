e27559734dd83813c0b5f6e2550d84a39efb35fb
Mark Harding <mark@kramnorth.com>
2020-05-06 14:14:45 +0100

    (chore): increase main column to 620px - closes design#38

--- src/app/modules/newsfeed/poster/poster.component.ts
+++ src/app/modules/newsfeed/poster/poster.component.ts
@@ -108,7 +108,7 @@ export class PosterComponent {
     if (width && width !== this.lastWidth) {
       this.lastWidth = width;
 
-      this.showActionBarLabels = width >= 580;
+      this.showActionBarLabels = width >= 620;
     }
   }
 
