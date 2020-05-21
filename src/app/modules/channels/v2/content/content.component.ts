232f927401d96da6b1abb8b73567d01e43d757b9
Ben Hayward <ben@minds.com>
2020-05-21 12:04:27 +0100

    Exclude banned switch-case for admins

--- src/app/modules/channels/v2/content/content.component.ts
+++ src/app/modules/channels/v2/content/content.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { ConfigsService } from '../../../../common/services/configs.service';
 import { ChannelContentService } from './content.service';
+import { Session } from '../../../../services/session';
 
 /**
  * A container for channel loading errors.
@@ -23,9 +24,14 @@ export class ChannelContentComponent {
   /**
    * Constructor
    * @param content
+   * @param session
    * @param configs
    */
-  constructor(public content: ChannelContentService, configs: ConfigsService) {
+  constructor(
+    public content: ChannelContentService,
+    public session: Session,
+    configs: ConfigsService
+  ) {
     this.cdnAssetsUrl = configs.get('cdn_assets_url');
   }
 }
