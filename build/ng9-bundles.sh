78653ab85f19e84c5220dcfea3b2db96c7f4b377
Emi Balbuena <edgebal@gmail.com>
2020-05-26 17:54:40 -0300

    (fix): Wrong parameters in build script

--- build/ng9-bundles.sh
+++ build/ng9-bundles.sh
@@ -4,10 +4,10 @@
 BROWSER_PATH=${1:-dist/browser}
 
 # 2nd parameter is browser bundle deploy URL
-BROWSER_DEPLOY_URL=${1:-/browser}
+BROWSER_DEPLOY_URL=${2:-/browser}
 
 # 3rd parameter is server bundle folder
-SERVER_PATH=${1:-dist/server}
+SERVER_PATH=${3:-dist/server}
 
 # Allow Node.js to use up to 3.5G
 export NODE_OPTIONS="--max-old-space-size=3584"
