f80d89fc603f26e7809c7ac0fcb2f1ac643e6a90
Emi Balbuena <edgebal@gmail.com>
2020-05-26 17:53:26 -0300

    (chore): Build process

--- build/ng9-bundles.sh
+++ build/ng9-bundles.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+
+# 1st parameter is browser bundle folder
+BROWSER_PATH=${1:-dist/browser}
+
+# 2nd parameter is browser bundle deploy URL
+BROWSER_DEPLOY_URL=${1:-/browser}
+
+# 3rd parameter is server bundle folder
+SERVER_PATH=${1:-dist/server}
+
+# Allow Node.js to use up to 3.5G
 export NODE_OPTIONS="--max-old-space-size=3584"
-npm run build:client-and-server-bundles
+
+# Build
+npx gulp build.sass --deploy-url=$BROWSER_DEPLOY_URL
+npm run build -- --stats-json --outputPath=$BROWSER_PATH --deployUrl=$BROWSER_DEPLOY_URL
+npm run build:ssr -- --outputPath=$SERVER_PATH
