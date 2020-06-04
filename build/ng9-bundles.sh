31000f49b9f08c3b8eaa6806e7f9a1502d85956a
Mark Harding <mark@kramnorth.com>
2020-06-04 13:02:39 +0100

    (feat): do not use cdn url, proxy via nginx for statics

--- build/ng9-bundles.sh
+++ build/ng9-bundles.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# 1st parameter is browser bundle folder
 BROWSER_PATH=${1:-dist/browser}
-
-# 2nd parameter is browser bundle deploy URL
-BROWSER_DEPLOY_URL=${2:-/}
-
-# 3rd parameter is server bundle folder
 SERVER_PATH=${3:-dist/server}
 
 # Allow Node.js to use up to 3.5G
 export NODE_OPTIONS="--max-old-space-size=3584"
 
 # Build
-npx gulp build.sass --deploy-url=$BROWSER_DEPLOY_URL
-npm run build -- --stats-json --outputPath=$BROWSER_PATH --deployUrl=$BROWSER_DEPLOY_URL
+npx gulp build.sass --deploy-url="/static/"
+npm run build -- --stats-json --outputPath=$BROWSER_PATH
 npm run build:ssr -- --outputPath=$SERVER_PATH
