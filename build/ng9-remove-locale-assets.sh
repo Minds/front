49121e3b347db182745b2bf2d59c62f045abb079
Mark Harding <mark@kramnorth.com>
2020-06-11 12:47:39 +0100

    (fix): assets folder is too big. only use /en

---
+++ build/ng9-remove-locale-assets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BROWSER_PATH=${1:-dist/browser}
+
+# rm -rf $BROWSER_PATH/*/assets
+
+for dirname in $BROWSER_PATH/*; do
+    [ -e "$dirname" ] || continue;
+
+    ## Do not delete from en
+    if [ $dirname == $BROWSER_PATH/en ]; then
+        continue;
+    fi;
+
+    echo "Deleting $dirname/assets";
+    rm -rf $dirname/assets;
+done
