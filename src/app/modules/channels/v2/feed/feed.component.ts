ba90a567a06a75c9ad42f84bd41168c5029a9b81
Ben Hayward <ben@minds.com>
2020-06-18 08:26:20 +0000

    New post prepending to feed when made from topbar #3085

--- src/app/modules/channels/v2/feed/feed.component.ts
+++ src/app/modules/channels/v2/feed/feed.component.ts
@@ -5,14 +5,18 @@ import {
   OnDestroy,
   Inject,
   PLATFORM_ID,
+  OnInit,
 } from '@angular/core';
 import { FeedService } from './feed.service';
 import { Router } from '@angular/router';
 import { ChannelsV2Service } from '../channels-v2.service';
 import { FeedFilterType } from '../../../../common/components/feed-filter/feed-filter.component';
 import { FeedsService } from '../../../../common/services/feeds.service';
+import { FeedsUpdateService } from '../../../../common/services/feeds-update.service';
 import { Subscription } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
+import { tap } from 'rxjs/operators';
+import { Session } from '../../../../services/session';
 
 /**
  * Channel feed component
@@ -23,7 +27,7 @@ import { isPlatformBrowser } from '@angular/common';
   templateUrl: 'feed.component.html',
   providers: [FeedService, FeedsService],
 })
-export class ChannelFeedComponent implements OnDestroy {
+export class ChannelFeedComponent implements OnDestroy, OnInit {
   /**
    * Parses the view onto the current feed/type
    * @param view
@@ -57,6 +61,11 @@ export class ChannelFeedComponent implements OnDestroy {
   protected guidSubscription: Subscription;
 
   /**
+   * Listening for new posts.
+   */
+  private feedsUpdatedSubscription: Subscription;
+
+  /**
    * Constructor
    * @param feed
    * @param service
@@ -66,6 +75,8 @@ export class ChannelFeedComponent implements OnDestroy {
     public feed: FeedService,
     public service: ChannelsV2Service,
     protected router: Router,
+    public feedsUpdate: FeedsUpdateService,
+    private session: Session,
     @Inject(PLATFORM_ID) platformId: Object
   ) {
     if (isPlatformBrowser(platformId)) {
@@ -75,12 +86,52 @@ export class ChannelFeedComponent implements OnDestroy {
     }
   }
 
+  ngOnInit() {
+    this.feedsUpdatedSubscription = this.feedsUpdate.postEmitter.subscribe(
+      newPost => {
+        if (
+          this.feed.guid$.getValue() === this.session.getLoggedInUser().guid
+        ) {
+          this.prepend(newPost);
+        }
+      }
+    );
+  }
+
+  prepend(activity: any) {
+    if (!activity) {
+      return;
+    }
+
+    // TODO: Increment scheduled count https://gitlab.com/minds/front/-/issues/3127
+    // if (activity.time_created > Date.now() / 1000) { // and route is actually on a channel.
+    // this.feed.scheduledCount$ = this.feed.scheduledCount$.pipe(
+    //   map(count => count++)
+    // );
+    // }
+
+    let feedItem = {
+      entity: activity,
+      urn: activity.urn,
+      guid: activity.guid,
+    };
+
+    // Todo: Move to FeedsService
+    this.feed.service.rawFeed.next([
+      ...[feedItem],
+      ...this.feed.service.rawFeed.getValue(),
+    ]);
+  }
+
   /**
    * Destroy lifecycle hook
    */
   ngOnDestroy(): void {
     if (this.guidSubscription) {
       this.guidSubscription.unsubscribe();
+    }
+    if (this.feedsUpdatedSubscription) {
+      this.feedsUpdatedSubscription.unsubscribe();
     }
   }
 
