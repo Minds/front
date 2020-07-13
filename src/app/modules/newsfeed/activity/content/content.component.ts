31d8044f09e032a27e26f8b5363c4cd2bce5bcff
Olivia Madrid <oliviajmadrid@gmail.com>
2020-07-13 16:22:31 -0600

    show readmore for long paywalled text posts

--- src/app/modules/newsfeed/activity/content/content.component.ts
+++ src/app/modules/newsfeed/activity/content/content.component.ts
@@ -44,8 +44,6 @@ import { ScrollAwareVideoPlayerComponent } from '../../../media/components/video
   templateUrl: 'content.component.html',
   animations: [
     trigger('fader', [
-      state('active', style({})),
-      state('dismissed', style({})),
       transition(':leave', [
         style({ opacity: '1' }),
         animate(
@@ -226,22 +224,18 @@ export class ActivityContentComponent
 
   get imageUrl(): string {
     if (this.entity.custom_type === 'batch') {
-      if (this.isPaywalledGif) {
-        return `${this.cdnAssetsUrl}assets/photos/andromeda-galaxy-blur.jpg`;
-      }
-
       let thumbUrl = this.entity.custom_data[0].src;
-      if (this.showPaywallBadge) {
-        /**
-         * Check whether we need to add 'unlock_paywall' query as the only
-         * query param OR append to an existing one
-         */
-        const joiner = thumbUrl.split('?').length > 1 ? '&' : '/?';
+      // if (this.showPaywallBadge) {
+      /**
+       * Check whether we need to add 'unlock_paywall' query as the only
+       * query param OR append to an existing one
+       */
+      const joiner = thumbUrl.split('?').length > 1 ? '&' : '/?';
 
-        const thumbTimestamp = this.paywallUnlocked ? moment().unix() : '0';
+      const thumbTimestamp = this.paywallUnlocked ? moment().unix() : '0';
 
-        thumbUrl += `${joiner}unlock_paywall=${thumbTimestamp}`;
-      }
+      thumbUrl += `${joiner}unlock_paywall=${thumbTimestamp}`;
+      // }
       return thumbUrl;
     }
 
