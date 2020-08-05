35f6ae3ee3f1713ae1feb0f716bf17a4ce5a01a1
Mark Harding <mark@kramnorth.com>
2020-08-05 18:24:22 +0100

    (chore): lazy load channels

--- src/app/modules/channel-container/channel-container.module.ts
+++ src/app/modules/channel-container/channel-container.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule as NgCommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ChannelsModule } from '../channels/channels.module';
 import { ProModule } from '../pro/pro.module';
 import { ChannelContainerComponent } from './channel-container.component';
 import { CommonModule } from '../../common/common.module';
 import { ChannelsV2Module } from '../channels/v2/channels-v2.module';
+import { CanDeactivateGuardService } from '../../services/can-deactivate-guard';
 
 @NgModule({
   imports: [
@@ -13,6 +15,14 @@ import { ChannelsV2Module } from '../channels/v2/channels-v2.module';
     ProModule,
     ChannelsModule,
     ChannelsV2Module,
+    RouterModule.forChild([
+      // Note: The wildcard lazy loads this
+      {
+        path: '',
+        component: ChannelContainerComponent,
+        canDeactivate: [CanDeactivateGuardService],
+      },
+    ]),
   ],
   declarations: [ChannelContainerComponent],
 })
