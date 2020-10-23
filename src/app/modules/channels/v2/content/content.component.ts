272f332d07b82b2b05b1fd6418e6961c81ce7f27
Olivia Madrid <olivia@minds.com>
2020-10-23 09:01:15 +0000

    Mask banner and avatar of nsfw channel

--- src/app/modules/channels/v2/content/content.component.ts
+++ src/app/modules/channels/v2/content/content.component.ts
@@ -1,7 +1,14 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { ConfigsService } from '../../../../common/services/configs.service';
 import { ChannelContentService } from './content.service';
 import { Session } from '../../../../services/session';
+import { Subscription } from 'rxjs';
 
 /**
  * A container for channel loading errors.
@@ -14,12 +21,17 @@ import { Session } from '../../../../services/session';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'content.component.html',
   providers: [ChannelContentService],
+  host: { '[class.m-channelContent--nsfw]': 'channelIsNsfw' },
 })
-export class ChannelContentComponent {
+export class ChannelContentComponent implements OnInit, OnDestroy {
   /**
    * Assets CDN URL
    */
   readonly cdnAssetsUrl: string;
+
+  protected stateSubscription: Subscription;
+
+  protected channelIsNsfw: boolean = false;
 
   /**
    * Constructor
@@ -33,5 +45,17 @@ export class ChannelContentComponent {
     configs: ConfigsService
   ) {
     this.cdnAssetsUrl = configs.get('cdn_assets_url');
+  }
+
+  ngOnInit(): void {
+    this.stateSubscription = this.content.state$.subscribe(state => {
+      this.channelIsNsfw = state === 'nsfw';
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.stateSubscription) {
+      this.stateSubscription.unsubscribe();
+    }
   }
 }
