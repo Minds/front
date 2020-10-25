4ce8b54ff86775528c7cd7bbe420fef1565500e0
namesty <nestor09amesty@gmail.com>
2020-10-26 00:55:15 +0100

    Updated web3service

--- src/app/modules/blockchain/web3.service.ts
+++ src/app/modules/blockchain/web3.service.ts
@@ -1,72 +1,68 @@
 import { Injectable } from '@angular/core';
-import { Contract, utils } from 'ethers';
+import { BigNumberish, Contract, utils } from 'ethers';
 import { Web3Provider, ExternalProvider } from '@ethersproject/providers';
 import BN from 'bn.js';
 import { ConfigsService } from '../../common/services/configs.service';
+import { Web3ModalService } from '@dorgtech/web3modal-angular';
+import { Interface } from 'ethers/lib/utils';
 
 type Address = string;
 
 @Injectable()
 export class Web3Service {
   public config: any;
-  public provider?: Web3Provider;
+  private provider: Web3Provider;
 
-  constructor(private configs: ConfigsService) {
+  constructor(
+    private configs: ConfigsService,
+    private web3modalService: Web3ModalService
+  ) {
     this.config = this.configs.get('blockchain');
 
     console.log(this.config);
+  }
+
+  async getProvider() {
+    if (!this.provider) {
+      const provider = await this.web3modalService.open();
+      this.setProvider(provider);
+    }
+
+    return this.provider;
+  }
+
+  async getSigner() {
+    const provider = await this.getProvider();
+    return provider.getSigner();
+  }
+
+  async getCurrentWalletAddress() {
+    const signer = await this.getSigner();
+
+    return await signer.getAddress();
+  }
+
+  getABIInterface(abi: any) {
+    return new Interface(abi);
   }
 
   public setProvider(provider: ExternalProvider) {
     this.provider = new Web3Provider(provider);
   }
 
-  public getContract(address: Address, abi: string[]): Contract {
-    if (!this.provider) {
-      throw new Error('Please call `setProvider` before calling `getContract`');
-    }
+  public async getContract(address: Address, abi: string[]): Promise<Contract> {
+    const signer = await this.getSigner();
 
-    return new Contract(address, abi, this.provider.getSigner());
+    return new Contract(address, abi, signer);
   }
 
-  public async callView(
-    address: Address,
-    method: string,
-    args: string
-  ): Promise<string> {
-    const contract = this.getContract(address, [method]);
-    const funcs = Object.keys(contract.interface.functions);
-    if (args[0] !== '[') {
-      args = `[${args}]`;
-    }
-    const parsedArgs = JSON.parse(args);
-    const res = await contract[funcs[0]](...parsedArgs);
-    return res.toString();
-  }
-
-  public async sendTransaction(
-    address: Address,
-    method: string,
-    args: string
-  ): Promise<string> {
-    const contract = this.getContract(address, [method]);
-    const funcs = Object.keys(contract.interface.functions);
-    if (args[0] !== '[') {
-      args = `[${args}]`;
-    }
-    const parsedArgs = JSON.parse(args);
-    const tx = await contract[funcs[0]](...parsedArgs);
-
-    return tx;
-  }
-
-  public fromWei(amount: BN): string {
-    const etherAmount = utils.formatEther(amount.toString());
+  public fromWei(amount: BN, unit?: BigNumberish): string {
+    const etherAmount = utils.formatUnits(amount.toString(), unit);
     return etherAmount.toString();
   }
 
-  public toWei(amount: string | number): BN {
-    const weiAmount = utils.parseEther(amount.toString());
+  public toWei(amount: string | number, unit?: BigNumberish): BN {
+    const weiAmount = utils.parseUnits(amount.toString(), unit);
     return new BN(weiAmount.toString());
   }
 }
