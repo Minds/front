5bf68e617bc4b7d459da7dab1a22b3d9add9de47
Ben Hayward <ben@minds.com>
2021-01-08 18:17:23 +0000

    Remove YT migrator link for non-Canary users.

--- src/app/modules/marketing/footer.component.ts
+++ src/app/modules/marketing/footer.component.ts
@@ -8,6 +8,7 @@ import {
   SkipSelf,
 } from '@angular/core';
 import { ConfigsService } from '../../common/services/configs.service';
+import { Session } from '../../services/session';
 import { OverlayModalService } from '../../services/ux/overlay-modal';
 
 @Component({
@@ -21,9 +22,18 @@ export class MarketingFooterComponent {
 
   readonly cdnAssetsUrl: string;
 
+  /**
+   * Determine whether user is opted into canary mode
+   * @returns { boolean } - True if user is in Canary mode
+   */
+  get isCanary(): boolean {
+    return this.session.getLoggedInUser().canary || false;
+  }
+
   constructor(
     private configs: ConfigsService,
     protected cd: ChangeDetectorRef,
+    private session: Session,
     @SkipSelf() private injector: Injector
   ) {
     this.cdnAssetsUrl = configs.get('cdn_assets_url');
