3d75453e7aa843c4e13990a1743c14513d608188
Ben Hayward <ben@minds.com>
2021-03-08 11:09:10 +0000

    Connect password reset modal

--- src/app/modules/settings-v2/security/two-factor-v2/confirm-password/comfirm-password.component.ts
+++ src/app/modules/settings-v2/security/two-factor-v2/confirm-password/comfirm-password.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, switchMap, take, throttleTime } from 'rxjs/operators';
+import { ApiService } from '../../../../../common/api/api.service';
+import { AbstractSubscriberComponent } from '../../../../../common/components/abstract-subscriber/abstract-subscriber.component';
+import { FormToastService } from '../../../../../common/services/form-toast.service';
 import { SettingsTwoFactorV2Service } from '../two-factor-v2.service';
 
 /**
  * Password confirm component - force a user to verify their password before enabling or removing 2fa
  *
- * TODO: TRY USE MODAL PASSWORD FORM
  * TODO: Remove 2FA logic - may need to pass in an intent to decide what the next panel is.
  */
 @Component({
@@ -22,21 +25,43 @@ import { SettingsTwoFactorV2Service } from '../two-factor-v2.service';
         (ngModelChange)="passwordValueChanged($event)"
       />
 
-      <m-button [color]="'blue'" (onAction)="onConfirmClick()">
+      <m-button
+        [color]="'blue'"
+        (onAction)="onConfirmClick()"
+        [disabled]="disabled$ | async"
+      >
         <ng-container>Confirm</ng-container>
       </m-button>
     </form>
   `,
   styleUrls: ['./confirm-password.component.ng.scss'],
 })
-export class SettingsTwoFactorPasswordComponent implements OnInit {
+export class SettingsTwoFactorPasswordComponent
+  extends AbstractSubscriberComponent
+  implements OnInit {
   // amount input form
   public form: FormGroup;
 
   // password string from user
   public password$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-  constructor(private service: SettingsTwoFactorV2Service) {}
+  /**
+   * Should progress be disabled?
+   * @returns { Observable<boolean> } - true if should be disabled.
+   */
+  get disabled$(): Observable<boolean> {
+    return this.password$.pipe(
+      map((password: string) => !password || password.length < 6)
+    );
+  }
+
+  constructor(
+    private service: SettingsTwoFactorV2Service,
+    private api: ApiService,
+    private toast: FormToastService
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     // init form group.
@@ -52,14 +77,39 @@ export class SettingsTwoFactorPasswordComponent implements OnInit {
    * @returns { void }
    */
   public onConfirmClick(): void {
-    this.service.activePanel$.next('recovery-code');
+    this.subscriptions.push(
+      this.password$
+        .pipe(
+          take(1), // call once
+          throttleTime(2000), // disallow more than 1 request every 2s
+          switchMap((password: string) => {
+            // switch outer observable to api req.
+            return this.api.post('api/v2/settings/password/validate', {
+              password: password,
+            });
+          }),
+          catchError(e => {
+            console.error(e);
+            this.toast.error('Sorry, an error has occurred. Please try again.');
+            return of(null);
+          })
+        )
+        .subscribe(response => {
+          if (response && response.status === 'success') {
+            this.service.activePanel$.next('recovery-code');
+            return;
+          }
+          this.password$.next(''); // clear password
+          this.toast.error('Incorrect password. Please try again.');
+        })
+    );
   }
 
   /**
    * Called on password change - updates password value.
    * @param { string } $event - new password value.
    */
-  passwordValueChanged($event: string) {
+  public passwordValueChanged($event: string) {
     this.password$.next($event);
   }
 }
