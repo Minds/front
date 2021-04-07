521b4433f81625d5c4759cc34b00c72f2ae5e4d6
Martin Santangelo <msantang78@gmail.com>
2021-04-07 14:59:55 -0300

    (fix) channel's banner URL

--- src/app/helpers/entity-to-banner-url.ts
+++ src/app/helpers/entity-to-banner-url.ts
@@ -1,9 +1,18 @@
 export default function entityToBannerUrl(
-  entity: { guid: string; banner?: any; icontime?: any } | null,
+  entity: {
+    guid: string;
+    banner?: any;
+    icontime?: any;
+    carousels?: any[] | boolean;
+  } | null,
   sizeOrTop: string | number = 'fat'
 ): string {
   if (!entity) {
     return '';
+  }
+
+  if (entity.carousels && entity.carousels[0]) {
+    return entity.carousels[0].src + (entity.banner || entity.icontime);
   }
 
   return `fs/v1/banners/${entity.guid}/${sizeOrTop}/${entity.banner ||
