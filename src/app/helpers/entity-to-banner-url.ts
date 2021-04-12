f69124521c0e8d1e4112dfff810a287a703b4daf
Mark Harding <mark@minds.com>
2021-04-12 14:02:51 +0000

    Revert "Merge branch 'fix/channels-banner-url' into 'master'"
    This reverts merge request !1422

--- src/app/helpers/entity-to-banner-url.ts
+++ src/app/helpers/entity-to-banner-url.ts
@@ -1,18 +1,9 @@
 export default function entityToBannerUrl(
-  entity: {
-    guid: string;
-    banner?: any;
-    icontime?: any;
-    carousels?: any[] | boolean;
-  } | null,
+  entity: { guid: string; banner?: any; icontime?: any } | null,
   sizeOrTop: string | number = 'fat'
 ): string {
   if (!entity) {
     return '';
-  }
-
-  if (entity.carousels && entity.carousels[0]) {
-    return entity.carousels[0].src + (entity.banner || entity.icontime);
   }
 
   return `fs/v1/banners/${entity.guid}/${sizeOrTop}/${entity.banner ||
