c643d52a69a1b8513f9b56ee0b826b4c12774d49
Ben Hayward <ben@minds.com>
2021-06-18 13:53:38 +0100

    Switch url to mindsapp

--- src/app/common/services/app-prompt.service.ts
+++ src/app/common/services/app-prompt.service.ts
@@ -141,7 +141,7 @@ export class AppPromptService implements OnDestroy {
                   window.location.href = 'mindsapp://';
                   break;
                 case 'android':
-                  window.location.replace('https://www.minds.com/');
+                  window.location.replace('mindsapp:// ');
                   setTimeout(
                     (window.location.href =
                       'https://play.google.com/store/apps/details?id=com.minds.mobile'),
