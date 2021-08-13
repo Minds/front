cf8b7c119ddfe933ccd5dc8887e4a67fe3afe4aa
Carlos <carlosfebres@dorg.tech>
2021-08-13 17:41:37 -0400

    feat: create proposal

--- src/app/modules/governance/snapshot.service.ts
+++ src/app/modules/governance/snapshot.service.ts
@@ -1,8 +1,18 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import snapshot from '@snapshot-labs/snapshot.js';
+import { Web3WalletService } from '../blockchain/web3-wallet.service';
 
-const SNAPSHOT_GRAPHQL_URL = 'https://hub.snapshot.page/graphql';
+const SNAPSHOT_GRAPHQL_URL = 'https://hub.snapshot.org/graphql';
+const MINDS_SPACE = 'mind.eth';
+
+type ProposalType =
+  | 'single-choice'
+  | 'approval'
+  | 'quadratic'
+  | 'ranked-choice'
+  | 'weighted';
 
 export interface SnapshotProposal {
   id: string;
@@ -20,11 +30,39 @@ export interface SnapshotProposal {
   };
 }
 
+interface SnapshotStrategy {
+  name: string;
+  params: Record<string, unknown>;
+}
+
+interface CreateProposal {
+  name: string;
+  body: string;
+  choices: string[];
+  start: number;
+  end: number;
+  snapshot: number;
+
+  type?: ProposalType;
+  metadata?: {
+    plugins?: Record<string, unknown>;
+    network?: string;
+    strategies?: SnapshotStrategy[];
+  };
+}
+
 interface GetProposalsVariables {
   first: number;
   skip: number;
   space?: string;
   state?: string;
+}
+
+interface Space {
+  id: string;
+  name: string;
+  network: string;
+  strategies: SnapshotStrategy[];
 }
 
 const QUERY_GET_PROPOSAL_LIST = `
@@ -46,13 +84,27 @@ query ($first: Int!, $skip: Int!, $space: String, $state: String) {
   }
 }`;
 
+const QUERY_GET_SPACE = `
+query ($id: String!) {
+  space(id: $id) {
+    id
+    name
+    network
+    strategies {
+      name
+      params
+    }
+  }
+}`;
+
 @Injectable()
 export class SnapshotService {
-  constructor(private httpClient: HttpClient) {}
+  private snapshotClient = new snapshot.Client();
 
-  static _(httpClient: HttpClient): SnapshotService {
-    return new SnapshotService(httpClient);
-  }
+  constructor(
+    protected web3Wallet: Web3WalletService,
+    private httpClient: HttpClient
+  ) {}
 
   getProposals(variables: GetProposalsVariables) {
     return this.execQuery<{ proposals: SnapshotProposal[] }>(
@@ -62,7 +114,47 @@ export class SnapshotService {
   }
 
   getMindsProposals(variables: GetProposalsVariables) {
-    return this.getProposals({ ...variables, space: 'mind.eth' });
+    return this.getProposals({ ...variables, space: MINDS_SPACE });
+  }
+
+  async createProposal() {
+    const provider = this.web3Wallet.provider;
+    if (provider) {
+      const space = await this.getSpace(MINDS_SPACE).toPromise();
+      const signer = this.web3Wallet.getSigner();
+      const address = await signer.getAddress();
+      const blockNumber = await snapshot.utils.getBlockNumber(provider);
+
+      const start = new Date().getTime();
+      const end = start + 3600 * 24 * 14;
+
+      const proposal: CreateProposal = {
+        name: '',
+        body: '',
+        type: 'single-choice',
+        end,
+        start,
+        choices: ['approve', 'reject'],
+        snapshot: blockNumber,
+        metadata: {
+          network: space.network,
+          strategies: space.strategies,
+        },
+      };
+
+      return this.snapshotClient.proposal(
+        provider,
+        address,
+        MINDS_SPACE,
+        proposal
+      );
+    }
+  }
+
+  getSpace(id: string) {
+    return this.execQuery<{ space: Space }>(QUERY_GET_SPACE, { id }).pipe(
+      map(response => response.space)
+    );
   }
 
   private execQuery<T>(query: string, variables?: Record<string, unknown>) {
