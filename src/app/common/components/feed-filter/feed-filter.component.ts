9f7b2180ca91d2eba2f3db7a3c98b167a1f8ae25
Olivia Madrid <olivia@minds.com>
2021-09-15 13:16:52 +0000

    channel filter date

--- src/app/common/components/feed-filter/feed-filter.component.ts
+++ src/app/common/components/feed-filter/feed-filter.component.ts
@@ -2,14 +2,19 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  Injector,
   Input,
+  OnInit,
   Output,
 } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FeedService } from '../../../modules/channels/v2/feed/feed.service';
+import { DateRangeModalService } from '../date-range-modal/date-range-modal.services';
 
 /**
  * Feed filter options
  */
-export type FeedFilterOption = 'type';
+export type FeedFilterOption = 'type' | 'dateRange';
 
 /**
  * Feed filter sort
@@ -23,14 +28,27 @@ export type FeedFilterSort = 'latest' | 'top' | 'hot' | 'scheduled';
 export type FeedFilterType = 'activities' | 'images' | 'videos' | 'blogs';
 
 /**
+ * Feed filter date range values
+ */
+export type FeedFilterDateRangeType = 'all' | 'custom';
+
+/**
  * Feed filter label/value set
  */
 interface LabelValue<T> {
-  label: string;
+  label?: string;
   value: T;
   hidden?: boolean;
+  menuLabel?: string;
 }
 
+/**
+ * Feed filter date range object (in seconds)
+ */
+export interface FeedFilterDateRange {
+  fromDate: number | null;
+  toDate: number | null;
+}
 /**
  * Feed filter component
  */
@@ -40,16 +58,23 @@ interface LabelValue<T> {
   templateUrl: 'feed-filter.component.html',
   styleUrls: ['feed-filter.component.ng.scss'],
 })
-export class FeedFilterComponent {
+export class FeedFilterComponent implements OnInit {
+  subscriptions: Array<Subscription>;
+
   /**
    * Displayed options
    */
-  @Input() options: Array<FeedFilterOption> = ['type'];
+  @Input() options: Array<FeedFilterOption> = ['type', 'dateRange'];
 
   /**
    * Current type
    */
   @Input() type: FeedFilterType;
+
+  /**
+   * Current date range
+   */
+  @Input() dateRangeType: FeedFilterDateRangeType = 'all';
 
   /**
    * Change current type
@@ -79,7 +104,65 @@ export class FeedFilterComponent {
     { label: 'Blogs', value: 'blogs' },
   ];
 
+  /**
+   * Date range types
+   */
+  readonly dateRangeTypes: Array<LabelValue<FeedFilterDateRangeType>> = [
+    { label: 'All', value: 'all', menuLabel: 'All time' },
+    { label: null, value: 'custom', menuLabel: 'Date range' },
+  ];
+
+  constructor(
+    public service: FeedService,
+    protected dateRangeModal: DateRangeModalService,
+    protected injector: Injector
+  ) {}
+
+  ngOnInit(): void {
+    this.subscriptions = [
+      this.service.dateRangeEnabled$.subscribe(dateRangeEnabled => {
+        this.dateRangeType = dateRangeEnabled ? 'custom' : 'all';
+      }),
+    ];
+  }
+
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
+  onDateRangeChange(dateRangeType: FeedFilterDateRangeType): void {
+    if (dateRangeType === 'all') {
+      this.service.dateRange$.next({
+        fromDate: null,
+        toDate: null,
+      });
+    } else {
+      this.openDateRangeModal();
+    }
+  }
+
+  /**
+   * Opens date range modal
+   */
+  async openDateRangeModal(): Promise<void> {
+    const dateRange: FeedFilterDateRange = await this.dateRangeModal
+      .present(this.injector)
+      .toPromise();
+
+    if (dateRange) {
+      this.service.dateRange$.next(dateRange);
+    }
+  }
+
   get typeLabel() {
     return this.types.find(typeObj => typeObj.value === this.type).label;
+  }
+
+  get dateRangeLabel() {
+    return this.dateRangeTypes.find(
+      dateRangeEntry => dateRangeEntry.value === this.dateRangeType
+    ).label;
   }
 }
