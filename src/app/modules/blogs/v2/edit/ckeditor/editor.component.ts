6b194f8735f609cc3144eb20d0561437e4598371
Mani Shooshtari <mani@shooshtari.com>
2021-09-24 14:55:08 +0300

    (feat) dynamically set theme

--- src/app/modules/blogs/v2/edit/ckeditor/editor.component.ts
+++ src/app/modules/blogs/v2/edit/ckeditor/editor.component.ts
@@ -2,19 +2,21 @@
  * @author Ben Hayward
  * @desc Wrapper for CKEditor5 text editor.
  */
-
+import { isPlatformBrowser } from '@angular/common';
 import {
   Component,
-  Input,
-  Output,
   EventEmitter,
   Inject,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
   PLATFORM_ID,
 } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { AttachmentService } from '../../../../../services/attachment';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { SiteService } from '../../../../../common/services/site.service';
 import { ThemeService } from '../../../../../common/services/theme.service';
+import { AttachmentService } from '../../../../../services/attachment';
 
 declare var require: any;
 
@@ -26,7 +28,7 @@ declare var require: any;
   templateUrl: 'editor.component.html',
   styleUrls: ['./editor.component.ng.scss', '../../../view/view.ng.scss'],
 })
-export class BlogEditorComponent {
+export class BlogEditorComponent implements OnInit, OnDestroy {
   /**
    * Content to place into the editor on load.
    */
@@ -37,7 +39,12 @@ export class BlogEditorComponent {
    */
   @Output() contentChanged: EventEmitter<Event> = new EventEmitter<Event>();
 
+  /**
+   * Editor instance
+   */
   Editor: any;
+  editorConfig$: BehaviorSubject<any> = new BehaviorSubject(this.buildConfig());
+  themeSubscription: Subscription;
 
   constructor(
     @Inject(PLATFORM_ID) protected platformId: Object,
@@ -46,36 +53,47 @@ export class BlogEditorComponent {
     public themeService: ThemeService
   ) {}
 
-  editorConfig = {
-    uploadHandler: async file => {
-      const response = this.attachment.upload(await file);
-      return `${this.site.baseUrl}fs/v1/thumbnail/${await response}/xlarge`;
-    },
-    isDark$: this.themeService.isDark$,
-    mediaEmbed: {
-      previewsInData: true,
-      extraProviders: [
-        {
-          name: 'minds',
-          url: [/^minds\.com\/newsfeed\/(\w+)/, /^minds\.com\/embed\/(\w+)/],
-          html: match => {
-            const guid = match[1];
-            return `<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">
+  buildConfig(dark?: boolean) {
+    return {
+      uploadHandler: async file => {
+        const response = this.attachment.upload(await file);
+        return `${this.site.baseUrl}fs/v1/thumbnail/${await response}/xlarge`;
+      },
+      isDark$: this.themeService.isDark$,
+      mediaEmbed: {
+        // setting this to false because we want to 
+        // populate minds links every time so we can set
+        // the embed theme according to site theme
+        previewsInData: false,
+        extraProviders: [
+          {
+            name: 'minds',
+            url: [/^minds\.com\/newsfeed\/(\w+)/, /^minds\.com\/embed\/(\w+)/],
+            html: match => {
+              const guid = match[1];
+              return `<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">
               <iframe src="http://localhost:4300/embed/${guid}?theme=${
-              this.themeService.isDark$.getValue() ? 'dark' : 'light'
-            }"
+                dark ? 'dark' : 'light'
+              }"
                   style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;"
                   frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
               </iframe>
           </div>`;
+            },
           },
-        },
-      ],
-    },
-  };
+        ],
+      },
+    };
+  }
 
   ngOnInit() {
-    this.themeService.emitThemePreference();
+    // TODO: dynamically change the config so we can change
+    // minds provider html output to output the embed theme
+    // according to site theme
+    this.themeSubscription = this.themeService.isDark$.subscribe(dark => {
+      // FIXME: for some reason this doesn't work
+      this.editorConfig$.next(this.buildConfig(dark));
+    });
     // Render on browser side.
     if (isPlatformBrowser(this.platformId)) {
       // Must be required here for client-side loading.
@@ -90,5 +108,9 @@ export class BlogEditorComponent {
    */
   onContentChanged($event: Event): void {
     this.contentChanged.emit($event);
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
   }
 }
