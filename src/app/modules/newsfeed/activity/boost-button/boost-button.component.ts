125294cd0f41dafcfc85163f16b9bf46363d01de
Mani Shooshtari <mani@shooshtari.com>
2021-10-07 17:23:40 +0300

    (fix) use OnPush and rxjs subscriptions

--- src/app/modules/newsfeed/activity/boost-button/boost-button.component.ts
+++ src/app/modules/newsfeed/activity/boost-button/boost-button.component.ts
@@ -1,23 +1,46 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ActivityEntity, ActivityService } from '../activity.service';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { BoostRecommendationService } from '../../../../common/services/boost-recommendation.service';
 
 @Component({
   selector: 'm-activity__boostButton',
   templateUrl: 'boost-button.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ActivityBoostButtonComponent {
+export class ActivityBoostButtonComponent implements OnInit {
   @Input()
   object = {
     guid: undefined,
   };
+  boostRecommendationsSubscription: Subscription;
+  shouldShowTooltip: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  shouldShowShimmer: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(public boostRecommendationService: BoostRecommendationService) {}
 
-  get shouldShowTooltip() {
-    return this.boostRecommendationService.shouldShowTooltip(this.object.guid);
+  ngOnInit() {
+    this.boostRecommendationsSubscription = this.boostRecommendationService.boostRecommendations.subscribe(
+      boostRecommendations => {
+        if (boostRecommendations.find(guid => guid === this.object.guid)) {
+          if (!this.boostRecommendationService.boostRecommended.getValue()) {
+            this.shouldShowTooltip.next(true);
+          }
+
+          this.shouldShowShimmer.next(true);
+        } else {
+          this.shouldShowTooltip.next(false);
+          this.shouldShowShimmer.next(false);
+        }
+      }
+    );
   }
-  get shouldShowBoost() {
-    return this.boostRecommendationService.shouldShowBoost(this.object.guid);
+
+  ngOnDestroy() {
+    this.boostRecommendationsSubscription?.unsubscribe();
   }
 }
