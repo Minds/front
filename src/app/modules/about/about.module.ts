f5113b9a99aa8ba2640931ac9a15b72bd0d45169
Ben Hayward <ben@minds.com>
2021-11-02 10:33:15 +0000

    Add homepageV2 to /about

---
+++ src/app/modules/about/about.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule as NgCommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '../../common/common.module';
+import { AboutComponent } from './about.component';
+import { HomepageV2Module } from '../homepage-v2/homepage.module';
+import { HomepageV2Component } from '../homepage-v2/homepage-v2.component';
+
+const routes: Routes = [
+  {
+    path: 'about',
+    component: HomepageV2Component,
+    data: {
+      title: 'About',
+      description:
+        'Free your mind and get paid for creating content, driving traffic and referring friends. A place to have open conversations and bring people together.',
+      canonicalUrl: '/about',
+    },
+  },
+];
+
+@NgModule({
+  imports: [
+    NgCommonModule,
+    CommonModule,
+    RouterModule.forChild(routes),
+    HomepageV2Module,
+  ],
+  declarations: [AboutComponent],
+})
+export class AboutModule {}
