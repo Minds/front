28f6ad40d9fb5abe98c74dbc17d5e73b69e885cc
Mani Shooshtari <mani@shooshtari.com>
2021-11-12 14:10:53 +0300

    optimize blurhash decoding performance

--- src/app/common/directives/blurhash/blurhash.directive.ts
+++ src/app/common/directives/blurhash/blurhash.directive.ts
@@ -14,6 +14,7 @@ import { timer } from 'rxjs';
   selector: 'img[m-blurhash]',
 })
 export class BlurhashDirective implements AfterViewInit, OnDestroy {
+  private RESOLUTION = 128;
   private _src: string;
 
   @HostBinding('attr.src')
@@ -28,7 +29,7 @@ export class BlurhashDirective implements AfterViewInit, OnDestroy {
 
   @HostListener('load')
   onLoad() {
-    if (this.canvas) {
+    if (this.canvas && !this.paywalled) {
       // TODO: also remove the canvas after transition is done
       this.canvas.style.opacity = 0;
     }
@@ -50,6 +51,9 @@ export class BlurhashDirective implements AfterViewInit, OnDestroy {
   @Input('m-blurhash')
   entity;
 
+  @Input('paywalled')
+  paywalled;
+
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
@@ -69,11 +73,14 @@ export class BlurhashDirective implements AfterViewInit, OnDestroy {
     const elementHeight = this.el?.nativeElement?.height;
 
     let { blurhash, width, height } = Object.assign(
+      {},
       {
         // NOTE: not sure if we should have a default blurhash
         // blurhash: 'LyHnO9n$tUt7}qoOxAs:-BskngRj',
         width: elementWidth,
         height: elementHeight,
+        // the blurhash might also be on this key
+        blurhash: this.entity.blurhash,
       },
       this.entity.custom_data[0]
     );
@@ -84,21 +91,29 @@ export class BlurhashDirective implements AfterViewInit, OnDestroy {
 
     const aspecRatio = width / height;
     width = elementWidth;
-    height = aspecRatio * elementWidth;
+    height = elementHeight || aspecRatio * elementWidth;
 
-    const pixels = decode(blurhash, width, height);
+    if (!width || !height) {
+      return null;
+    }
 
+    const pixels = decode(blurhash, this.RESOLUTION, this.RESOLUTION);
     this.canvas = document.createElement('canvas');
     const ctx = this.canvas.getContext('2d');
-    ctx.canvas.width = width;
-    ctx.canvas.height = height;
-    const imageData = ctx.createImageData(width, height);
-    imageData.data.set(pixels);
 
+    ctx.canvas.width = this.RESOLUTION;
+    ctx.canvas.height = this.RESOLUTION;
+
+    const imageData = ctx.createImageData(this.RESOLUTION, this.RESOLUTION);
+    imageData.data.set(pixels);
     ctx.putImageData(imageData, 0, 0);
+
+    const scaleX = width / this.RESOLUTION;
+    const scaleY = height / this.RESOLUTION;
+
     this.canvas.setAttribute(
       'style',
-      `position: absolute; top: 0; left: 0; transition: all 0.3s; transition-timing-function: ease-out;`
+      `position: absolute; top: 0; left: 0; transition: all 0.3s; transition-timing-function: ease-out; transform-origin: top left; transform: scaleX(${scaleX}) scaleY(${scaleY})`
     );
     this.el.nativeElement.parentElement.append(this.canvas);
   }
