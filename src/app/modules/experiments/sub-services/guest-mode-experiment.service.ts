d0c509979261c798e40170906aedfa798c6ade69
Ben Hayward <ben@minds.com>
2021-11-24 11:29:23 +0000

    Change inject to injectable

--- src/app/modules/experiments/sub-services/guest-mode-experiment.service.ts
+++ src/app/modules/experiments/sub-services/guest-mode-experiment.service.ts
@@ -1,4 +1,4 @@
-import { Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { FeaturesService } from '../../../services/features.service';
 import { ExperimentsService } from '../experiments.service';
 
@@ -6,7 +6,7 @@ import { ExperimentsService } from '../experiments.service';
  * Returns whether guest mode experiment is active, based upon whether
  * the user has been assigned the experimental variant the feature flag is enabled.
  */
-@Inject({ providedIn: 'root' })
+@Injectable({ providedIn: 'root' })
 export class GuestModeExperimentService {
   constructor(
     private featuresService: FeaturesService,
