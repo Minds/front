43e61723210828f85c06ff28300f380eae92d263
Mani Shooshtari <mani@shooshtari.com>
2021-11-24 13:51:55 +0300

    add tooltip prompt

--- src/app/modules/newsfeed/feeds/feed-type-popover/feed-type-popover.component.ts
+++ src/app/modules/newsfeed/feeds/feed-type-popover/feed-type-popover.component.ts
@@ -1,4 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Storage } from '../../../../services/storage';
+
+const TOP_FEED_PROMPT_STORAGE_KEY = 'top-feed:recommended';
+const TOP_FEED_PROMPT_DURATION = 30000;
 
 @Component({
   selector: 'm-feedTypePopover',
@@ -8,6 +13,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class FeedTypePopoverComponent implements OnInit {
   @Input() type: 'top' | 'latest' = 'latest';
   shown: boolean = false;
+  shouldShowTooltip$ = new BehaviorSubject<boolean>(true);
 
   // Here we are using offset modifier, but more options
   // are available at https://popper.js.org/docs/v2
@@ -18,7 +24,27 @@ export class FeedTypePopoverComponent implements OnInit {
     },
   };
 
+  constructor(private storage: Storage) {}
+
   ngOnInit(): void {
+    const topFeedRecommended = this.storage.get(TOP_FEED_PROMPT_STORAGE_KEY);
+
+    if (!topFeedRecommended) {
+      this.recommendTopFeed();
+    }
+  }
+
+  /**
+   * recommends top feed by showing a tooltip
+   **/
+  private recommendTopFeed() {
+    this.shouldShowTooltip$.next(true);
+    this.storage.set(TOP_FEED_PROMPT_STORAGE_KEY, true);
+
+    setTimeout(
+      () => this.shouldShowTooltip$.next(false),
+      TOP_FEED_PROMPT_DURATION
+    );
   }
 
   /**
