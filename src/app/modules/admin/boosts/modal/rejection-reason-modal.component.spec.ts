6a9ce8a29982fe5fcfbda4905543d2c181411831
Ben Hayward <ben@minds.com>
2021-12-07 14:34:12 +0000

    Boost rejection reason deprecation strategy #5162

--- src/app/modules/admin/boosts/modal/rejection-reason-modal.component.spec.ts
+++ src/app/modules/admin/boosts/modal/rejection-reason-modal.component.spec.ts
@@ -177,4 +177,31 @@ describe('RejectionReasonModalComponent', () => {
 
     expect(comp.close).toHaveBeenCalled();
   });
+
+  it('should filter out deprecated options', () => {
+    expect(comp.reasons).toEqual([
+      { code: 0, label: 'Illegal' },
+      { code: 1, label: 'Explicit' },
+      { code: 2, label: 'Encourages or incites violence' },
+      {
+        code: 3,
+        label: 'Threatens, harasses, bullies or encourages others to do so',
+      },
+      { code: 4, label: 'Personal and confidential information' },
+      {
+        code: 5,
+        label: 'Maliciously targets users (@name, links, images or videos)',
+      },
+      {
+        code: 6,
+        label: 'Impersonates someone in a misleading or deceptive manner',
+      },
+      { code: 7, label: 'Spam' },
+      { code: 8, label: 'Appeals on Boost decisions' },
+      { code: 13, label: 'Original post removed' },
+      { code: 14, label: 'High Boost backlog' },
+      { code: 15, label: 'Support Request' },
+      { code: 16, label: 'Onchain payment failed' },
+    ]);
+  });
 });
