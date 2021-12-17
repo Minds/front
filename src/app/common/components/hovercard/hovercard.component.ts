92d2ff42e6a2ed249a17c0a8cbfc499179f74ebb
Olivia Madrid <oliviajmadrid@gmail.com>
2021-12-16 18:48:42 -0700

    Optimize popper performance

--- src/app/common/components/hovercard/hovercard.component.ts
+++ src/app/common/components/hovercard/hovercard.component.ts
@@ -11,26 +11,35 @@ export class HovercardComponent implements OnInit {
 
   shown: boolean = false;
 
-  // Here we are using offset modifier, but more options
-  // are available at https://popper.js.org/docs/v2
-  popperModifiers: any = {
-    name: 'offset',
-    options: {
-      offset: [0, 0],
+  popperModifiers: Array<any> = [
+    {
+      name: 'eventListeners',
+      enabled: false,
     },
-  };
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 0],
+      },
+    },
+  ];
 
   ngOnInit(): void {
     if (this.offset.length === 2) {
-      this.popperModifiers.options.offset = this.offset;
+      this.popperModifiers.find(
+        x => x.name === 'offset'
+      ).options.offset = this.offset;
     }
   }
 
-  onShown(): void {
+  // Only calculate popper positioning changes when popper is visible
+  popperOnShown() {
+    this.popperModifiers.find(x => x.name === 'eventListeners').enabled = true;
     this.shown = true;
   }
 
-  onHidden(): void {
+  popperOnHide() {
+    this.popperModifiers.find(x => x.name === 'eventListeners').enabled = false;
     this.shown = false;
   }
 }
