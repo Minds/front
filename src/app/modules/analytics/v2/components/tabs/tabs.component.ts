779d67f2f05a2649f53e6268a24f9c22620bc513
Mark Harding <mark@kramnorth.com>
2022-01-05 17:47:05 +0000

    Merge branch 'feat/new-nav-4989' into 'master'

---
+++ src/app/modules/analytics/v2/components/tabs/tabs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'm-analytics__tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.ng.scss'],
+})
+export class AnalyticsTabsComponent {}
