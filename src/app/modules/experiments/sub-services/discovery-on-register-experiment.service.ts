d2ed794c8718508d8c22a3f683a1a35ae8ec834d
Olivia Madrid <oliviajmadrid@gmail.com>
2022-01-19 08:53:27 -0700

    WIP disco on register experiment

---
+++ src/app/modules/experiments/sub-services/discovery-on-register-experiment.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { FeaturesService } from '../../../services/features.service';
+import { ExperimentsService } from '../experiments.service';
+
+/**
+ * Returns whether discovery on register experiment is active, based upon whether
+ * the user has been assigned the experimental variant the feature flag is enabled.
+ */
+@Injectable({ providedIn: 'root' })
+export class DiscoveryOnRegisterExperimentService {
+  constructor(
+    private featuresService: FeaturesService,
+    private experiments: ExperimentsService
+  ) {}
+
+  /**
+   * Returns true if the guest mode experiment is active.
+   * @returns { boolean } whether guest mode experiment is active.
+   */
+  public isActive(): boolean {
+    return true;
+    // ojm uncomment below
+
+    //  return (
+    //    this.featuresService.has('guest-mode') &&
+    //    this.experiments.hasVariation(
+    //      'discovery-on-register',
+    //      'on'
+    //    )
+    //  );
+  }
+
+  public redirectUrl(): string {
+    if (this.isActive()) {
+      return '/discovery/top';
+    } else {
+      return '/newsfeed/subscriptions/latest';
+    }
+  }
+}
