b07e150d3e5d46173626fa48cca096b1e4c5f10e
Mark Harding <mark@kramnorth.com>
2022-01-20 13:43:18 +0000

    (refactor): support for using growthbook features and to handle restrictions via growthbook ui- minds#2905

--- src/app/modules/experiments/experiments.service.ts
+++ src/app/modules/experiments/experiments.service.ts
@@ -1,24 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 
 import { GrowthBook, Experiment } from '@growthbook/growthbook';
 import { ConfigsService } from '../../common/services/configs.service';
 import { AnalyticsService } from '../../services/analytics';
 import { Session } from '../../services/session';
 import { CookieService } from '../../common/services/cookie.service';
+import { Storage } from '../../services/storage';
+import * as snowplow from '@snowplow/browser-tracker';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 export { Experiment } from '@growthbook/growthbook';
 
 @Injectable({ providedIn: 'root' })
-export class ExperimentsService {
+export class ExperimentsService implements OnDestroy {
   growthbook: GrowthBook;
-  experiments: Experiment<unknown>[] = [];
+  routerEventsSubscription: Subscription;
+  loggedInSubscription: Subscription;
 
   constructor(
     private session: Session,
     private configs: ConfigsService,
     private analytics: AnalyticsService,
-    private cookieService: CookieService
-  ) {}
+    private cookieService: CookieService,
+    private storage: Storage,
+    private router: Router
+  ) {
+    this.routerEventsSubscription = this.router.events.subscribe(
+      navigationState => {
+        if (navigationState instanceof NavigationEnd) {
+          this.growthbook?.setAttributes({
+            ...this.growthbook.getAttributes(),
+            route: navigationState.urlAfterRedirects,
+          });
+        }
+      }
+    );
+    this.session.loggedinEmitter.subscribe(isLoggedIn => {
+      this.growthbook?.setAttributes({
+        ...this.growthbook.getAttributes(),
+        loggedIn: isLoggedIn,
+      });
+    });
+  }
 
   /**
    * Initialize Growthbook, only want to do this once
@@ -29,42 +53,22 @@ export class ExperimentsService {
       const userId = this.getUserId();
 
       this.growthbook = new GrowthBook({
-        user: { id: userId },
+        user: {
+          id: userId,
+        },
         trackingCallback: (experiment, result) => {
-          /**
-           * Tracking is only called if force is not used.
-           */
           this.addToAnalytics(experiment.key, result.variationId);
-          // Note: we don't need to tell the backend, as it's the backend that tells us to run experiments
         },
       });
     }
 
-    const experiments = this.configs.get('experiments');
+    this.growthbook.setFeatures(this.configs.get('growthbook')?.features);
 
-    if (experiments && experiments.length > 0) {
-      for (let experiment of experiments) {
-        const originExperiment = Object.assign(experiment);
-
-        // Remap
-        experiment = {
-          key: experiment.experimentId,
-          variations: experiment.variations,
-        };
-
-        // If logged in, we force the experiment
-        if (this.session.isLoggedIn()) {
-          experiment.force = originExperiment.variationId;
-        }
-
-        this.experiments.push(experiment);
-        this.growthbook.run(experiment);
-
-        if (experiment.force != undefined) {
-          this.addToAnalytics(experiment.key, experiment.force);
-        }
-      }
-    }
+    this.growthbook.setAttributes({
+      ...this.configs.get('growthbook')?.attributes,
+      ...this.growthbook.getAttributes(),
+      id: this.getUserId(),
+    });
   }
 
   /**
@@ -74,33 +78,49 @@ export class ExperimentsService {
    * @returns { string } - variation to display.
    */
   public run(key: string): string {
-    for (let experiment of this.experiments) {
-      if (experiment.key === key) {
-        const { value } = this.growthbook.run(experiment);
-        return String(value);
-      }
-    }
+    const result = this.growthbook.feature(key);
 
-    throw 'Could not find experiment with key ' + key;
+    return result.value;
   }
 
+  /**
+   * Adds and event to snowplow
+   * @param experimentId
+   * @param variationId
+   */
   private addToAnalytics(experimentId: string, variationId: number): void {
-    this.analytics.contexts.push({
-      schema: 'iglu:com.minds/growthbook_context/jsonschema/1-0-1',
-      data: {
-        experiment_id: experimentId,
-        variation_id: variationId,
+    // Have we recently pushed this event? (last 24 hours)
+    const CACHE_KEY = `experiment:${experimentId}`;
+    if (parseInt(this.storage.get(CACHE_KEY)) > Date.now() - 86400000) {
+      return; // Do not emit event
+    } else {
+      this.storage.set(CACHE_KEY, Date.now());
+      console.log('MH: submitting growthbook_experiment: ' + CACHE_KEY);
+    }
+
+    //
+    snowplow.trackSelfDescribingEvent({
+      event: {
+        schema: 'iglu:com.minds/growthbook_experiment/jsonschema/1-0-0',
+        data: {
+          experiment_id: experimentId,
+          variation_id: variationId,
+        },
       },
+      context: this.analytics.getContexts(),
     });
   }
 
   /**
    * Return whether an experiment has a given variation state.
    * @param { string } experimentId - experiment key.
-   * @param { string } variation - variation to check, e.g. 'on' or 'off'.
+   * @param { string|number|boolean } variation - variation to check, e.g. 'on' or 'off'.
    * @returns { boolean } - true if params reflect current variation.
    */
-  public hasVariation(experimentId: string, variation: string = 'on'): boolean {
+  public hasVariation(
+    experimentId: string,
+    variation: string | number | boolean = 'on'
+  ): boolean {
     try {
       return this.run(experimentId) === variation;
     } catch (e) {
@@ -132,5 +152,10 @@ export class ExperimentsService {
     }
 
     return cookieValue;
+  }
+
+  ngOnDestroy() {
+    this.routerEventsSubscription?.unsubscribe();
+    this.loggedInSubscription?.unsubscribe();
   }
 }
