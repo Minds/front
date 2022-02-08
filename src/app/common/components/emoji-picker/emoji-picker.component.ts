2279064774900ab698a369894ea6aa2e5183301d
Mark Harding <mark@kramnorth.com>
2022-02-08 10:34:20 +0000

    (fix): move 3rd party emoji module to lazy load

--- src/app/common/components/emoji-picker/emoji-picker.component.ts
+++ src/app/common/components/emoji-picker/emoji-picker.component.ts
@@ -1,6 +1,17 @@
 import { ThemeService } from './../../services/theme.service';
-import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  Compiler,
+  Injector,
+  ChangeDetectorRef,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+} from '@angular/core';
 import { NgxPopperjsContentComponent } from 'ngx-popperjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'm-emojiPicker',
@@ -9,7 +20,10 @@ import { NgxPopperjsContentComponent } from 'ngx-popperjs';
 })
 export class EmojiPickerComponent {
   popperPlacement: string = 'bottom';
-  shown: boolean = false;
+  showSelector = false;
+
+  @ViewChild('emojiPickerOutlet', { read: ViewContainerRef })
+  emojiPickerOutlet: ViewContainerRef;
 
   /**
    * On Post event emitter
@@ -20,10 +34,86 @@ export class EmojiPickerComponent {
   popperModifiers: Array<any> = [];
   @ViewChild('popper') popper: NgxPopperjsContentComponent;
 
-  constructor(public themeService: ThemeService) {}
+  onSelectSubscription: Subscription;
+
+  constructor(
+    public themeService: ThemeService,
+    private compiler: Compiler,
+    private injector: Injector,
+    private cd: ChangeDetectorRef,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
 
   emojiSelect($event) {
     this.emojiSelectEmitter.emit($event.emoji);
     this.popper?.hide();
+  }
+
+  onShowSelector() {
+    this.showSelector = true;
+    this.detectChanges();
+    this.loadEmojiMartComponent();
+  }
+
+  onHideSelector() {
+    this.showSelector = false;
+    this.detectChanges();
+  }
+
+  /**
+   * Asyncronously loads in the emoji mark module
+   */
+  async loadEmojiMartComponent() {
+    // Import our module
+    const { PickerModule, PickerComponent } = await import(
+      '@ctrl/ngx-emoji-mart'
+    );
+
+    const moduleFactory = await this.compiler.compileModuleAsync(PickerModule);
+    const moduleRef = moduleFactory.create(this.injector);
+
+    // Resolves the available components
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+      PickerComponent
+    );
+
+    if (!this.emojiPickerOutlet) {
+      console.error('Could not find emoji picker outlet');
+    }
+
+    // Clear previous component
+    this.emojiPickerOutlet.clear();
+
+    // Attach the component
+    const componentRef = this.emojiPickerOutlet.createComponent(
+      componentFactory
+    );
+
+    componentRef.instance.autoFocus = true;
+    componentRef.instance.isNative = true;
+    componentRef.instance.title = 'Pick your emoji…';
+    componentRef.instance.emoji = 'point_up';
+    componentRef.instance.color = '#1b85d6';
+    componentRef.instance.darkMode = this.themeService.isDark$.getValue();
+
+    this.onSelectSubscription?.unsubscribe();
+
+    this.onSelectSubscription = componentRef.instance.emojiSelect.subscribe(
+      event => {
+        this.emojiSelect(event);
+      }
+    );
+
+    // Trigger change detection
+    this.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.onSelectSubscription?.unsubscribe();
+  }
+
+  detectChanges() {
+    this.cd.markForCheck();
+    this.cd.detectChanges();
   }
 }
