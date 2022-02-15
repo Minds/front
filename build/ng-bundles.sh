4fe2c622e4e48f7e70b73af394e7012484d238e2
Mani Shooshtari <mani@shooshtari.com>
2022-02-15 19:37:59 +0300

    fix a couple of things

--- build/ng-bundles.sh
+++ build/ng-bundles.sh
@@ -5,7 +5,7 @@ EMBED_PATH=${1:-dist/embed}
 SERVER_PATH=${3:-dist/server}
 
 # Allow Node.js to use up to 4G
-export NODE_OPTIONS="--max_old_space_size=4096"
+export NODE_OPTIONS="--max_old_space_size=6144"
 
 # Build global CSS
 npx gulp build.sass --deploy-url="/static/en/"
@@ -14,10 +14,3 @@ npx gulp build.sass --deploy-url="/static/en/"
 npm run build -- --stats-json --outputPath=$BROWSER_PATH
 if [ "$?" != "0" ]; then exit 1; fi
 
-# Build embed
-npm run build:embed -- --stats-json --outputPath=$EMBED_PATH
-if [ "$?" != "0" ]; then exit 1; fi
-
-# Build SSR
-npm run build:ssr -- --outputPath=$SERVER_PATH
-if [ "$?" != "0" ]; then exit 1; fi
