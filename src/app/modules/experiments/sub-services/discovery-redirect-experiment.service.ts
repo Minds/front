dac44d5ae513ed6eb9d0ffe6d9d78c40a70dfd9d
Ben Hayward <ben@minds.com>
2022-03-03 14:12:53 +0000

    Experiment to redirect registered users to channel suggestions minds#2960

--- src/app/modules/experiments/sub-services/discovery-redirect-experiment.service.ts
+++ src/app/modules/experiments/sub-services/discovery-redirect-experiment.service.ts
@@ -9,7 +9,6 @@ import * as moment from 'moment';
  * based upon whether the user has recently registered from homepage
  * and has been assigned the experimental variant
  */
-
 @Injectable({ providedIn: 'root' })
 export class DiscoveryRedirectExperimentService {
   constructor(
@@ -26,22 +25,30 @@ export class DiscoveryRedirectExperimentService {
     return this.experiments.hasVariation('discovery-redirect', true);
   }
 
+  /**
+   * Gets URL to be redirected to on registration, varying
+   * based on which experiments are active.
+   * @returns { string } url to be redirected to on registration
+   */
   public getRedirectUrl(): string {
     if (
       this.isRegistration() &&
-      this.isOnHomepage() &&
+      this.isOnRedirectablePage() &&
       this.shouldUseExperiment()
     ) {
-      return '/discovery/top';
+      // sub-experiment where we redirect to discovery/suggestions/user or /discovery/top.
+      return this.isChannelSuggestionsRedirectExperimentActive()
+        ? '/discovery/suggestions/user'
+        : '/discovery/top';
     } else {
+      // default redirect
       return '/newsfeed/subscriptions';
     }
   }
 
   /**
-   *
-   * @returns If user registered their account within the
-   * last 5 minutes
+   * Whether user has registered their account in the last 5 minutes.
+   * @returns { boolean } true if user registered their account within the last 5 minutes.
    */
   private isRegistration(): boolean {
     const user = this.session.getLoggedInUser();
@@ -54,7 +61,23 @@ export class DiscoveryRedirectExperimentService {
     }
   }
 
-  public isOnHomepage(): boolean {
-    return this.router.url === '/' || this.router.url === '/about';
+  /**
+   * Returns true if the user is on a page should be redirected from.
+   * @returns { boolean } true if the user is on a page should be redirected from.
+   */
+  private isOnRedirectablePage(): boolean {
+    return (
+      this.router.url === '/' ||
+      this.router.url === '/about' ||
+      this.router.url === '/register'
+    );
+  }
+
+  /**
+   * Returns true if the channel-suggestions-redirect sub-experiment is active.
+   * @returns { boolean } whether channel-suggestions-redirect sub-experiment is active.
+   */
+  public isChannelSuggestionsRedirectExperimentActive(): boolean {
+    return this.experiments.hasVariation('channel-suggestions-redirect', true);
   }
 }
