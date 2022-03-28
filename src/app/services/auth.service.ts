b3f9358a3e5d6dd82069f3c19731cc88539d73d7
Mani Shooshtari <mani@shooshtari.com>
2022-03-28 17:54:00 +0300

    unregister on logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Client } from './api/client';
+import { PushNotificationService } from './push-notification.service';
 import { Session } from './session';
 
 @Injectable()
 export class AuthService {
   constructor(
     protected readonly client: Client,
-    protected readonly session: Session
+    protected readonly session: Session,
+    protected readonly pushNotificationService: PushNotificationService,
   ) {}
 
-  logout(closeAll: boolean = false): boolean {
+  async logout(closeAll: boolean = false): Promise<boolean> {
     let endpoint: string = 'api/v1/authenticate';
 
     if (closeAll) {
       endpoint += '/all';
     }
 
-    this.client.delete(endpoint);
+    await this.pushNotificationService.unregisterToken();
+    await this.client.delete(endpoint);
     this.session.logout();
 
     return true;
