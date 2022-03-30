f4cbde7d177047e6a57a2e26c7993f9225311d6b
Ben Hayward <ben@minds.com>
2022-03-30 16:52:40 +0100

    Add analytic clicks to dismiss button

--- src/app/modules/notices/template/feed-notice.component.ts
+++ src/app/modules/notices/template/feed-notice.component.ts
@@ -26,7 +26,8 @@ export class FeedNoticeComponent {
   // Icon to be shown.
   @Input() icon: string = 'info';
 
-  // id to append to data-ref attribute so we know which notice has been dismissed.
+  // id to append to data-ref attribute so we can report
+  // to analytics which notice has been dismissed.
   @Input() dataRefId: string = 'unknown';
 
   // Makes notice to stick to the top of the feed.
