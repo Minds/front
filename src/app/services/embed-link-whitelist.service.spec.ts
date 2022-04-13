c8102fed5af14e4ec4222ca670168a52fb069c40
Ben <ben@minds.com>
2022-04-13 08:47:42 +0000

    Odysee & Rumble video embeds #4540

---
+++ src/app/services/embed-link-whitelist.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+import { EmbedLinkWhitelistService } from './embed-link-whitelist.service';
+
+describe('EmbedLinkWhitelistService', () => {
+  let service: EmbedLinkWhitelistService;
+
+  beforeEach(() => {
+    service = TestBed.inject(EmbedLinkWhitelistService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should determine an Odysee video embed link is whitelisted', () => {
+    expect(
+      service.isWhitelisted(
+        'https://odysee.com/$/embed/odyseeplaylists/dbd9a2dcca07d8fdd78e8281b764dedc839c2d3a'
+      )
+    ).toBeTruthy();
+  });
+
+  it('should NOT determine a direct Odysee video link is whitelisted', () => {
+    expect(
+      service.isWhitelisted(
+        'https://odysee.com/odyseeplaylists:dbd9a2dcca07d8fdd78e8281b764dedc839c2d3a'
+      )
+    ).toBeFalsy();
+  });
+
+  it('should determine a Rumble video embed link is whitelisted', () => {
+    expect(
+      service.isWhitelisted('https://rumble.com/embed/v7z2oy/?pub=4')
+    ).toBeTruthy();
+  });
+
+  it('should NOT determine a direct Rumble video link is whitelisted', () => {
+    expect(
+      service.isWhitelisted('https://rumble.com/val8vm-how-to-use-rumble.html')
+    ).toBeFalsy();
+  });
+
+  it('should identify a link is not whitelisted', () => {
+    expect(
+      service.isWhitelisted('https://www.youtube.com/watch?v=jNQXAC9IVRw')
+    ).toBeFalsy();
+  });
+});
