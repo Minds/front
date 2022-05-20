4543dbda02239d6c1ee6efacee9ef86137a627c3
Mani Shooshtari <mani@shooshtari.com>
2022-05-20 15:44:42 +0300

    better function

--- src/app/helpers/is-brave.ts
+++ src/app/helpers/is-brave.ts
@@ -5,12 +5,10 @@ export default function isBrave() {
       // @ts-ignore
       if (window.navigator.brave.isBrave.name === 'isBrave') {
         return true;
-      } else {
-        return false;
       }
-    } else {
-      return false;
     }
+
+    return false;
   } catch (e) {
     return false;
   }
