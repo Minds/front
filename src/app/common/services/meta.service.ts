b02724199dc6a6b6f5ffd17ec822b63600c38cb2
Mani Shooshtari <mani@shooshtari.com>
2022-05-26 15:42:48 +0300

    add notif count in the title

--- src/app/common/services/meta.service.ts
+++ src/app/common/services/meta.service.ts
@@ -4,6 +4,7 @@ import { SiteService } from './site.service';
 import { Location } from '@angular/common';
 import { ConfigsService } from './configs.service';
 import { DOCUMENT } from '@angular/common';
+import maxNum from 'src/app/helpers/max';
 
 const DEFAULT_META_TITLE = 'Minds';
 const DEFAULT_META_DESCRIPTION = '...';
@@ -287,7 +288,7 @@ export class MetaService {
 
   private applyTitle(): void {
     if (this.counter) {
-      this.titleService.setTitle(`(*) ${this.title}`);
+      this.titleService.setTitle(`(${maxNum(this.counter)}) ${this.title}`);
     } else {
       this.titleService.setTitle(this.title);
     }
