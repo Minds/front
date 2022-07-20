8c21d3480e548dca4b0e1bf0aa6b7bd673955dd3
Fausto Arcidiacono <fausto@minds.com>
2022-07-20 09:12:52 +0100

    (feat): Change report reasons for admin

--- src/app/modules/report/juryduty/session/content.component.ts
+++ src/app/modules/report/juryduty/session/content.component.ts
@@ -1,21 +1,33 @@
-import { Component, Input, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, Input, PLATFORM_ID } from '@angular/core';
 
 import { JurySessionService } from './session.service';
 import { isPlatformBrowser } from '@angular/common';
 import { FormToastService } from '../../../../common/services/form-toast.service';
+import {
+  Reason,
+  ReportService,
+} from '../../../../common/services/report.service';
 
 @Component({
   selector: 'm-juryDutySession__content',
   templateUrl: 'content.component.html',
+  styleUrls: ['content.component.scss'],
 })
 export class JuryDutySessionContentComponent {
   @Input() report;
   decided: boolean = false;
+  changeReportReason: boolean = false;
+  reasons = this.reportService.reasons;
+
+  private previousSelectedReason: Reason | null = null;
+  private selectedReason: Reason | null = null;
+  private selectedSubReason: Reason | null = null;
 
   constructor(
     private sessionService: JurySessionService,
     private toast: FormToastService,
-    @Inject(PLATFORM_ID) protected platformId: Object
+    @Inject(PLATFORM_ID) protected platformId: Object,
+    private reportService: ReportService
   ) {}
 
   getReasonString(report) {
@@ -62,14 +74,54 @@ export class JuryDutySessionContentComponent {
     if (!isPlatformBrowser(this.platformId)) {
       return;
     }
-    if (confirm('Are you sure?')) {
+    let confirmMessage = 'Are you sure?';
+    let adminReasonOverride = null;
+    if (this.selectedReason) {
+      adminReasonOverride = `${this.selectedReason.value}`;
+      let adminReasonOverrideLabel = this.selectedReason.label;
+
+      if (this.selectedSubReason) {
+        adminReasonOverrideLabel = `${adminReasonOverrideLabel} - ${this.selectedSubReason.label}`;
+        adminReasonOverride = `${adminReasonOverride}.${this.selectedSubReason.value}`;
+      }
+      confirmMessage = `You have selected to override the report reason from ${this.getReasonString(
+        this.report
+      )} to ${adminReasonOverrideLabel}
+      \nAre you sure you want to uphold this report?`;
+    }
+
+    if (confirm(confirmMessage)) {
       try {
-        await this.sessionService.uphold(this.report);
+        await this.sessionService.uphold(this.report, adminReasonOverride);
         this.decided = true;
       } catch (e) {
         console.error(e);
         this.toast.error(e?.message ?? 'An unknown error has occurred');
       }
     }
+  }
+
+  toggleChangeReportReason(): void {
+    this.changeReportReason = !this.changeReportReason;
+    this.selectedReason = this.changeReportReason ? this.reasons[0] : null;
+    this.selectedSubReason = this.selectedReason.hasMore
+      ? (this.selectedReason.reasons[0] as Reason)
+      : null;
+  }
+
+  updateReasonSelection($event): void {
+    if (($event.option as Reason) === this.previousSelectedReason) {
+      return;
+    }
+
+    this.previousSelectedReason = this.selectedReason;
+    this.selectedReason = $event.option as Reason;
+    this.selectedSubReason = this.selectedReason.hasMore
+      ? (this.selectedReason.reasons[0] as Reason)
+      : null;
+  }
+
+  updateSubReasonSelection($event): void {
+    this.selectedSubReason = $event.option as Reason;
   }
 }
--- src/app/modules/report/juryduty/session/session.component.ts
+++ src/app/modules/report/juryduty/session/session.component.ts
@@ -1,10 +1,11 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Session } from '../../../../services/session';
 
 @Component({
   selector: 'm-juryduty__session',
   templateUrl: 'session.component.html',
+  styleUrls: ['session.component.scss'],
 })
 export class JuryDutySessionComponent implements AfterViewInit {
   paramsSubscription;
