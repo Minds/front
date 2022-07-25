e93d989a4b815438be0fc4046c8618c86638e09f
Mani Shooshtari <mani@shooshtari.com>
2022-07-25 16:55:35 +0200

    fix feed header padding

--- src/app/common/components/feed-header/feed-header.component.ts
+++ src/app/common/components/feed-header/feed-header.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { ExperimentsService } from '../../../modules/experiments/experiments.service';
+import { ActivityV2ExperimentService } from '../../../modules/experiments/sub-services/activity-v2-experiment.service';
 
 @Component({
   selector: 'm-feedHeader',
   templateUrl: './feed-header.component.html',
   styleUrls: ['./feed-header.component.ng.scss'],
 })
-export class FeedHeaderComponent {}
+export class FeedHeaderComponent {
+  constructor(
+    public experiments: ExperimentsService,
+    private activityV2Experiment: ActivityV2ExperimentService
+  ) {}
+
+  @HostBinding('class.m-feedHeader--activityV2')
+  get activityV2Feature(): boolean {
+    return this.activityV2Experiment.isActive();
+  }
+}
