64e3c8895fa03cf7078793edf73ec76000604f34
Mani Shooshtari <mani@shooshtari.com>
2022-07-26 16:38:16 +0200

    remove unused code

--- src/app/services/storage/memory/memory-storage.service.ts
+++ src/app/services/storage/memory/memory-storage.service.ts
@@ -2,16 +2,7 @@ import ApiResource from '../user/tables/ApiResource';
 
 export class MemoryStorageService {
   private apiResources: Map<string, ApiResource> = new Map();
-  private feedOffsets: Map<string, number> = new Map();
   private feedState: Map<string, any> = new Map();
-
-  getFeedOffset(feedUrl: string, routeUrl: string) {
-    return this.feedOffsets.get(feedUrl + routeUrl);
-  }
-
-  setFeedOffset(feedUrl: string, routeUrl: string, offset: number) {
-    return this.feedOffsets.set(feedUrl + routeUrl, offset);
-  }
 
   getFeedState(feedUrl: string, routeUrl: string): any {
     return this.feedState.get(feedUrl + routeUrl);
