d8be0103b0672e99e6d8e10a7115983e44fd3894
Olivia Madrid <olivia@minds.com>
2022-08-11 08:32:26 +0000

    Remove legacy boost button component #5683

--- src/app/common/components/boost-button/boost.ts
+++ src/app/common/components/boost-button/boost.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { Session } from '../../../../services/session';
+import { Session } from '../../../services/session';
 
-import { BoostModalLazyService } from '../../../boost/modal/boost-modal-lazy.service';
-import { FeaturesService } from '../../../../services/features.service';
-import { ModalService } from '../../../../services/ux/modal.service';
+import { BoostModalLazyService } from '../../../modules/boost/modal/boost-modal-lazy.service';
+import { FeaturesService } from '../../../services/features.service';
+import { ModalService } from '../../../services/ux/modal.service';
 
 /**
  * Boost button that opens boost modal when clicked.
  * Seen on the bottom bar of blogs.
  */
 @Component({
-  selector: 'minds-button-boost',
+  selector: 'm-boostButton',
   inputs: ['object'],
   template: `
     <m-button size="small" color="blue" (onAction)="boost()">
@@ -30,7 +30,9 @@ export class BoostButton {
     private modalService: ModalService,
     private boostLazyModal: BoostModalLazyService,
     private features: FeaturesService
-  ) {}
+  ) {
+    console.log('ojm boost button');
+  }
 
   /**
    * Open boost modal
