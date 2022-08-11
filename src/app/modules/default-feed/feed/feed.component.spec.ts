760c70b8c3670491f08faed5d847f3f4fce3cf6a
Ben <ben@minds.com>
2022-08-11 08:33:57 +0000

    Discovery boosts minds#3280

--- src/app/modules/default-feed/feed/feed.component.spec.ts
+++ src/app/modules/default-feed/feed/feed.component.spec.ts
@@ -4,11 +4,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MockComponent, MockService } from '../../../utils/mock';
 import { FeedsService } from '../../../common/services/feeds.service';
-import { GlobalScrollService } from '../../../services/ux/global-scroll.service';
 import { By } from '@angular/platform-browser';
-import { Session } from './../../../services/session';
 import { ExperimentsService } from '../../experiments/experiments.service';
 import { BehaviorSubject, of } from 'rxjs';
+import { DiscoveryBoostExperimentService } from '../../experiments/sub-services/discovery-boost-experiment.service';
+import { DismissalService } from '../../../common/services/dismissal.service';
 
 describe('DefaultFeedComponent', () => {
   let comp: DefaultFeedComponent;
@@ -71,18 +71,24 @@ describe('DefaultFeedComponent', () => {
         ],
         imports: [RouterTestingModule, ReactiveFormsModule],
         providers: [
-          { provide: FeedsService, useValue: feedsServiceMock },
-          {
-            provide: GlobalScrollService,
-            useValue: MockService(GlobalScrollService),
-          },
-          { provide: Session, useValue: MockService(Session) },
           {
             provide: ExperimentsService,
             useValue: MockService(ExperimentsService),
           },
+          {
+            provide: DiscoveryBoostExperimentService,
+            useValue: MockService(DiscoveryBoostExperimentService),
+          },
+          {
+            provide: DismissalService,
+            useValue: MockService(DismissalService),
+          },
         ],
-      }).compileComponents();
+      })
+        .overrideProvider(FeedsService, {
+          useValue: feedsServiceMock,
+        })
+        .compileComponents();
     })
   );
 
@@ -154,5 +160,45 @@ describe('DefaultFeedComponent', () => {
     );
     expect(inlineElements).toBeTruthy();
     expect(inlineElements.length).toBe(4); // pos 6, 12, 18, 24
+  });
+
+  it('should determine whether boost should NOT be shown because discovery boost experiment is off', () => {
+    (comp as any).discoveryBoostExperiment.isActive.and.returnValue(false);
+
+    expect(comp.shouldShowBoostInPosition(3)).toBe(false);
+    expect((comp as any).discoveryBoostExperiment.isActive).toHaveBeenCalled();
+  });
+
+  it('should determine whether boost should be shown in position 3', () => {
+    (comp as any).discoveryBoostExperiment.isActive.and.returnValue(true);
+
+    expect(comp.shouldShowBoostInPosition(3)).toBe(true);
+    expect((comp as any).discoveryBoostExperiment.isActive).toHaveBeenCalled();
+  });
+
+  it('should determine whether boost should be shown in a non 0 position divisible by 5', () => {
+    (comp as any).discoveryBoostExperiment.isActive.and.returnValue(true);
+
+    expect(comp.shouldShowBoostInPosition(5)).toBe(true);
+    expect(comp.shouldShowBoostInPosition(10)).toBe(true);
+    expect(comp.shouldShowBoostInPosition(15)).toBe(true);
+    expect(comp.shouldShowBoostInPosition(20)).toBe(true);
+    expect(comp.shouldShowBoostInPosition(25)).toBe(true);
+    expect(comp.shouldShowBoostInPosition(9995)).toBe(true);
+
+    expect((comp as any).discoveryBoostExperiment.isActive).toHaveBeenCalled();
+  });
+
+  it('should determine whether boost should NOT be shown in a non 0 position that is NOT 3 OR divisible by 5', () => {
+    (comp as any).discoveryBoostExperiment.isActive.and.returnValue(true);
+
+    expect(comp.shouldShowBoostInPosition(1)).toBe(false);
+    expect(comp.shouldShowBoostInPosition(2)).toBe(false);
+    expect(comp.shouldShowBoostInPosition(4)).toBe(false);
+    expect(comp.shouldShowBoostInPosition(6)).toBe(false);
+    expect(comp.shouldShowBoostInPosition(9994)).toBe(false);
+    expect(comp.shouldShowBoostInPosition(9996)).toBe(false);
+
+    expect((comp as any).discoveryBoostExperiment.isActive).toHaveBeenCalled();
   });
 });
