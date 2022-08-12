62238c0e4242e6aae40b0bc16d27dad03a40c4ed
Mani Shooshtari <mani@shooshtari.com>
2022-08-12 11:18:09 +0200

    (fix) tests

--- src/app/common/api/api-resource-mock.ts
+++ src/app/common/api/api-resource-mock.ts
@@ -1,7 +1,7 @@
+import { apiServiceMock } from '../../mocks/services/api-mock.spec';
 import { StorageV2 } from '../../services/storage/v2';
-import { MockService } from '../../utils/mock';
 import { QueryRef } from './api-resource.service';
-import { ApiRequestMethod, ApiService } from './api.service';
+import { ApiRequestMethod } from './api.service';
 
 export let experimentsServiceMock = new (function() {
   this.hasVariation = jasmine.createSpy('hasVariation');
@@ -9,15 +9,9 @@ export let experimentsServiceMock = new (function() {
 
 const apiResourceMock = {
   query: () =>
-    // @ts-ignore
-    new QueryRef(
-      MockService(ApiService),
-      new StorageV2(),
-      experimentsServiceMock,
-      {
-        method: ApiRequestMethod.GET,
-      }
-    ),
+    new QueryRef(apiServiceMock, new StorageV2(), experimentsServiceMock, '', {
+      method: ApiRequestMethod.GET,
+    }),
 };
 
 export default apiResourceMock;
