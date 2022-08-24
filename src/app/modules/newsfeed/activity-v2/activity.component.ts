1dfc31add1f85fed746bc846d34b88cb73540d48
Olivia Madrid <oliviajmadrid@gmail.com>
2022-08-23 19:15:51 -0600

    Boost console activity v2 cleanup

--- src/app/modules/newsfeed/activity-v2/activity.component.ts
+++ src/app/modules/newsfeed/activity-v2/activity.component.ts
@@ -113,6 +113,9 @@ export class ActivityV2Component implements OnInit, AfterViewInit, OnDestroy {
   @HostBinding('class.m-activity--noOwnerBlock')
   noOwnerBlock: boolean;
 
+  @HostBinding('class.m-activity--noToolbar')
+  noToolbar: boolean;
+
   @HostBinding('class.m-activity--isFeed')
   isFeed: boolean;
 
@@ -151,6 +154,7 @@ export class ActivityV2Component implements OnInit, AfterViewInit, OnDestroy {
     this.isFixedHeight = this.service.displayOptions.fixedHeight;
     this.isFixedHeightContainer = this.service.displayOptions.fixedHeightContainer;
     this.noOwnerBlock = !this.service.displayOptions.showOwnerBlock;
+    this.noToolbar = !this.service.displayOptions.showToolbar;
     this.isFeed = this.service.displayOptions.isFeed;
     this.isSidebarBoost = this.service.displayOptions.isSidebarBoost;
     this.isModal = this.service.displayOptions.isModal;
