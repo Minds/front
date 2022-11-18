bb1eaa6077e0b318658851076da674b7b6d6518f
Mark Harding <mark@minds.com>
2022-11-18 10:42:08 +0000

    Inline twitter auth flow - #5811

--- src/app/modules/twitter/modal/connect-twitter-modal.component.spec.ts
+++ src/app/modules/twitter/modal/connect-twitter-modal.component.spec.ts
@@ -63,14 +63,7 @@ describe('ConnectTwitterModalComponent', () => {
     fixture = TestBed.createComponent(ConnectTwitterModalComponent);
     comp = fixture.componentInstance;
 
-    (comp as any).twitterConnection.isConnected$.next(false);
-    (comp as any).twitterConnection.authUrlRequestInProgress$.next(false);
-    (comp as any).twitterConnection.authUrl$.next('/auth');
-    (comp as any).twitterConnection.postAuthRedirectPath$.next(
-      '/redirect-path'
-    );
-
-    (comp as any).assignWindowLocation = () => void 0;
+    (comp as any).openTwitterAuthTab = () => void 0;
 
     if (fixture.isStable()) {
       done();
@@ -91,17 +84,9 @@ describe('ConnectTwitterModalComponent', () => {
 
     expect(comp.bodyText).toBe(defaultBodyText);
 
-    comp.setModalData({ bodyText: newBodyText });
+    comp.setModalData({ bodyText: newBodyText, onConnect: () => {} });
     fixture.detectChanges();
 
     expect(comp.bodyText).toBe(newBodyText);
   });
-
-  it('should get auth url and assign new location on connect click', fakeAsync(() => {
-    comp.onConnectClick(null);
-    tick();
-    expect(
-      (comp as any).twitterConnection.postAuthRedirectPath$.getValue()
-    ).toBe('/supermind/inbox');
-  }));
 });
