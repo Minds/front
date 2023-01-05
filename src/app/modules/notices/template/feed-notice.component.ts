dcf773899325f4b166aea834593f0cb9014b575c
Ben Hayward <ben@minds.com>
2023-01-05 13:13:39 +0000

    Analytics for feed notice views minds#3652

--- src/app/modules/notices/template/feed-notice.component.ts
+++ src/app/modules/notices/template/feed-notice.component.ts
@@ -1,11 +1,16 @@
 import {
   Component,
+  ElementRef,
   EventEmitter,
-  HostBinding,
   Input,
+  OnDestroy,
+  OnInit,
   Output,
 } from '@angular/core';
-import { FeedNoticeService } from '../services/feed-notice.service';
+import { Subscription } from 'rxjs';
+import { debounceTime, filter } from 'rxjs/operators';
+import { IntersectionObserverService } from '../../../common/services/interception-observer.service';
+import { AnalyticsService } from '../../../services/analytics';
 
 /**
  * Feeds notices component. To use, provide content via ng-content.
@@ -20,8 +25,23 @@ import { FeedNoticeService } from '../services/feed-notice.service';
   templateUrl: 'feed-notice.component.html',
   styleUrls: ['feed-notice.component.ng.scss'],
 })
-export class FeedNoticeComponent {
-  constructor(private service: FeedNoticeService) {}
+export class FeedNoticeComponent implements OnInit, OnDestroy {
+  // emits on feed notice entering and leaving the viewport.
+  private interceptionObserverSubscription: Subscription;
+
+  constructor(
+    private el: ElementRef,
+    private interceptionObserver: IntersectionObserverService,
+    private analytics: AnalyticsService
+  ) {}
+
+  ngOnInit(): void {
+    this.setupInterceptionObserver();
+  }
+
+  ngOnDestroy(): void {
+    this.interceptionObserverSubscription?.unsubscribe();
+  }
 
   // Whether component is dismissible or not (shows close button).
   @Input() dismissible: boolean = true;
@@ -30,11 +50,31 @@ export class FeedNoticeComponent {
   @Input() icon: string = 'info';
 
   // id to append to data-ref attribute so we can report
-  // to analytics which notice has been dismissed.
+  // to analytics which notice has been interacted with.
   @Input() dataRefId: string = 'unknown';
 
   // Fired on dismiss click.
   @Output() dismissClick: EventEmitter<boolean> = new EventEmitter<boolean>(
     false
   );
+
+  /**
+   * Setup InterceptionObserver to watch for feed notice entering
+   * and leaving the viewport - once one has entered for more than a
+   * second, calls to log a view event in analytics.
+   * @returns { void }
+   */
+  private setupInterceptionObserver(): void {
+    if (this.interceptionObserverSubscription) {
+      console.warn('Attempted to re-register FeedNotice InterceptionObserver');
+      return;
+    }
+
+    this.interceptionObserverSubscription = this.interceptionObserver
+      .createAndObserve(this.el)
+      .pipe(debounceTime(1000), filter(Boolean))
+      .subscribe((isVisible: boolean) => {
+        this.analytics.trackView(`feed-notice-${this.dataRefId}`);
+      });
+  }
 }
