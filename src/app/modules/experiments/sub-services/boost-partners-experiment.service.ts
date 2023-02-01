a2d3b87a8aae3ebbec39d43db39311b76ce5dfad
Olivia Madrid <oliviajmadrid@gmail.com>
2023-01-31 18:19:11 -0700

    DRAFT: boosts in channel page

---
+++ src/app/modules/experiments/sub-services/boost-partners-experiment.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ExperimentsService } from '../experiments.service';
+
+/**
+ * Central service for handling logic around the boost partners experiment.
+ */
+@Injectable({ providedIn: 'root' })
+export class BoostPartnersExperimentService {
+  constructor(private experiments: ExperimentsService) {}
+
+  /**
+   * Whether boost partners experiment is active.
+   * @returns { boolean } - true if experiment is active.
+   */
+  public isActive(): boolean {
+    // ojm remove
+    return true;
+    return this.experiments.hasVariation('epic-303-boost-partners', true);
+  }
+}
