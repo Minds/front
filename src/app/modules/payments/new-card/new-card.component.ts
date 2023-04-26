d0bc09429a8c0af0062a5f130e44b43751722654
Ben Hayward <ben@minds.com>
2023-04-26 12:28:00 +0100

    front-5785-stripe-checkout

--- src/app/modules/payments/new-card/new-card.component.ts
+++ src/app/modules/payments/new-card/new-card.component.ts
@@ -1,31 +1,17 @@
 import {
   Component,
   EventEmitter,
-  Input,
   Output,
   ChangeDetectorRef,
   ChangeDetectionStrategy,
-  ViewChild,
-  ElementRef,
   Inject,
   PLATFORM_ID,
 } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-
-import { Client } from '../../../services/api';
-import { WalletService } from '../../../services/wallet';
-import { Storage } from '../../../services/storage';
-import { Session } from '../../../services/session';
 import { ConfigsService } from '../../../common/services/configs.service';
 import { isPlatformBrowser } from '@angular/common';
-import { ExperimentsService } from '../../experiments/experiments.service';
 
 /**
- * Form that uses a Stripe iframe.
- * Allows uers to add a new credit/debit card
- * to use with Minds Pay.
- *
- * See it in the Minds Pay modal.
+ * Loading panel for use while going to stripe checkout to add a new card.
  */
 @Component({
   selector: 'm-payments__newCard',
@@ -34,59 +20,35 @@ import { ExperimentsService } from '../../experiments/experiments.service';
   styleUrls: ['./new-card.component.ng.scss'],
 })
 export class PaymentsNewCard {
-  intentKey: string = '';
-  intentId: string = '';
-  @ViewChild('iframe') iframe: ElementRef;
   @Output() completed: EventEmitter<void> = new EventEmitter();
-  _opts: any;
-
-  /**
-   * Feature flag, set on ngOnInit.
-   */
-  v2: boolean;
+  private _opts: any;
 
   constructor(
-    public session: Session,
-    public client: Client,
     public cd: ChangeDetectorRef,
-    private sanitizer: DomSanitizer,
     private configs: ConfigsService,
-    @Inject(PLATFORM_ID) private platformId: Object,
-    private experimentsService: ExperimentsService
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
-  setModalData(opts: { onCompleted: () => void }) {
-    this._opts = opts;
-  }
-
   ngOnInit() {
-    this.v2 = this.experimentsService.hasVariation(
-      'front-5785-stripe-checkout',
-      true
-    );
-    if (this.v2) {
-      if (isPlatformBrowser(this.platformId)) {
-        this.goToCheckout();
-      }
-    } else {
-      // V1
-      window.addEventListener(
-        'message',
-        msg => {
-          if (msg.data === 'completed-saved-card') {
-            this.saveCard();
-          }
-        },
-        false
-      );
-      this.setupIntent();
+    if (isPlatformBrowser(this.platformId)) {
+      this.goToCheckout();
     }
   }
 
   /**
-   * Will open stripe checkout in a new window and poll for closed event
+   * Set modal data
+   * @param opts - opts containing onCompleted function.
+   * @returns { void }
    */
-  goToCheckout(): void {
+  setModalData(opts: { onCompleted: () => void }): void {
+    this._opts = opts;
+  }
+
+  /**
+   * Will open stripe checkout in a new window and poll for closed event.
+   * @returns { void }
+   */
+  public goToCheckout(): void {
     const windowRef = window.open(
       this.configs.get('site_url') + 'api/v3/payments/stripe/checkout/setup',
       '_blank'
@@ -102,35 +64,11 @@ export class PaymentsNewCard {
     }, 500);
   }
 
-  async setupIntent() {
-    const { intent } = <any>(
-      await this.client.put('api/v2/payments/stripe/intents/setup')
-    );
-    this.intentKey = intent.client_secret;
-    this.intentId = intent.id;
-    this.detectChanges();
-  }
-
-  get url() {
-    const url =
-      'https://checkout.minds.com/stripe?intent_key=' + this.intentKey;
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
-
-  async saveCard() {
-    const { success } = <any>await this.client.post(
-      'api/v2/payments/stripe/paymentmethods/apply',
-      {
-        intent_id: this.intentId,
-      }
-    );
-    this.intentKey = '';
-    this.completed.next();
-    this._opts?.onCompleted?.();
-    this.detectChanges();
-  }
-
-  detectChanges() {
+  /**
+   * Run change detection manually.
+   * @returns { void }
+   */
+  public detectChanges(): void {
     this.cd.markForCheck();
     this.cd.detectChanges();
   }
