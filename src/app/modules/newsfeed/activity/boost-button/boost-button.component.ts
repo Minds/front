f2bee9d8d2db417ce8acd0efe84aa5a2b57c07bf
Ben Hayward <ben@minds.com>
2023-05-18 12:48:33 +0100

    Tidying

--- src/app/modules/newsfeed/activity/boost-button/boost-button.component.ts
+++ src/app/modules/newsfeed/activity/boost-button/boost-button.component.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnInit,
-} from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { NgStyleValue } from '../../../../common/types/angular.types';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 /**
  * Boost button used in activity toolbar.
@@ -17,24 +10,4 @@ import { NgStyleValue } from '../../../../common/types/angular.types';
   styleUrls: ['./boost-button.component.ng.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ActivityBoostButtonComponent {
-  @Input()
-  object = {
-    guid: undefined,
-  };
-  boostRecommendationsSubscription: Subscription;
-  shouldShowTooltip$: BehaviorSubject<boolean> = new BehaviorSubject(false);
-  shouldShowShimmer$: BehaviorSubject<boolean> = new BehaviorSubject(false);
-
-  /**
-   * Get style for settings tooltip bubble.
-   * @returns { NgStyleValue } - ngStyle value.
-   */
-  get tooltipBubbleStyle(): NgStyleValue {
-    return {
-      right: 0,
-      width: '400px',
-      bottom: '35px',
-    };
-  }
-}
+export class ActivityBoostButtonComponent {}
