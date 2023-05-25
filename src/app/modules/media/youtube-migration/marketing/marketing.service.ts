f8707ded80fe55ef567bd7fb66ed21b6331213af
Ben Hayward <ben@minds.com>
2023-05-25 12:14:23 +0100

    Missing service

---
+++ src/app/modules/media/youtube-migration/marketing/marketing.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Apollo, QueryRef, gql } from 'apollo-angular';
+import { MARKETING_PAGE_QUERY_FULL } from '../../../../common/services/strapi/marketing-page/marketing-page.constants';
+import { MarketingAttributes } from '../../../../common/services/strapi/marketing-page/marketing-page.types';
+
+// query to get page copy.
+export const YOUTUBE_MIGRATION_MARKETING_PAGE_QUERY = gql`
+  {
+    youtubeMigrationMarketingPage {
+      ${MARKETING_PAGE_QUERY_FULL}
+    }
+  }
+`;
+
+export type YoutubeMigrationMarketingPageResponse = {
+  youtubeMigrationMarketingPage: {
+    data: {
+      attributes: MarketingAttributes;
+    };
+  };
+};
+
+/**
+ * Service for the getting of content from our CMS.
+ */
+@Injectable({ providedIn: 'root' })
+export class YoutubeMigrationMarketingService {
+  public readonly copyData: QueryRef<
+    YoutubeMigrationMarketingPageResponse
+  > = this.apollo.watchQuery<YoutubeMigrationMarketingPageResponse>({
+    query: YOUTUBE_MIGRATION_MARKETING_PAGE_QUERY,
+  });
+
+  constructor(private apollo: Apollo) {}
+}
