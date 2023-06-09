69cf538a173911fedb25958b2e434fc65aadb0cd
Olivia Madrid <olivia@minds.com>
2023-06-09 10:41:31 +0000

    Refactor activity flag row #5945

--- src/app/modules/newsfeed/activity/activity.component.ts
+++ src/app/modules/newsfeed/activity/activity.component.ts
@@ -128,10 +128,6 @@ export class ActivityComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild(ClientMetaDirective) clientMeta: ClientMetaDirective;
 
-  // Whether the boost/remind/supermind/mutualSubscriptions flag
-  // should appear on top of owner block
-  showFlagRow: boolean = false;
-
   @Output() previousBoost: EventEmitter<any> = new EventEmitter();
   @Output() nextBoost: EventEmitter<any> = new EventEmitter();
 
@@ -185,27 +181,6 @@ export class ActivityComponent implements OnInit, AfterViewInit, OnDestroy {
         this.cd.detectChanges();
       }
     );
-
-    this.entitySubscription = this.service.entity$.subscribe(entity => {
-      if (!entity) {
-        return;
-      }
-
-      const notInBoostRotator = !this.service.displayOptions.boostRotatorMode;
-      const boosted = entity.boosted;
-      const reminded = entity.remind_users && entity.remind_users.length;
-
-      const isSupermindOffer =
-        entity.supermind &&
-        !entity.supermind.is_reply &&
-        entity.supermind.receiver_user;
-
-      this.showFlagRow =
-        notInBoostRotator && (boosted || reminded || isSupermindOffer);
-
-      this.cd.markForCheck();
-      this.cd.detectChanges();
-    });
   }
 
   ngOnDestroy() {
