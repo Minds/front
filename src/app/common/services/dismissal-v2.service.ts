c12a0df33a185d90d45414a4540fb95dd8c0fe15
Ben Hayward <ben@minds.com>
2023-07-10 15:55:36 +0100

    Explainer screens

---
+++ src/app/common/services/dismissal-v2.service.ts
@@ -0,0 +1,143 @@
+import { Injectable } from '@angular/core';
+import {
+  DismissGQL,
+  DismissMutation,
+  Dismissal,
+  GetDismissalsGQL,
+  GetDismissalsQuery,
+} from '../../../graphql/generated.engine';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { ApolloQueryResult } from '@apollo/client';
+
+export const CACHE_KEY = 'dismissals-v2';
+
+/**
+ * Dismissal service v2 - handles dismissal of an item by a given key.
+ * Initially built for explainer screens with the intention that it can be used
+ * for other front-end dismissible items in the future.
+ */
+@Injectable({ providedIn: 'root' })
+export class DismissalV2Service {
+  constructor(
+    private getDismissalsGQL: GetDismissalsGQL,
+    private dismissGQL: DismissGQL
+  ) {}
+
+  /**
+   * Get dismissals from cache or API.
+   * @param { boolean } bypassCache whether local cache should be bypassed.
+   * @returns { Observable<Dismissal[]> } - dismissals.
+   */
+  public getDismissals(bypassCache: boolean = false): Observable<Dismissal[]> {
+    if (!bypassCache) {
+      const cachedDismissals: Dismissal[] = this.getCachedDismissals();
+
+      if (cachedDismissals?.length) {
+        return of(cachedDismissals);
+      }
+    }
+
+    return this.getDismissalsGQL.fetch().pipe(
+      map((result: ApolloQueryResult<GetDismissalsQuery>): Dismissal[] => {
+        return result.data.dismissals;
+      }),
+      tap((dismissals: Dismissal[]): void => {
+        this.setCachedDismissals(dismissals);
+      }),
+      catchError(
+        (e: unknown): Observable<Dismissal[]> => {
+          console.error(e);
+          return of([]);
+        }
+      )
+    );
+  }
+
+  /**
+   * Whether a given key has been dismissed.
+   * @param { string } key - key to check.
+   * @returns { Observable<boolean> } - whether key has been dismissed.
+   */
+  public isDismissed(key: string): Observable<boolean> {
+    return this.getDismissals().pipe(
+      map((dismissals: Dismissal[]): boolean => {
+        return dismissals.some(
+          (dismissal: Dismissal) => dismissal?.key === key
+        );
+      })
+    );
+  }
+
+  /**
+   * Dismiss an item by a given key.
+   * @param { Observable<Dismissal> } key - key to dismiss.
+   * @returns { Observable<Dismissal> } - dismissed item on success.
+   */
+  public dismiss(key: string): Observable<Dismissal> {
+    return this.dismissGQL.mutate({ key }).pipe(
+      map(
+        (result: ApolloQueryResult<DismissMutation>): Dismissal => {
+          return result?.data?.dismiss;
+        }
+      ),
+      tap((dismissal: Dismissal): void => this.addCachedDismissal(dismissal)),
+      catchError(
+        (e: unknown): Observable<Dismissal> => {
+          console.error(e);
+          return of(null);
+        }
+      )
+    );
+  }
+
+  /**
+   * Gets locally cached dismissals.
+   * @returns { Dismissal[] } locally cached dismissals.
+   */
+  private getCachedDismissals(): Dismissal[] {
+    try {
+      return JSON.parse(localStorage.getItem(CACHE_KEY));
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  }
+
+  /**
+   * Adds a dismissal to the local storage cache.
+   * @param { Dismissals } dismissal - dismissal to add.
+   * @returns { void }
+   */
+  private addCachedDismissal(dismissal: Dismissal): void {
+    this.setCachedDismissals([
+      ...this.getCachedDismissals(),
+      {
+        userGuid: dismissal.userGuid,
+        key: dismissal.key,
+        dismissalTimestamp: dismissal.dismissalTimestamp,
+      },
+    ]);
+  }
+
+  /**
+   * Initially set or overwrite existing cached dismissals in local storage.
+   * @param { Dismissal[] } dismissals - dismissals to set.
+   * @returns { Promise<void> }
+   */
+  private async setCachedDismissals(dismissals: Dismissal[]): Promise<void> {
+    try {
+      dismissals = dismissals.map(
+        (dismissal: Dismissal): Dismissal => {
+          return {
+            userGuid: dismissal.userGuid,
+            key: dismissal.key,
+            dismissalTimestamp: dismissal.dismissalTimestamp,
+          };
+        }
+      );
+      return localStorage.setItem(CACHE_KEY, JSON.stringify(dismissals));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
---
+++ src/app/modules/explainer-screens/services/explainer-screen.service.ts
@@ -0,0 +1,142 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import {
+  ExplainerScreenWeb,
+  GetExplainerScreensGQL,
+  GetExplainerScreensQuery,
+} from '../../../../graphql/generated.strapi';
+import {
+  Observable,
+  catchError,
+  forkJoin,
+  map,
+  of,
+  shareReplay,
+  take,
+} from 'rxjs';
+import { ApolloQueryResult } from '@apollo/client';
+import { ExplainerScreenModalService } from './explainer-screen-lazy-modal.service';
+import { DismissalV2Service } from '../../../common/services/dismissal-v2.service';
+import { Dismissal } from '../../../../graphql/generated.engine';
+import { AbstractSubscriberComponent } from '../../../common/components/abstract-subscriber/abstract-subscriber.component';
+
+/**
+ * Service handling the showing and loading of data for explainer screens.
+ * Screens that show in a modal upon first navigation to a route.
+ */
+@Injectable({ providedIn: 'root' })
+export class ExplainerScreensService extends AbstractSubscriberComponent
+  implements OnDestroy {
+  constructor(
+    private getExplainerScreensGQL: GetExplainerScreensGQL,
+    private explainerScreenModal: ExplainerScreenModalService,
+    private dismissalV2Service: DismissalV2Service
+  ) {
+    super();
+  }
+
+  /**
+   * Get explainer screens from server.
+   */
+  public getExplainerScreens$: Observable<
+    ExplainerScreenWeb[]
+  > = this.getExplainerScreensGQL.fetch().pipe(
+    map(
+      (
+        result: ApolloQueryResult<GetExplainerScreensQuery>
+      ): ExplainerScreenWeb[] => {
+        return result.data.explainerScreensWeb.data.map(
+          explainerScreen => explainerScreen.attributes
+        ) as ExplainerScreenWeb[];
+      }
+    ),
+    shareReplay(),
+    catchError(
+      (e: unknown): Observable<ExplainerScreenWeb[]> => {
+        console.error(e);
+        return of([]);
+      }
+    )
+  );
+
+  /**
+   * Trigger routes from server.
+   */
+  public triggerRoutes$: Observable<string[]> = this.getExplainerScreens$.pipe(
+    map((explainerScreens: ExplainerScreenWeb[]) => {
+      return explainerScreens.map(
+        (explainerScreen: ExplainerScreenWeb) => explainerScreen.triggerRoute
+      );
+    })
+  );
+
+  /**
+   * Handles route change.
+   * @param { string } route - route that was changed to.
+   * @returns { void }
+   */
+  public handleRouteChange(route: string): void {
+    this.subscriptions.push(
+      this.triggerRoutes$
+        .pipe(take(1))
+        .subscribe((triggerRoutes: string[]): void => {
+          // Find matched route taking into account wildcards.
+          const matchedRoute: string = triggerRoutes.filter(
+            (triggerRoute: string) =>
+              new RegExp('^' + triggerRoute.replace('*', '.*') + '$').test(
+                route
+              )
+          )?.[0];
+
+          if (matchedRoute) {
+            this.handleMatchingTriggerRoute(matchedRoute);
+          }
+        })
+    );
+  }
+
+  /**
+   * Handles a matching trigger route, checking whether it has already been dismissed
+   * and opening the explainer screen if it has not.
+   * @param { string } route - route that was matched.
+   * @returns { void }
+   */
+  private handleMatchingTriggerRoute(route: string): void {
+    this.subscriptions.push(
+      // get both in parallel.
+      forkJoin([
+        this.getExplainerScreens$,
+        this.dismissalV2Service.getDismissals(),
+      ]).subscribe(
+        ([explainerScreens, dismissals]: [
+          ExplainerScreenWeb[],
+          Dismissal[]
+        ]): void => {
+          // get matching explainer screen for passed route
+          const explainerScreen: ExplainerScreenWeb = explainerScreens.filter(
+            (explainerScreen: ExplainerScreenWeb) => {
+              return explainerScreen.triggerRoute === route;
+            }
+          )?.[0];
+
+          if (!explainerScreen) {
+            console.error(`No explainer screen found for route: ${route}`);
+            return;
+          }
+
+          // check if explainer screen has already been dismissed.
+          if (
+            dismissals.some(
+              (dismissal: Dismissal): boolean =>
+                dismissal.key === explainerScreen.key
+            )
+          ) {
+            return;
+          }
+
+          // open explainer screen modal.
+          this.explainerScreenModal.open(explainerScreen);
+        }
+      )
+    );
+  }
+}
