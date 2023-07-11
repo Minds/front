2fc7abd3d0a892fe3a87255a697123464a30f9ae
Ben Hayward <ben@minds.com>
2023-07-11 10:34:50 +0100

    Change group ogImage to group avatar minds#4111.

--- src/app/modules/groups/v2/services/seo.service.ts
+++ src/app/modules/groups/v2/services/seo.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import {
   MetaService,
   MIN_METRIC_FOR_ROBOTS,
 } from '../../../../common/services/meta.service';
 import { MindsGroup } from '../group.model';
+import { CDN_URL } from '../../../../common/injection-tokens/url-injection-tokens';
 
 /**
  * SEO meta headers service for groups
@@ -14,7 +15,10 @@ export class GroupSeoService {
    * Constructor
    * @param meta
    */
-  constructor(protected meta: MetaService) {}
+  constructor(
+    protected meta: MetaService,
+    @Inject(CDN_URL) private readonly cdnUrl
+  ) {}
 
   /**
    * Sets SEO meta headers
@@ -25,6 +29,11 @@ export class GroupSeoService {
       this.meta.setTitle(group);
       return;
     }
+
+    this.meta.setOgImage(this.getAvatarUrl(group), {
+      height: 315,
+      width: 600,
+    });
 
     // TODO
     // if (group.not_found || !group.avatar_url) {
@@ -37,10 +46,6 @@ export class GroupSeoService {
     //   .setDescription(user.briefdescription || `Subscribe to @${user.username}`)
     //   .setOgUrl(url)
     //   .setCanonicalUrl(url)
-    //   .setOgImage(user.avatar_url.master, {
-    //     width: 2000,
-    //     height: 1000,
-    //   })
     //   .setOgType('profile')
     //   .setRobots(
     //     user['subscribers_count'] < MIN_METRIC_FOR_ROBOTS ? 'noindex' : 'all'
@@ -49,5 +54,15 @@ export class GroupSeoService {
     // if (user.is_mature || user.nsfw.length) {
     //   this.meta.setNsfw(true);
     // }
+  }
+
+  /**
+   * Gets avatar URL for a given group.
+   * @param { MindsGroup } group - Minds group to get avatar for.
+   * @returns { string } group URL.
+   */
+  private getAvatarUrl(group: MindsGroup): string {
+    const iconTime: number = group.icontime ?? Date.now();
+    return `${this.cdnUrl}fs/v1/avatars/${group.guid}/large/${iconTime}`;
   }
 }
