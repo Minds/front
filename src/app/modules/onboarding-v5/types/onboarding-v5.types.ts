8538ff12c43fe736beb49eb7844575b0075c1b5b
Ben Hayward <ben@minds.com>
2023-07-12 18:06:49 +0100

    Add email change functionality to onboarding v5 flow minds#4166

--- src/app/modules/onboarding-v5/types/onboarding-v5.types.ts
+++ src/app/modules/onboarding-v5/types/onboarding-v5.types.ts
@@ -1,8 +1,14 @@
 import { ComponentOnboardingV5OnboardingStep } from '../../../../graphql/generated.strapi';
 
-// representation of an onboarding step.
+/** Representation of an onboarding step. */
 export type OnboardingStep = {
   completed: boolean;
   stepType: string;
   data: ComponentOnboardingV5OnboardingStep;
 };
+
+/** Sub-panels for verify email step */
+export enum OnboardingV5VerifyEmailSubPanel {
+  CODE_INPUT,
+  CHANGE_EMAIL,
+}
