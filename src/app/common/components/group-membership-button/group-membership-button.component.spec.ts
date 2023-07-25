8154fcd3253b8b2d211404900b83b00ccfbf6fd5
Ben Hayward <ben@minds.com>
2023-07-25 20:57:53 +0100

    Add boost click tracking update for groups minds#4159

--- src/app/common/components/group-membership-button/group-membership-button.component.spec.ts
+++ src/app/common/components/group-membership-button/group-membership-button.component.spec.ts
@@ -14,6 +14,9 @@ import { LoginReferrerService } from '../../../services/login-referrer.service';
 import { loginReferrerServiceMock } from '../../../mocks/services/login-referrer-service-mock.spec';
 import { ModernGroupsExperimentService } from '../../../modules/experiments/sub-services/modern-groups-experiment.service';
 import { BehaviorSubject } from 'rxjs';
+import { ClientMetaService } from '../../services/client-meta.service';
+import { groupMock } from '../../../mocks/responses/group.mock';
+import { ClientMetaDirective } from '../../directives/client-meta.directive';
 
 describe('GroupMembershipButtonComponent', () => {
   let fixture: ComponentFixture<GroupMembershipButtonComponent>;
@@ -73,6 +76,14 @@ describe('GroupMembershipButtonComponent', () => {
             provide: ModernGroupsExperimentService,
             useValue: MockService(ModernGroupsExperimentService),
           },
+          {
+            provide: ClientMetaService,
+            useValue: MockService(ClientMetaService),
+          },
+          {
+            provide: ClientMetaDirective,
+            useValue: MockService(ClientMetaDirective),
+          },
         ],
       })
         .overrideProvider(GroupMembershipService, {
@@ -109,5 +120,50 @@ describe('GroupMembershipButtonComponent', () => {
 
   it('should initialize', () => {
     expect(comp).toBeTruthy();
+  });
+
+  describe('recordClick', () => {
+    it('should record a click on a non-boosted group', () => {
+      comp.group = groupMock;
+      (comp as any).joinClickRecorded = false;
+      comp.recordClick();
+
+      expect((comp as any).clientMetaService.recordClick).toHaveBeenCalledWith(
+        groupMock.guid,
+        (comp as any).parentClientMeta,
+        {}
+      );
+      expect((comp as any).joinClickRecorded).toBe(true);
+    });
+
+    it('should record a click on a boosted group', () => {
+      comp.group = {
+        ...groupMock,
+        boosted_guid: '1234567890123456',
+        urn: 'urn:boost:1234567890123456',
+      } as any;
+
+      (comp as any).joinClickRecorded = false;
+      comp.recordClick();
+
+      expect((comp as any).clientMetaService.recordClick).toHaveBeenCalledWith(
+        groupMock.guid,
+        (comp as any).parentClientMeta,
+        {
+          campaign: comp.group.urn,
+        }
+      );
+      expect((comp as any).joinClickRecorded).toBe(true);
+    });
+
+    it('should NOT record a click if one has already been recorded', () => {
+      (comp as any).joinClickRecorded = true;
+      comp.recordClick();
+
+      expect(
+        (comp as any).clientMetaService.recordClick
+      ).not.toHaveBeenCalled();
+      expect((comp as any).joinClickRecorded).toBe(true);
+    });
   });
 });
