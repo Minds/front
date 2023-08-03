864fd3728b2911550248a2b6016caf6059d84c7a
Olivia Madrid <olivia@minds.com>
2023-08-03 08:01:33 +0000

    Explore tabs consolidation #6050

--- src/app/modules/discovery/top/top.component.ts
+++ src/app/modules/discovery/top/top.component.ts
@@ -1,15 +1,22 @@
-import { Component, HostBinding } from '@angular/core';
-import { ExperimentsService } from '../../experiments/experiments.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 /**
- * Discovery top feed component.
- * Presents a default recommendations feed with discovery tabs.
+ * Discovery "Trending" feed component (formerly top)
+ * Presents a default recommendations feed with discovery/explore tabs.
  */
 @Component({
   selector: 'm-discovery__top',
   styleUrls: ['top.component.ng.scss'],
   templateUrl: './top.component.html',
 })
-export class DiscoveryTopComponent {
-  constructor() {}
+export class DiscoveryTopComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // The trending feed is actually a search result page
+    this.router.navigate(['/discovery/search'], {
+      queryParams: { t: 'all', f: 'top', explore: true },
+    });
+  }
 }
