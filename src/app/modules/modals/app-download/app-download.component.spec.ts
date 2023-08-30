d1ba6387738113ee3e307281dc42842c84002ca6
Olivia Madrid <oliviajmadrid@gmail.com>
2023-08-29 17:30:21 -0700

    tests

--- src/app/modules/modals/app-download/app-download.component.spec.ts
+++ src/app/modules/modals/app-download/app-download.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppDownloadModalComponent } from './app-download.component';
+import { ConfigsService } from '../../../common/services/configs.service';
+import { MockService } from '../../../utils/mock';
 
 describe('AppDownloadModalComponent', () => {
   let component: AppDownloadModalComponent;
@@ -9,6 +11,9 @@ describe('AppDownloadModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppDownloadModalComponent],
+      providers: [
+        { provide: ConfigsService, useValue: MockService(ConfigsService) },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppDownloadModalComponent);
