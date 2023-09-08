05dcae42957c26258d06c29c6681c62c14161dd4
Ben Hayward <ben@minds.com>
2023-09-08 12:57:27 +0100

    Add feature flag

--- src/app/modules/wallet/components/credits/send/send.component.ts
+++ src/app/modules/wallet/components/credits/send/send.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GiftCardPurchaseExperimentService } from '../../../../experiments/sub-services/gift-card-purchase-experiment.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'm-walletV2__creditsSend',
   templateUrl: './send.component.html',
   styleUrls: ['./send.component.ng.scss'],
 })
-export class WalletV2CreditsSendComponent {}
+export class WalletV2CreditsSendComponent implements OnInit {
+  constructor(
+    private purchaseExperiment: GiftCardPurchaseExperimentService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.purchaseExperiment.isActive()) {
+      this.router.navigate(['/wallet/credits/history']);
+    }
+  }
+}
