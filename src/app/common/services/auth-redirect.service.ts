c972212541a0edac9395cfe666ad1147856e5cb7
Ben <ben@minds.com>
2023-11-20 14:17:59 +0000

    Tenant login redirect minds#4461

--- src/app/common/services/auth-redirect.service.ts
+++ src/app/common/services/auth-redirect.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { IS_TENANT_NETWORK } from '../injection-tokens/tenant-injection-tokens';
 
 /**
  * Currently this service provides the default redirect URL
@@ -8,7 +9,10 @@ import { Router } from '@angular/router';
  */
 @Injectable()
 export class AuthRedirectService {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    @Inject(IS_TENANT_NETWORK) private readonly isTenantNetwork: boolean
+  ) {}
 
   public redirect(): Promise<boolean> {
     return this.router.navigate([this.getRedirectUrl()]);
@@ -19,6 +23,8 @@ export class AuthRedirectService {
    * @returns { string } redirect URL.
    */
   public getRedirectUrl(): string {
-    return '/newsfeed/subscriptions/for-you';
+    return this.isTenantNetwork
+      ? '/newsfeed/subscriptions/top'
+      : '/newsfeed/subscriptions/for-you';
   }
 }
