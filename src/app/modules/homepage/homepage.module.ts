f0a57ac8b7e671b772079beb77cd46f399e7499e
Olivia Madrid <oliviajmadrid@gmail.com>
2024-02-12 08:57:53 -0700

    Tenants guest mode feed

--- src/app/modules/homepage/homepage.module.ts
+++ src/app/modules/homepage/homepage.module.ts
@@ -13,7 +13,7 @@ import { MindsFormsModule } from '../forms/forms.module';
 import { MarketingModule } from '../marketing/marketing.module';
 import { ExperimentsModule } from '../experiments/experiments.module';
 import { HomepageV3Module } from '../homepage-v3/homepage-v3.module';
-import { HomepageContainerComponent } from './container.component';
+import { HomepageContainerComponent } from './homepage-container.component';
 import { DiscoverySharedModule } from '../discovery/discovery-shared.module';
 import { DefaultFeedModule } from '../default-feed/default-feed.module';
 import { AboutModule } from '../about/about.module';
