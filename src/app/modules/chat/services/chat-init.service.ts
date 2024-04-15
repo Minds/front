142b057bf512c4d45c7efc1a65c0af94f57a930d
Ben <ben@minds.com>
2024-04-15 12:33:04 +0000

    [front] Live chat messages front#6163

---
+++ src/app/modules/chat/services/chat-init.service.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscription, distinctUntilChanged, map, of } from 'rxjs';
+import {
+  InitChatGQL,
+  InitChatQuery,
+} from '../../../../graphql/generated.engine';
+import { Session } from '../../../services/session';
+import { QueryRef } from 'apollo-angular';
+import { GlobalChatSocketService } from './global-chat-socket.service';
+
+/**
+ * Service to aggregate queries used to initialize the chat service.
+ */
+@Injectable({ providedIn: 'root' })
+export class ChatInitService {
+  /** The query reference. */
+  private _queryRef: QueryRef<InitChatQuery>;
+
+  /** The subscription to get chat room guids. */
+  private getChatRoomGuidsSubscription: Subscription;
+
+  constructor(
+    private initChatGQL: InitChatGQL,
+    private globalChatSocketService: GlobalChatSocketService,
+    private session: Session
+  ) {}
+
+  /**
+   * Initializes the chat init service.
+   * @returns { void }
+   */
+  public init(): void {
+    if (!this.session.isLoggedIn()) {
+      console.info('Skipping chat socket init as user is not logged in');
+      return;
+    }
+
+    this.getChatRoomGuidsSubscription = this.getChatRoomGuids$()
+      .pipe(distinctUntilChanged())
+      .subscribe((roomGuids: string[]): void => {
+        this.globalChatSocketService.listenToRoomGuids(roomGuids);
+      });
+  }
+
+  /**
+   * Destroys the chat init service.
+   * @returns { void }
+   */
+  public destroy(): void {
+    this._queryRef?.resetLastResults();
+    this.getChatRoomGuidsSubscription?.unsubscribe();
+    this.globalChatSocketService.leaveAllRooms();
+  }
+
+  /**
+   * Refetches the query.
+   * @returns { void }
+   */
+  public refetch(): void {
+    this.getQueryRef().refetch();
+  }
+
+  /**
+   * Reinitializes the chat init service.
+   * @returns { void }
+   */
+  public reinit(): void {
+    this.destroy();
+    this.refetch();
+    this.init();
+  }
+
+  /**
+   * Get unread message count.
+   * @returns { Observable<number> } - the unread message count.
+   */
+  public getUnreadCount$(): Observable<number> {
+    if (!this.session.isLoggedIn()) {
+      return of(null);
+    }
+    return this.getQueryRef().valueChanges.pipe(
+      map(({ data }) => data.chatUnreadMessagesCount)
+    );
+  }
+
+  /**
+   * Get chat room guids.
+   * @returns { Observable<string[]> } - the chat room guids.
+   */
+  public getChatRoomGuids$(): Observable<string[]> {
+    if (!this.session.isLoggedIn()) {
+      return of([]);
+    }
+    return this.getQueryRef().valueChanges.pipe(
+      map(({ data }) => data.chatRoomGuids)
+    );
+  }
+
+  /**
+   * Gets the query reference or instantiates a new one.
+   * @returns { QueryRef<GetChatUnreadCountQuery> } - the query reference.
+   */
+  private getQueryRef(): QueryRef<InitChatQuery> {
+    if (!this._queryRef) {
+      this._queryRef = this.initChatGQL.watch();
+    }
+    return this._queryRef;
+  }
+}
--- src/app/modules/chat/services/chat-messages.service.ts
+++ src/app/modules/chat/services/chat-messages.service.ts
@@ -13,6 +13,7 @@ import {
   ChatMessageEdge,
   DeleteChatMessageGQL,
   DeleteChatMessageMutation,
+  GetChatMessagesDocument,
   GetChatMessagesGQL,
   GetChatMessagesQuery,
   GetChatMessagesQueryVariables,
@@ -22,9 +23,14 @@ import { MutationResult, QueryRef } from 'apollo-angular';
 import { ToasterService } from '../../../common/services/toaster.service';
 import { ApolloQueryResult } from '@apollo/client';
 import { Router } from '@angular/router';
+import {
+  ChatRoomEvent,
+  GlobalChatSocketService,
+} from './global-chat-socket.service';
+import { Session } from '../../../services/session';
 
 /** Size of an individual page. */
-const PAGE_SIZE: number = 24;
+export const PAGE_SIZE: number = 24;
 
 @Injectable()
 export class ChatMessagesService extends AbstractSubscriberComponent {
@@ -35,6 +41,14 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
   /** Exposed observable to track whether a request is in progress. */
   public readonly inProgress$: Observable<boolean> =
     this._inProgress$.asObservable();
+
+  /** Whether request to fetch new is in progress. */
+  private readonly _fetchNewInProgress$: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(false);
+
+  /** Exposed observable to track whether a request to fetch new is in progress. */
+  public readonly fetchNewInProgress$: Observable<boolean> =
+    this._fetchNewInProgress$.asObservable();
 
   /** Whether the service has been initialized. */
   private readonly _initialized$: Subject<boolean> =
@@ -66,16 +80,26 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
     GetChatMessagesQueryVariables
   >;
 
-  private _chatMessageAppended$: Subject<boolean> = new Subject<boolean>();
+  /** Subject to request that listening components scroll to bottom. */
+  private readonly _scrollToBottom$: Subject<boolean> = new Subject<boolean>();
 
-  public chatMessageAppended$: Observable<boolean> =
-    this._chatMessageAppended$.asObservable();
+  /** Observable to request that listening components scroll to bottom. */
+  public readonly scrollToBottom$: Observable<boolean> =
+    this._scrollToBottom$.asObservable();
+
+  /** Updated when there is a new message to load. */
+  public readonly hasNewMessage$: Subject<void> = new Subject<void>();
+
+  /** The end cursor for the query. */
+  private endCursor: string;
 
   constructor(
     private getChatMessagesGql: GetChatMessagesGQL,
     private deleteChatMessage: DeleteChatMessageGQL,
+    private globalChatSocketService: GlobalChatSocketService,
     private toaster: ToasterService,
-    private router: Router
+    private router: Router,
+    private session: Session
   ) {
     super();
   }
@@ -86,7 +110,7 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
    */
   public init(roomGuid: string): void {
     this.initQueryRef(roomGuid);
-    this.subscribeToValueChanges();
+    this.setupSubscriptions(roomGuid);
   }
 
   /**
@@ -105,6 +129,25 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
         });
       })
     );
+  }
+
+  /**
+   * Fetches new messages. Will manually patch values into the cache such that
+   * it can be handled properly by the value change listener.
+   * @returns { void }
+   */
+  public fetchNew(): void {
+    this._fetchNewInProgress$.next(true);
+
+    this.queryRef.subscribeToMore({
+      document: GetChatMessagesDocument,
+      variables: {
+        roomGuid: this.queryRef.variables.roomGuid,
+        after: this.endCursor,
+        first: PAGE_SIZE,
+      },
+      updateQuery: this.updateCacheWithNewMessage.bind(this),
+    });
   }
 
   /**
@@ -130,9 +173,10 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
 
   /**
    * Subscribes to value changes on the query reference.
+   * @param { string } roomGuid - The room guid to subscribe to.
    * @returns { void }
    */
-  private subscribeToValueChanges(): void {
+  private setupSubscriptions(roomGuid: string): void {
     if (!this.queryRef) {
       console.warn(
         'Attempted to subscribe to value changes before initializing query reference'
@@ -170,11 +214,41 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
               result?.data?.chatMessages?.edges as ChatMessageEdge[]
             );
             this._pageInfo$.next(result?.data?.chatMessages?.pageInfo);
+
+            // we want to keep track of the end cursor for fetching new messages.
+            this.endCursor =
+              result?.data?.chatMessages?.edges[
+                result?.data?.chatMessages?.edges.length - 1
+              ].cursor;
+
             this._inProgress$.next(false);
             this._initialized$.next(true);
+
+            if (this._fetchNewInProgress$.getValue()) {
+              this._fetchNewInProgress$.next(false);
+              this.requestScrollToBottom();
+            }
           } catch (e) {
             this.handleError(e);
           }
+        }),
+      this.globalChatSocketService
+        .getEventsByChatRoomGuid(roomGuid)
+        .subscribe((event: ChatRoomEvent): void => {
+          // Skip our own messages.
+          if (
+            this.session.getLoggedInUser()?.guid ===
+            parseInt(event.data['metadata']['senderGuid'])
+          ) {
+            return;
+          }
+
+          this._pageInfo$.next({
+            ...this._pageInfo$.getValue(),
+            hasNextPage: true,
+          });
+
+          this.hasNewMessage$.next();
         })
     );
   }
@@ -196,9 +270,8 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
    * @param { ChatMessageEdge } chatMessageEdge - The chat message edge to append.
    * @returns { void }
    */
-  public appendChatMessage(chatMessageEdge: ChatMessageEdge): void {
-    this._edges$.next([...this._edges$.getValue(), chatMessageEdge]);
-    this._chatMessageAppended$.next(true);
+  public requestScrollToBottom(): void {
+    this._scrollToBottom$.next(true);
   }
 
   /**
@@ -239,5 +312,56 @@ export class ChatMessagesService extends AbstractSubscriberComponent {
       this.toaster.error(e?.message);
       return false;
     }
+  }
+
+  /**
+   * Check if there is a next page.
+   * @returns { boolean } - true if there is a next page.
+   */
+  public hasNextPage(): boolean {
+    return this._pageInfo$.getValue()?.hasNextPage;
+  }
+
+  /**
+   * Update the Apollo cache with a new message.
+   * @param { GetChatMessagesQuery } prev - The previous query.
+   * @param { ApolloQueryResult<GetChatMessagesQuery> } queryResult - containing subscription data.
+   * @returns { GetChatMessagesQuery } - The updated query.
+   */
+  private updateCacheWithNewMessage(
+    prev: GetChatMessagesQuery,
+    {
+      subscriptionData,
+    }: { subscriptionData: ApolloQueryResult<GetChatMessagesQuery> }
+  ): GetChatMessagesQuery {
+    if (!subscriptionData.data) {
+      return prev;
+    }
+
+    // deduplicate messages.
+    let newEdges = subscriptionData.data.chatMessages.edges.filter(
+      (edge: ChatMessageEdge) =>
+        prev.chatMessages.edges.filter((prevEdge: ChatMessageEdge): boolean => {
+          return prevEdge.id !== edge.id;
+        }).length === prev.chatMessages.edges.length
+    );
+
+    let pageInfo: PageInfo = subscriptionData.data.chatMessages.pageInfo;
+
+    /**
+     * We need to patch edges to insert new edges, but also page data, as we are
+     * changing the next page states, but don't want to change the previous page states.
+     */
+    return {
+      chatMessages: {
+        edges: [...prev.chatMessages.edges, ...newEdges],
+        pageInfo: {
+          hasNextPage: pageInfo.hasNextPage,
+          hasPreviousPage: prev.chatMessages.pageInfo.hasPreviousPage,
+          startCursor: prev.chatMessages.pageInfo.startCursor,
+          endCursor: pageInfo.endCursor,
+        },
+      },
+    };
   }
 }
