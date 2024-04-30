1859d49ac598b832eeb45cb67a0f26f0fbdfea6f
Ben <ben@minds.com>
2024-04-30 12:30:17 +0000

    Fix issues with group description on custom homepage minds#4827

---
+++ src/app/modules/marketing/consts/tenant-footer-stub.ts
@@ -0,0 +1,36 @@
+import { Footer } from '../../../../graphql/generated.strapi';
+
+/**
+ * Stubbed data to render a static footer component for tenant networks.
+ */
+export const STRAPI_FOOTER_STUB_DATA: Footer = {
+  __typename: 'Footer',
+  columns: [],
+  copyrightText: '',
+  logo: {},
+  showLanguageBar: false,
+  slogan: '',
+  bottomLinks: [
+    {
+      id: '1',
+      text: 'Terms of Service',
+      url: '/pages/terms-of-service',
+      dataRef: 'tenant-footer-link-terms',
+      __typename: 'ComponentFooterLink',
+    },
+    {
+      id: '2',
+      text: 'Privacy Policy',
+      url: '/pages/privacy-policy',
+      dataRef: 'tenant-footer-link-privacy',
+      __typename: 'ComponentFooterLink',
+    },
+    {
+      id: '3',
+      text: 'Content Policy',
+      url: '/pages/community-guidelines',
+      dataRef: 'tenant-footer-link-content',
+      __typename: 'ComponentFooterLink',
+    },
+  ],
+};
