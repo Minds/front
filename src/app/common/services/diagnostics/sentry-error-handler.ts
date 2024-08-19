ca7099624cb9080206f8c29c569b000a6513e1a1
Ben <ben@minds.com>
2024-08-19 14:43:49 +0000

    Fix Sentry for SSR https://gitlab.com/minds/front/-/issues/6189

---
+++ src/app/common/services/diagnostics/sentry-error-handler.ts
@@ -0,0 +1,51 @@
+import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
+import {
+  getIsolationScope,
+  addIntegration,
+  SentryErrorHandler,
+} from '@sentry/angular';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { SSR_SENTRY_INTEGRATIONS } from '../../injection-tokens/common-injection-tokens';
+import type { Integration } from '@sentry/types';
+import { isPlatformBrowser } from '@angular/common';
+export { DiagnosticsService } from './diagnostics.service';
+
+/**
+ * Sentry error handler.
+ */
+@Injectable({ providedIn: 'root' })
+export class MindsSentryErrorHandler extends SentryErrorHandler {
+  constructor(
+    @Optional()
+    @Inject(SSR_SENTRY_INTEGRATIONS)
+    private ssrIntegrations: Integration[],
+    @Optional() @Inject(REQUEST) private request: Request,
+    @Optional() @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    super();
+    // take SSR integrations from server injection.
+    if (this.ssrIntegrations) {
+      for (const integration of this.ssrIntegrations) {
+        addIntegration(integration);
+      }
+    }
+  }
+
+  /**
+   * Handle error.
+   * @param { any } error
+   * @returns { void }
+   */
+  handleError(error: any): void {
+    // Discard client side for now.
+    if (isPlatformBrowser(this.platformId)) return;
+
+    if (this.request) {
+      getIsolationScope().setSDKProcessingMetadata({
+        request: this.request,
+      });
+    }
+
+    super.handleError(error);
+  }
+}
