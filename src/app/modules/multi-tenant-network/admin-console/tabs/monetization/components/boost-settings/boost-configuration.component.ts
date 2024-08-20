02c8e38ecfa6132fbeb969afda428a1438f25d60
Ben <ben@minds.com>
2024-08-20 13:37:49 +0100

    Refactor Boost configurations into Monetization tab minds#5027

--- src/app/modules/multi-tenant-network/admin-console/tabs/monetization/components/boost-settings/boost-configuration.component.ts
+++ src/app/modules/multi-tenant-network/admin-console/tabs/monetization/components/boost-settings/boost-configuration.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MultiTenantConfig } from '../../../../../../../../graphql/generated.engine';
-import { Observable, map } from 'rxjs';
+import { Observable, firstValueFrom, map } from 'rxjs';
 import { MultiTenantNetworkConfigService } from '../../../../../services/config.service';
 import { BoostEmbedBuilderComponent } from './boost-embed-builder/boost-embed-builder.component';
 import { NetworkAdminEnableBoostToggleComponent } from './enable-boost-toggle/enable-boost-toggle.component';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { GrowShrinkFast } from '../../../../../../../animations';
 import { NetworkAdminCreateBoostLinkComponent } from './create-boost-link/create-boost-link.component';
+import { StripeKeysService } from '../../services/stripe-keys.service';
+import { Router } from '@angular/router';
+import { ToasterService } from '../../../../../../../common/services/toaster.service';
 
 /**
  * Network admin boost configuration component.
@@ -25,7 +28,7 @@ import { NetworkAdminCreateBoostLinkComponent } from './create-boost-link/create
   ],
   animations: [GrowShrinkFast],
 })
-export class NetworkAdminBoostConfigurationComponent {
+export class NetworkAdminBoostConfigurationComponent implements OnInit {
   /** Whether boost embed builder should be shown. */
   protected readonly isBoostEnabled$: Observable<boolean> =
     this.multiTenantConfigService.config$.pipe(
@@ -43,6 +46,30 @@ export class NetworkAdminBoostConfigurationComponent {
     );
 
   constructor(
-    private multiTenantConfigService: MultiTenantNetworkConfigService
+    private multiTenantConfigService: MultiTenantNetworkConfigService,
+    private stripeKeysService: StripeKeysService,
+    private router: Router,
+    private toaster: ToasterService
   ) {}
+
+  ngOnInit(): void {
+    this.checkStripeKeys(); // async
+  }
+
+  /**
+   * Checks whether stripe keys are set, and redirects if the user has not set any.
+   * @returns { void } 
+   */
+  private async checkStripeKeys(): Promise<void> {
+    // if not initialized, fetch any keys from server.
+    if (!await firstValueFrom(this.stripeKeysService.initialized$)) {
+      await this.stripeKeysService.fetchStripeKeys();
+    }
+
+    // check whether user has set stripe keys, redirect if they have not.
+    if (!await firstValueFrom(this.stripeKeysService.hasSetStripeKeys$)) {
+      this.toaster.warn('You must enable Stripe keys before accessing this page.')
+      this.router.navigate(['/network/admin/monetization']);
+    }
+  }
 }
--- src/app/modules/multi-tenant-network/admin-console/tabs/monetization/components/monetization/monetization.component.ts
+++ src/app/modules/multi-tenant-network/admin-console/tabs/monetization/components/monetization/monetization.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { StripeKeysService } from '../../services/stripe-keys.service';
-import { Observable } from 'rxjs';
 
 /**
  * Monetization tab base component.
@@ -10,10 +8,4 @@ import { Observable } from 'rxjs';
   templateUrl: './monetization.component.html',
   styleUrls: ['./monetization.component.ng.scss'],
 })
-export class NetworkAdminMonetizationComponent {
-  /** True when Stripe keys have been set. */
-  protected readonly hasSetStripeKeys$: Observable<boolean> =
-    this.stripeKeysService.hasSetStripeKeys$;
-
-  constructor(private stripeKeysService: StripeKeysService) {}
-}
+export class NetworkAdminMonetizationComponent {}
