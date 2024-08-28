3522178a92d77ad3256c194b06d37d1a179c808c
Ben <ben@minds.com>
2024-08-28 08:22:28 +0000

    Refactor Boost settings into monetization tab minds#5027

--- src/app/modules/multi-tenant-network/admin-console/tabs/monetization/components/tabs/tabs.component.spec.ts
+++ src/app/modules/multi-tenant-network/admin-console/tabs/monetization/components/tabs/tabs.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NetworkAdminMonetizationTabsComponent } from './tabs.component';
+import { MockService } from '../../../../../../../utils/mock';
+import { StripeKeysService } from '../../services/stripe-keys.service';
+import { BehaviorSubject } from 'rxjs';
 
 describe('NetworkAdminMonetizationTabsComponent', () => {
   let comp: NetworkAdminMonetizationTabsComponent;
@@ -8,6 +11,19 @@ describe('NetworkAdminMonetizationTabsComponent', () => {
   beforeEach((done: DoneFn) => {
     TestBed.configureTestingModule({
       declarations: [NetworkAdminMonetizationTabsComponent],
+      providers: [
+        {
+          provide: StripeKeysService,
+          useValue: MockService(StripeKeysService, {
+            has: ['hasSetStripeKeys$'],
+            props: {
+              hasSetStripeKeys$: {
+                get: () => new BehaviorSubject<boolean>(true),
+              },
+            },
+          }),
+        },
+      ],
     });
 
     fixture = TestBed.createComponent(NetworkAdminMonetizationTabsComponent);
@@ -34,5 +50,29 @@ describe('NetworkAdminMonetizationTabsComponent', () => {
         'a[data-ref=tenant-admin-monetization-tab-memberships]'
       )
     ).toBeTruthy();
+  });
+
+  describe('Boost tab', () => {
+    it('should not show Boost tab when Stripe keys are not set', () => {
+      (comp as any).stripeKeysService.hasSetStripeKeys$.next(false);
+
+      fixture.detectChanges();
+      expect(
+        fixture.nativeElement.querySelector(
+          'a[data-ref=tenant-admin-monetization-tab-boost]'
+        )
+      ).toBeFalsy();
+    });
+
+    it('should show Boost tab when Stripe keys are set', () => {
+      (comp as any).stripeKeysService.hasSetStripeKeys$.next(true);
+
+      fixture.detectChanges();
+      expect(
+        fixture.nativeElement.querySelector(
+          'a[data-ref=tenant-admin-monetization-tab-boost]'
+        )
+      ).toBeTruthy();
+    });
   });
 });
