574428256788a41f7e321743fc6e9356c2ccd7a8
Mark Harding <mark@minds.com>
2025-04-01 15:07:37 +0100

    (chore): Add the release version to front-ssr sentry init and server docker file

--- instrument-sentry.ts
+++ instrument-sentry.ts
@@ -1,8 +1,9 @@
-import * as Sentry from "@sentry/node";
-import { SENTRY_IGNORED_ERRORS } from "./src/app/common/services/diagnostics/sentry-ignored-errors";
+import * as Sentry from '@sentry/node';
+import { SENTRY_IGNORED_ERRORS } from './src/app/common/services/diagnostics/sentry-ignored-errors';
 
 Sentry.init({
-  dsn: "https://bbf22a249e89416884e8d6e82392324f@o293216.ingest.us.sentry.io/5729114",
+  dsn: 'https://bbf22a249e89416884e8d6e82392324f@o293216.ingest.us.sentry.io/5729114',
   tracesSampleRate: 1.0,
-  ignoreErrors: SENTRY_IGNORED_ERRORS
+  ignoreErrors: SENTRY_IGNORED_ERRORS,
+  release: process.env.MINDS_VERSION,
 });
