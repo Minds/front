b3aa0c02388a11ffb4f8dd84487842ca6bdfed2d
Mark Harding <mark@minds.com>
2025-05-21 14:25:17 +0100

    (chore): Support global mode for notification bell

--- src/app/modules/channels/v2/actions/actions.component.ts
+++ src/app/modules/channels/v2/actions/actions.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChannelsV2Service } from '../channels-v2.service';
+import { ConfigsService } from '../../../../common/services/configs.service';
 
 /**
  * Toolbar at top of channel banner, with options that change
@@ -13,8 +14,20 @@ import { ChannelsV2Service } from '../channels-v2.service';
 })
 export class ChannelActionsComponent {
   /**
+   * If global model is on, the post notification bell will always show
+   */
+  isGlobalMode: boolean;
+
+  /**
    * Constructor
    * @param service
    */
-  constructor(public service: ChannelsV2Service) {}
+  constructor(
+    public service: ChannelsV2Service,
+    private config: ConfigsService
+  ) {}
+
+  ngOnInit() {
+    this.isGlobalMode = this.config.get('tenant')?.global_mode;
+  }
 }
